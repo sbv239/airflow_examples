cffc4e90ba3d32e29ae4076b5c411cba2d10129f
Vladislav Zabolotsky <colmar_24@mail.ru>
2022-03-20 16:24:45 +0300

    task_3

---
+++ dags/v-zabolotskij/task_3.py
@@ -0,0 +1,33 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG\
+    (
+    "task_3_v_zabolotskij",
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5), 
+    },
+    description = "DAG for task #3",
+    ) as dag:
+
+    for task in range(30):
+
+        if task <= 10:
+            bash_task = BashOperator(
+                task_id = "BO_task_" + str(task),
+                bash_command = f"echo {task}"            
+            )
+        else:
+            def task_number(task_number = task):
+                return f"task number is: {task_number}"
+            py_task = PythonOperator(
+                task_id = "PY_task_" + str(task),
+                python_callable = task_number
+            )
