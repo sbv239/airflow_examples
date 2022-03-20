143a391b3d73edf354d4dea593de87e92d3d1b2e
Vladislav Zabolotsky <colmar_24@mail.ru>
2022-03-20 17:51:38 +0300

    task_5

---
+++ dags/v-zabolotskij/task_5.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow.operators.bash import BashOperator
+with DAG\
+    (
+    "task_5_v_zabolotskij",
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5), 
+    },
+    description = "DAG for task #5",
+    ) as dag:
+
+    templated_code = dedent(
+        """
+    {% for i in range(5) %}
+        echo "{{ ts }}"
+    {% endfor %}
+    echo "{{ run_id }}"
+        """
+    )
+  
+    task_1 = BashOperator(
+        task_id = "templated_print_ts_run_id",
+        bash_command = templated_code
+    )
+
