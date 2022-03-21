1443c0a6b7ccc76c646586ca6222fb63bdc43e29
Vladislav Zabolotsky <colmar_24@mail.ru>
2022-03-21 22:45:20 +0300

    modified task_3

--- dags/v-zabolotskij/task_3.py
+++ dags/v-zabolotskij/task_3.py
@@ -24,9 +24,10 @@ with DAG\
                 bash_command = f"echo {task}"            
             )
         else:
-            def task_number(task_number):
+            def print_task_num(task_number):
                 return f"task number is: {task_number}"
             py_task = PythonOperator(
                 task_id = "PY_task_" + str(task),
-                python_callable = task_number
+                python_callable = print_task_num,
+                op_kwargs = {"task_number": task}
             )
