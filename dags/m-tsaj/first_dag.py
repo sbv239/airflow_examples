3a6c5e6d543972df33d3c9140df5722b5824115f
GoldenGarr <maxim20031128@gmail.com>
2022-03-22 12:48:08 +0300

    Presumably fix t2

--- dags/m-tsaj/first_dag.py
+++ dags/m-tsaj/first_dag.py
@@ -13,6 +13,11 @@ with DAG(
             'retries': 1,
             'retry_delay': timedelta(minutes=5),
         },
+        description='First dag',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2021, 1, 1),
+        catchup=False,
+        tags=['example'],
 ) as dag:
     t1 = BashOperator(
         task_id='print_working_directory',
