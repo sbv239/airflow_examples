b2afa5176fb19d3fe67456021ef3ed0f667ad56a
Vladislav Zabolotsky <colmar_24@mail.ru>
2022-03-23 21:19:13 +0300

    fixed name of function

--- dags/v-zabolotskij/task_3.py
+++ dags/v-zabolotskij/task_3.py
@@ -28,10 +28,10 @@ with DAG\
                 bash_command = f"echo {task}"            
             )
         else:
-            def print_task_num(task_number):
+            def print_task_number(task_number):
                 return f"task number is: {task_number}"
             py_task = PythonOperator(
                 task_id = "PY_task_" + str(task),
-                python_callable = print_task_num,
+                python_callable = print_task_number,
                 op_kwargs = {"task_number": task}
             )
