9a1635032f403ff0ec445351d42b0c80bda6a4c5
Aleksei Vorotnikov <dududupi@yandex.ru>
2022-03-23 17:25:33 +0300

    modified:   hw_5.py

--- dags/a-vorotnikov-5/hw_5.py
+++ dags/a-vorotnikov-5/hw_5.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
 
 
 def check_task_num(task_number):
@@ -16,9 +15,5 @@ with DAG('hw_5_vorotnikov', default_args={
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }, start_date=datetime(2022, 3, 20), catchup=False) as dag:
-    for i in range(1, 31):
-        if i <= 10:
-            task = BashOperator(task_id='BashOp_t_' + str(i), bash_command="echo $NUMBER", env={"NUMBER": str(i)})
-        else:
-            task = PythonOperator(task_id='P_op_t_' + str(i), python_callable=check_task_num,
-                                  op_kwargs={'task_number': int(i)})
+    for i in range(1, 11):
+        task = BashOperator(task_id='BashOp_t_' + str(i), bash_command="echo $NUMBER", env={"NUMBER": str(i)})
