2bafe51889143bbadf3e33918f5519db6a0705d2
Vladislav Zabolotsky <colmar_24@mail.ru>
2022-03-23 23:47:52 +0300

    task_6 added

--- dags/v-zabolotskij/task_6.py
+++ dags/v-zabolotskij/task_6.py
@@ -1,10 +1,11 @@
 from airflow import DAG
 from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+import os
+
 with DAG\
     (
-    "task_3_v_zabolotskij",
+    "task_6_v_zabolotskij",
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -13,14 +14,19 @@ with DAG\
         'retries': 1,
         'retry_delay': timedelta(minutes=5), 
     },
-    description = "DAG for task #3",
+    description = "DAG for task #6",
+    schedule_interval = timedelta(days=1),
+    start_date = datetime(2022, 3, 20),
+    catchup = False,
+    tags = ["task_6"]
+
     ) as dag:
 
     for task in range(10):
-        NUMBER = task
+        os.environ['NUMBER'] = task
         if task <= 10:
             bash_task = BashOperator(
                 task_id = "BO_task_" + str(task),
                 bash_command = "echo $NUMBER"            
             )
-        
+    
