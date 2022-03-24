bd381a30ad34c676c1d1494f51ec85aba7481a7f
Alena Kizenko <alenakizenko@Bobs-MacBook-Pro.local>
2022-03-24 19:00:10 +0100

    edit file dag.py

--- dags/a-kizenko/dag.py
+++ dags/a-kizenko/dag.py
@@ -5,13 +5,22 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.bash import PythonOperator
+from datetime import timedelta
 
 
 date = "{{ ds }}"
 def return_date(date):
     return date
 
-with DAG() as dag:
+with DAG(
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}) as dag:
     task1 = PythonOperator(
     task_id='print_the_date',
     python_callable=return_date,
