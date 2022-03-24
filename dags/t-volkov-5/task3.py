69c55cab9b28075ae4cad9b6dc820f5788d1db86
Timur Volkov <volkovtimurp@yandex.ru>
2022-03-25 01:23:31 +0300

    yey another change task3.py

--- dags/t-volkov-5/task3.py
+++ dags/t-volkov-5/task3.py
@@ -24,7 +24,7 @@ with DAG(
 
     for i in range(10):
         t1 = BashOperator(
-            task_id='looping bash operator_' + str(i),
+            task_id='looping_bash_operator_' + str(i),
             bash_command=f'echo {i}'
         )
    
@@ -34,7 +34,7 @@ with DAG(
    
     for i in range(20):
         t2 = PythonOperator(
-            task_id='looping python operator_' + str(i),
+            task_id='looping_python_operator_' + str(i),
             python_callable=print_task_number,  
             op_kwargs={'task_number' : i}
         )
