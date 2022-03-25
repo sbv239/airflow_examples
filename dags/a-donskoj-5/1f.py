40c574af991af48d936750a94b972a03e45d5e3e
Andrew Donskow <donskoi.com@gmail.com>
2022-03-25 14:15:35 +0300

    1st dag

--- dags/a-donskoj-5/1f.py
+++ dags/a-donskoj-5/1f.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-        'hw_1_a-donskoj-5',
+        'hw_2_DON',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
