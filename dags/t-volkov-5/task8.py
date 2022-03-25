f9f3dedb6b0c76faab1a85763ebb7d5fe308fbba
Timur Volkov <volkovtimurp@yandex.ru>
2022-03-25 04:01:49 +0300

    add task8.py

---
+++ dags/t-volkov-5/task8.py
@@ -0,0 +1,50 @@
+from datetime import timedelta, datetime
+from textwrap import dedent
+
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}
+
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+with DAG(
+    'hw_8_t-volkov-5',
+    default_args=default_args,
+    description='God bless my creature',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 3, 20),
+    catchup=False
+) as dag:
+
+    def xcom_var_push(ti):
+        ti.xcom_push(
+        key='sample_xcom_key',
+        value='xcom test'
+    )
+
+    def xcom_var_pull(ti):
+        ti.xcom_pull(
+        key='sample_xcom_key',
+        tasks = 'push var into xcom'
+    )
+
+    t1 = PythonOperator(
+        task_id='push var into xcom',
+        python_callable=xcom_var_push
+    )
+
+    t2 = PythonOperator(
+        task_id='pull var from xcom',
+        python_callable=xcom_var_pull
+    )
+    
+    t1>>t2
