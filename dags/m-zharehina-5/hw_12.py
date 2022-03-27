e4ab99d8eef435e83ca6b4ca339baeb74e9410d5
marinazherehina@mail.ru <marinazherehina@mail.ru>
2022-03-28 02:48:02 +0500

    hw___12

--- dags/m-zharehina-5/hw_12.py
+++ dags/m-zharehina-5/hw_12.py
@@ -11,39 +11,43 @@ from airflow.operators.python_operator import PythonOperator
 
 from airflow.providers.postgres.operators.postgres import PostgresHook
 
+###################
 
-def get_branch():
+
+def func():
     from airflow.models import Variable
-    if Variable.get("is_startml"):
-        return "startml_desc"
-    else:
-        return "not_startml_desc"
-    
-    
+    if Variable.get('is_startml'):
+        return 'startml_desc'
+    return 'not_startml_desc'
+
+
 with DAG(
-    'hw_12_m_zharehina_5',
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5), 
-    },
-    description='hw_12_m_zharehina_5',
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2022, 3, 26),
-    catchup=False,
-    tags=['hw_12_m_zharehina_5'],
-    ) as dag:   
+        'hw_12_m_zharehina_5',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),
+        },
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2021, 3, 26),
+        catchup=False,
+) as dag:
     
-    t1 = DummyOperator(task_id='hw_12_m_zharehina_5_t1')
-    t2 = BranchPythonOperator(task_id='hw_12_m_zharehina_5_t2', 
-                              python_callable=get_branch)
-    t3 = BashOperator(task_id='startml_desc', 
-                      bash_command='echo "StartML is a starter course for ambitious people"')
-    t4 = BashOperator(task_id='not_startml_desc', 
-                      bash_command='echo "Not a startML course, sorry"')
-    t5 = DummyOperator(task_id='hw_12_m_zharehina_5_t5')
+    t1 = DummyOperator(task_id='bef_branch',)
+
+    t2 = BranchPythonOperator(task_id='determ',
+                        provide_context=True,
+                        python_callable=func,)
+
+    t3 = BashOperator(task_id='startml_desc',
+                        bash_command='echo "StartML is a starter course for ambitious people"',)
+
+    t4 = BashOperator(task_id='not_startml_desc',
+                        bash_command='echo "Not a startML course, sorry"',)
+
+    t5 = DummyOperator(task_id='aft_branch',)
 
     t1 >> t2 >> [t3, t4] >> t5
