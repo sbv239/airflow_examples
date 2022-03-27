66416a87591bf88a82500d4f4454bd13a8c28f1e
Dmitriy Peskin <peskind@yandex.ru>
2022-03-27 20:03:53 +0300

    DAG for task 1

---
+++ dags/peskind/task_1.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'task_1',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+    },
+    description='DAG for task 1',
+    sheduled_interval=timedelta(days=1),
+    startdate=datetime(2022, 3, 27),
+    catchup=False,
+    tags=['DP HW1']
+) as dag:
+    t1 = BashOperator(task_id = 'Bash operator task', bash_command = 'pwd')
+
+
+    def print_ds(ds):
+        print(ds)
+        return  'Ok'
+
+    t2 = PythonOperator(task_id = 'Python operator task', python_callable = print_ds)
+
+    t1 >> t2
