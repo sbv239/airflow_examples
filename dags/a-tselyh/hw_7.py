44c09d07a0af1ab8289fdc093da4660d063ea566
Anastasia <anansee@gmail.com>
2022-03-29 11:08:00 +0300

    HW_6_a-tselyh

--- dags/a-tselyh/hw_7.py
+++ dags/a-tselyh/hw_7.py
@@ -1,43 +1,34 @@
-"""
-# Foo
-Hello, these are DAG docs.
-"""
-from textwrap import dedent
+from datetime import datetime, timedelta
 
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-from datetime import timedelta, datetime
-
 with DAG(
-    "new_hw7",
+    'oh_my_dag_7',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
-        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        'retry_delay': timedelta(minutes=5),
     },
-    description='oh_my_DAG',
+    description='DAG_for_tselyh_hw_7',
     schedule_interval=timedelta(days=1),
-    start_date=datetime(year=2022, month=3, day=22),
+    start_date=datetime(2022, 3, 18),
     catchup=False,
-    tags=['hw_7'],
+    tags=['step_7'],
 ) as dag:
 
-    def print_context(task_n: int, ts, run_id, **kwargs):
-        print(f'task number is: {task_n}')
+    def print_task_number(task_number, ts, run_id):
+        print(f'task number is: {task_number}')
         print(ts)
         print(run_id)
 
-    for task_n in range(1, 21):
+    for i in range(21):
         t1 = PythonOperator(
-            task_id='task_number_is' + str(i),
-            python_callable=print_context,
-            op_kwargs={'task_number': task_n},
+            task_id='task_number_' + str(i),
+            python_callable=print_task_number,
+            op_kwargs={'task_number': i},
         )
-
         t1
-
