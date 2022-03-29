087c84a46a9e124599f4d6213e01b1c5b44c0aec
Anastasia <anansee@gmail.com>
2022-03-29 12:51:12 +0300

    hw-8-ats

--- dags/a-tselyh/hw_7.py
+++ dags/a-tselyh/hw_7.py
@@ -1,56 +1,34 @@
+from datetime import datetime, timedelta
+
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-from datetime import timedelta, datetime
-
-
-def print_context(ts, run_id,**kwargs,):
-    print("task number is: {kwargs['task_number']}")
-    print(ts)
-    print(run_id))
-
-  templated_command = dedent(
-        """
-    {% for i in range({NUMBER}) %}
-        echo "{{ ts }}"
-    {% endfor %}
-    echo "{{ run_id }}"
-    """
-
 with DAG(
-    "hw_7_a-tselyh",
+    'a-tselyh_hw_6',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
-        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        'retry_delay': timedelta(minutes=5),
     },
-    description='fuck DAG',
+    description='DAG_for_e_bogomolova_step_7',
     schedule_interval=timedelta(days=1),
-    start_date=datetime(year=2022, month=3, day=22),
+    start_date=datetime(2022, 3, 18),
     catchup=False,
-    tags=['hw_7'],
+    tags=['step_7'],
 ) as dag:
-    t1 = BashOperator(
-    for i in range 10:
-        task_id='templated_' + str(i)',  # id, будет отображаться в интерфейсе
-        bash_command= templated_command,  # какую bash команду выполнить в этом таске
-    )
 
-    t2 = PythonOperator(
-        for i in range 20:
-            task_id="'task_number_' + str(i)",
-            python_callable=print_context,
-            op_kwargs={kwargs['task_number']: i }
-    )
-    t2.doc_md = dedent(
-        """\
-    # Task with PythonOperator
-    This **task** *printing* 10 consecutive numbers
-    in the form `task number is: {task_number}`
+    def print_task_number(task_number, ts, run_id):
+        print(f'task number is: {task_number}')
+        print(ts)
+        print(run_id)
 
-    """
-    t1 >> t2
+    for i in range(21):
+        t1 = PythonOperator(
+            task_id='task_number_is' + str(i),
+            python_callable=print_task_number,
+            op_kwargs={'task_number': i},
+        )
+        t1
