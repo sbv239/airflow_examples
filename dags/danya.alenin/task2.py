7f927d0f9ab2fb6406feb04a9f4faca6bd5fdb2f
Danya Alenin <danya.alenin@gmail.com>
2022-04-03 01:48:23 +0530

    fix danya.alenin/task2.py

--- dags/danya.alenin/task2.py
+++ dags/danya.alenin/task2.py
@@ -4,22 +4,19 @@ from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
 from textwrap import dedent
 
-
-def print_logic_date(ds):
-    print(ds)
-    return 'Lol Lol See The Logs Buddy'
-
-
-with DAG(
-    'first_dag',
-    default_args={
+default_args = {
         'depends_on_past': False,
         'email': ['airflow@example.com'],
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5)
-    },
+    }
+
+
+with DAG(
+    'first_dag',
+    default_args=default_args,
     description='Simple first dag',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 4, 3),
@@ -30,6 +27,11 @@ with DAG(
         task_id='print_directory',
         bash_command='pwd'
     )
+
+
+    def print_logic_date(ds, **kwargs):
+        print(ds)
+
 
     t2 = PythonOperator(
         task_id='print_date',
