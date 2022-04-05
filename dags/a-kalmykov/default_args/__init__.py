73b89bd2cf02a66fa4ebd60de5104960cde26ea9
Андрей Калмыков <voldr4396@yandex.ru>
2022-04-05 20:00:00 +0300

    fix dag 1 args

--- dags/a-kalmykov/default_args/__init__.py
+++ dags/a-kalmykov/default_args/__init__.py
@@ -1,1 +1,10 @@
-from default_args import *
+from datetime import timedelta
+
+default_args = {
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
