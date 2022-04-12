07a158a40c18f050afa0f58e0badecf79d3e1116
mistah-df <dubyago.fyodor@gmail.com>
2022-04-12 21:49:14 +0300

    again

--- dags/f-dubjago-7/f-dubjago-7_dag1.py
+++ dags/f-dubjago-7/f-dubjago-7_dag1.py
@@ -1,23 +1,27 @@
 # Для объявления DAG нужно импортировать класс из airflow
 from airflow import DAG
-from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
-with DAG(
-        'f-dubjago-7_dag1',
-        # Параметры по умолчанию для тасок
-        default_args={
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+# Параметры по умолчанию для тасок
+default_args= {
             'depends_on_past': False,
             'email': ['airflow@example.com'],
             'email_on_failure': False,
             'email_on_retry': False,
             'retries': 1,
             'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-        },
+}
+
+with DAG(
+        'f-dubjago-7_dag1',
+        default_args=default_args,
         tags=['task1']
 ) as dag:
     t = BashOperator(
-        task_id="give_dir",
+        task_id="curr_dir",
         bash_command="pwd ",  # обратите внимание на пробел в конце!
     )
 
