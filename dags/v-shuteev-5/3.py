e90a6729dd39dcb505015de2ae06b66cdda5a099
Local Viacheslav <vshuteev@exapmple.com>
2022-04-12 13:44:42 +0300

    3rd 3

--- dags/v-shuteev-5/3.py
+++ dags/v-shuteev-5/3.py
@@ -53,7 +53,7 @@ with DAG(
             """\
         #### Task Documentation
         You can document your task using the attributes `doc_md` (markdown),
-        `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+        `doc2` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
         rendered in the UI's Task Instance Details page.
         ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
 
