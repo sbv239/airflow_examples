403a4e54ecb24c6cc32669005a8f7d63710f58a7
Григорий Шевердин <grigory.sheverdin@gmail.com>
2022-04-13 20:39:53 +0300

    hw_3_g-sheverdin-7.py

--- dags/g-sheverdin-7/hw_3_g-sheverdin-7.py
+++ dags/g-sheverdin-7/hw_3_g-sheverdin-7.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 with DAG(
-    'g-sheverdin-7_task02',
+    'g-sheverdin-7_task03',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -14,11 +14,11 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
-    description='g-sheverdin-7_DAG_task02',
+    description='g-sheverdin-7_DAG_task03',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 4, 11),
     catchup=False,
-    tags=['g-sheverdin-7-task02'],
+    tags=['g-sheverdin-7-task03'],
 ) as dag:
 
     for i in range(1, 11):
