e82013298cd3e94d3646217300aa8e1abb68e53e
Григорий Шевердин <grigory.sheverdin@gmail.com>
2022-04-13 23:18:58 +0300

    hw_12_g-sheverdin-7.py

--- dags/g-sheverdin-7/hw_12_g-sheverdin-7.py
+++ dags/g-sheverdin-7/hw_12_g-sheverdin-7.py
@@ -27,34 +27,28 @@ with DAG(
     tags=['g-sheverdin-7-task12'],
 ) as dag:
 
-    def startml_desc():
-        print("StartML is a starter course for ambitious people")
-
-    def not_startml_desc():
-        print("Not a startML course, sorry")
-
-    t1 = PythonOperator(
-        task_id='startml_desc',
-        python_callable=startml_desc,
-    )
-
-    t2 = PythonOperator(
-        task_id='not_startml_desc',
-        python_callable=not_startml_desc,
-    )
-
-    t3 = BranchPythonOperator(
-        task_id='determine_course',
-        python_callable=get_condition,
-        trigger_rule='all_done'
-    )
-
     start = DummyOperator(
-        task_id='before_branching'
+        task_id='before_branching',
     )
 
-    finish = DummyOperator(
-        task_id='after_branching'
+    branch_op = BranchPythonOperator(
+        task_id='determine_course',
+        provide_context=True,
+        python_callable=func,
     )
 
-    start >> t3 >> [t1, t2] >> finish
+    t1 = BashOperator(
+        task_id='startml_desc',
+        bash_command='echo "StartML is a starter course for ambitious people"',
+    )
+
+    t2 = BashOperator(
+        task_id='not_startml_desc',
+        bash_command='echo "Not a startML course, sorry"',
+    )
+
+    end = DummyOperator(
+        task_id='after_branching',
+    )
+
+    start >> branch_op >> [t1, t2] >> end
