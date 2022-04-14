9cd183cc80b950a30f8e2e55d7f8011a6b0fe4d2
Григорий Шевердин <grigory.sheverdin@gmail.com>
2022-04-14 18:48:46 +0300

    hw_5_g-sheverdin-7.py

--- dags/g-sheverdin-7/hw_5_g-sheverdin-7.py
+++ dags/g-sheverdin-7/hw_5_g-sheverdin-7.py
@@ -1,39 +1,42 @@
 import os
 from datetime import datetime, timedelta
+from textwrap import dedent
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'g-sheverdin-7_task05',
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_retry': False,
-        'email_on_failure': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-    },
-    description='g-sheverdin-7_DAG_task05',
-    start_date=datetime(2022, 2, 10),
-    catchup=False,
-    tags=['g-sheverdin-7-task05'],
+        'g-sheverdin-7_DAG_task05',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='DAG in 4 step',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 4, 11),
+        catchup=False,
+        tags=['g-sheverdin-7_task05'],
 ) as dag:
-    def print_task(num):
+
+    def func(num):
         print(f"task number is: {num}")
 
-    for i in range(30):
-        if i < 10:
+    for i in range(31):
+        if i <= 10:
             os.environ['NUMBER'] = str(i)
-            bash_op = BashOperator(
-                task_id='print_task'+str(i),
-                bash_command="echo '{}'".format(os.environ['NUMBER'])
+            t1 = BashOperator(
+                task_id='print_'+str(i),
+                bash_command=f"echo $NUMBER"
             )
         else:
-            python_op = PythonOperator(
+            t2 = PythonOperator(
                 task_id='task_number_' + str(i),
-                python_callable=print_task,
+                python_callable=func,
                 op_kwargs={'num': i}
             )
 
-    bash_op >> python_op
+    t1 >> t2
