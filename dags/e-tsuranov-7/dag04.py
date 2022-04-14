238833354973c4940c7df058b5030e62fea85309
Эдуард Цуранов <tsuranov@mail.ru>
2022-04-14 22:42:14 +1000

    dag04.py

--- dags/e-tsuranov-7/dag04.py
+++ dags/e-tsuranov-7/dag04.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'dag01',
+    'dag04',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -18,39 +18,22 @@ with DAG(
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 4, 13),
     catchup=False,
-    tags=['tags'],
+    tags=['dag04'],
 ) as dag:
 
-    for i in range(10):
-        t1 = BashOperator(task_id=f'BashOperator_{i+1}', bash_command=f"echo {i}")
+    templated_command = dedent(
+    """
+    {% for i in range(5) %}
+        echo "{{ ts }}"
+    {% endfor %}
+        echo "{{ run_id }}"
+    """
+    )  # поддерживается шаблонизация через Jinja
 
-        t1.doc_md = dedent(
-        """
-        #### Task Documentation (BashOperator)
-        элементы кода (заключены в кавычки `code`),
-        **полужирный**, *курсив*
-        # обзац (неполный)
-        """
-        )  # dedent - это особенность Airflow, в него нужно оборачивать всю доку
-        
-    def print_2(task_number):
-        print(f"task number is: {task_number}")
-
-    for i in range(11,31):
-        t2 = PythonOperator(task_id=f'PythonOperator_{i}', python_callable=print_2, op_kwargs={'task_number': i})
-
-        t2.doc_md = dedent(
-        """
-        #### Task Documentation (PythonOperator)
-        элементы кода (заключены в кавычки `code`),
-        **полужирный**, *курсив*
-        # обзац (неполный)
-        """
-        )  # dedent - это особенность Airflow, в него нужно оборачивать всю доку
+    t1 = BashOperator(
+        task_id='templated',
+        depends_on_past=False,
+        bash_command=templated_command,
+    )
     
-    t1 >> t2
-
-    # будет выглядеть вот так
-    #      -> t2
-    #  t1 | 
-    #      -> t3
+    t1
