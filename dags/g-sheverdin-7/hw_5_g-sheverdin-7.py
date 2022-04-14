750eb1c9044fc510f6519c3ff19f6b40d270975b
Григорий Шевердин <grigory.sheverdin@gmail.com>
2022-04-14 18:54:08 +0300

    hw_5_g-sheverdin-7.py

--- dags/g-sheverdin-7/hw_5_g-sheverdin-7.py
+++ dags/g-sheverdin-7/hw_5_g-sheverdin-7.py
@@ -1,42 +1,43 @@
-import os
 from datetime import datetime, timedelta
 from textwrap import dedent
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
+import os
 
 with DAG(
-        'g-sheverdin-7_DAG_task05',
-        default_args={
-            'depends_on_past': False,
-            'email': ['airflow@example.com'],
-            'email_on_failure': False,
-            'email_on_retry': False,
-            'retries': 1,
-            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-        },
-        description='DAG in 4 step',
-        schedule_interval=timedelta(days=1),
-        start_date=datetime(2022, 4, 11),
-        catchup=False,
-        tags=['g-sheverdin-7_task05'],
+    'g-sheverdin-7_task02',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='g-sheverdin-7_DAG_task02',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 4, 11),
+    catchup=False,
+    tags=['g-sheverdin-7-task02'],
 ) as dag:
 
-    def func(num):
-        print(f"task number is: {num}")
+    for i in range(1, 11):
+        os.environ['NUMBER'] = str(i)
+        t1 = BashOperator(
+            task_id='task_number' +str(i),
+            bash_command= f'echo $NUMBER'
+        )
 
-    for i in range(31):
-        if i <= 10:
-            os.environ['NUMBER'] = str(i)
-            t1 = BashOperator(
-                task_id='print_'+str(i),
-                bash_command=f"echo $NUMBER"
-            )
-        else:
-            t2 = PythonOperator(
-                task_id='task_number_' + str(i),
-                python_callable=func,
-                op_kwargs={'num': i}
-            )
+    def task_number(task_number):
+        print(f'task number is: {task_number}')
+        return None
+
+    for i in range(11, 31):
+        t2 = PythonOperator(
+            task_id='task_number' + str(i),
+            python_callable=task_number,
+            op_kwargs={'task_number': i}
+        )
 
     t1 >> t2
