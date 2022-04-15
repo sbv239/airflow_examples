f130cecb62137805f2b22f6b48844f13e7c193e2
Maksim Valishevskii <valishevsky.max@gmail.com>
2022-04-15 20:43:40 +0200

    hw_1_m-valishevskij-7

--- dags/m-valishevskij-7/hw_1_m-valishevskij-7.py
+++ dags/m-valishevskij-7/hw_1_m-valishevskij-7.py
@@ -1,9 +1,22 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from datetime import timedelta, datetime
 
 with DAG(
-        'hw_1_m-valishevskij-7'
+        'hw_1_m-valishevskij-7',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 4, 21),
+        catchup=False,
+        tags=['valishevskij']
 ) as dag:
     t1 = BashOperator(
         task_id='hw_1_m-valishevskij-7_1',
