35cbbfdd545e824a3d729cae43f136e371d5e96c
b33ckham <a.syryamin@gmail.com>
2022-04-23 12:33:35 +0700

    commit zad 3

---
+++ dags/a.syrjamin-15/task2.py
@@ -0,0 +1,39 @@
+
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'blabla_2',
+
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='harder DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    def print_context(random_base, **kwargs):
+        print(f'task number is: {random_base}')
+
+    for i in range(10):
+        t1 = BashOperator(
+            task_id=f'buya_{i}',
+            bash_command=f"echo {i}")
+
+    for i in range(20):
+        t2 = PythonOperator(
+            task_id='print_' + str(i),
+            python_callable=print_context,
+            op_kwargs={'random_base': i},
+            )
+
+    t1 >> t2
