6af75756a01d4bbc2b0fdc1c1bad40e56c72f767
Sergey Filkin <fserg@MacBookFsM1.local>
2022-04-24 13:32:59 +0300

    hw_6_s-filkin-7

--- dags/s-filkin-7/dag6.py
+++ dags/s-filkin-7/dag6.py
@@ -12,7 +12,7 @@ def python_task(task_number, ts, run_id):
     return 'ok'
 
 with DAG(
-    'dag4',
+    's-filkin-7-dag6',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
