f38300cf63cb3b4131ca4551bb791ed2150bdbc1
Roman Baldaev <baldaev.roman@gmail.com>
2022-04-25 00:54:53 +0700

    Lesson 11 - Task 5 (fix) - Roman Baldaev

--- dags/r.baldaev-1/task_5.py
+++ dags/r.baldaev-1/task_5.py
@@ -36,7 +36,10 @@ with DAG(
         os.putenv('NUMBER', str(i))
         task = BashOperator(
             task_id=f'echo_task_{i}',
-            bash_command=f'NUMBER={i} & echo $NUMBER',
+            bash_command=f'echo $NUMBER',
+            env={
+                "NUMBER": str(i),
+            },
         )
         if curr_task:
             curr_task >> task
