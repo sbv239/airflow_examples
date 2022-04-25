77c475373b40fe417eb76e857db829a24d0ce8c1
Aygr <aygr@yandex.ru>
2022-04-25 18:14:48 +0300

    task_5

--- dags/a-gordin-7/task_5.py
+++ dags/a-gordin-7/task_5.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta
-
+import os
 #чтоб работать с DAG импортируем класс
 from airflow import DAG
 
@@ -8,7 +8,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'a.gordin_task_2',
+    'a.gordin_task_5',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -18,7 +18,7 @@ with DAG(
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
 # Описание DAG (не тасок, а самого DAG)
-    description='A simple tutorial DAG',
+    description='a.gordin_task_5',
     # Как часто запускать DAG
     schedule_interval=timedelta(days=1),
     # С какой даты начать запускать DAG
@@ -30,23 +30,13 @@ with DAG(
     # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
     catchup=False,
     # теги, способ помечать даги
-    tags=['example'],
+    tags=['a.gordin_task_5'],
 ) as dag:
     for i in range(10):
+
         t1 = BashOperator(
-            task_id='print'+str(i),  # id в интерфейсе
-            bash_command=f"echo {i}",  # выполнение команды
+            task_id='t1_' + str(i),
+            bash_command="echo $NUMBER",
+            env={"NUMBER": i}
         )
-    def print_context(task_number):
-        # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
-        print("task number is: {task_number}")
-        return print
-
-    for i in range(20):
-        t2 = PythonOperator(
-            task_id='print_the_context' + str(i),  # нужен task_id, как и всем операторам
-            python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
-            op_kwargs={'task_number': i}
-        )
-
-    t1 >> t2
+    t1
