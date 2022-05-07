8745df1d4678f572cc3d13f479eea9f9693a1853
Анна Ушакова <basket96@bk.ru>
2022-05-07 22:46:13 +0300

    update

--- dags/a-ushakova-7/hw_2.py
+++ dags/a-ushakova-7/hw_2.py
@@ -28,19 +28,14 @@ with DAG('AUshakova_HW_2',
     catchup=False,
     tags=['hw_2'], ) as dag:
 
-    templated_command = dedent(
-        """
-        f"echo {i}"
-    """
-
     for i in range(10):
         task_id = BashOperator(task_id='templated_command_' + str(i),  # id, будет отображаться в интерфейсе
-                      bash_command = templated_command,  # какую bash команду выполнить в этом таске
-                                        )
+                               bash_command = f"echo {i}",  # какую bash команду выполнить в этом таске
+                                )
         task_id
 
-    def print_number(number):
-        print("task number is: {number}".format(number))
+    def print_number(task_number):
+        print("task number is: {number}".format(task_number))
 
     for i in range(20):
     # Каждый таск будет спать некое количество секунд
@@ -48,7 +43,7 @@ with DAG('AUshakova_HW_2',
             task_id='number_' + str(i),  # в id можно делать все, что разрешают строки в python
             python_callable = print_number,
             # передаем в аргумент с названием random_base значение float(i) / 10
-            op_kwargs={'number': int(i)},
+            op_kwargs={'task_number': int(i)},
             )
     # настраиваем зависимости между задачами
     # run_this - это некий таск, объявленный ранее (в этом примере не объявлен)
