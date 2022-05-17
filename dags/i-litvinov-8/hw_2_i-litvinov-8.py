9631f1238860a182598bbdcfbb9fcad2e2704699
Ivan Litvinov <IvanLitvinov1357@yandex.ru>
2022-05-17 11:59:09 +0300

    Task 2 Ivan Litvinov commit

---
+++ dags/i-litvinov-8/hw_2_i-litvinov-8.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import date, timedelta, datetime
+
+
+def task_id_print(task_number):
+    return f"task number is: {task_number}"
+
+
+with DAG(
+        'hw_2_i-litvinov-8',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='2_Task_DAG_i-litvinov-8',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 5, 16),
+        catchup=False,
+        tags=['i-litvinov-8']
+) as dag:
+
+    for i in range(30):
+        if i < 10:
+            t = BashOperator(
+                    task_id=f'task_{i}',
+                    bash_command=f"echo {i}",
+                    dag=dag
+            )
+        else:
+            t = PythonOperator(
+                    task_id=f'task_{i}',
+                    python_callable=task_id_print,
+                    op_kwargs={'task_number': i}
+            )
