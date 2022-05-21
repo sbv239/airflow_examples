926cb997d596569224e69c2b7720bca581aeca9d
Nadezhda Zolotareva <nadzolot1@roregroup.net>
2022-05-22 01:41:34 +0300

    DAGs with markdown and templates

--- dags/n-dmitrieva/hw_3_n-dmitrieva_DAG.py
+++ dags/n-dmitrieva/hw_3_n-dmitrieva_DAG.py
@@ -1,12 +1,12 @@
+
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
 from textwrap import dedent
 from datetime import timedelta, datetime
 
 
 '''
-Second DAG documentation
+Forth DAG documentation
 
 '''
 
@@ -26,7 +26,7 @@ with DAG(
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
     # Описание DAG (не тасок, а самого DAG)
-    description='Second exersize',
+    description='Forth exersize',
     # Как часто запускать DAG
     schedule_interval=timedelta(days=1),
     # С какой даты начать запускать DAG
@@ -41,32 +41,19 @@ with DAG(
     tags=['NDmitrieva'],
 ) as dag:
 
-    def print_task_number(task_number):
-        print(f'task number is: {task_number}')
-
-    for i in range(30):
-
-        if i<10: 
-            Btask1 = BashOperator(
-                task_id = 'echo' + str(i), # id, будет отображаться в интерфейсе
-                bash_command = f"echo {i}",
-            )# какую bash команду выполнить в этом таске
-            Btask1.doc_md = dedent(
-            f"""\
-            #Task Documentation
-            'f"echo {i}"' 
-            you will see 
-            *iteration* of _first 10_ tasks of **BashOperator** type""")
+    templated_command = dedent( 
+        """
+    {% for i in range(5) %}
+        echo "{{ ts }}"
+    {% endfor %}
+    echo "{{ run_id }}"    
+        """
+    )
     
-        else: 
-            Ptask2 = PythonOperator(
-                task_id = f'print_task_number_{i}', # в id можно делать все, что разрешают строки в python
-                python_callable = print_task_number,  # свойственен только для PythonOperator - передаем саму функцию
-                op_kwargs={'task_number': i},  # передаем в аргумент с названием task_number и задействуем переменную из цикла
-            )
-            Ptask2.doc_md = dedent("""
-            # Task_Documentation 
-            **print** `"task number is: {task_number}"`, where `i` *iteration number*
-            using  PythonOperator
-            """)
-    Btask1 >> Ptask2
+    
+    task = BashOperator(
+            task_id = 'Jinja_trying',# id, будет отображаться в интерфейсе
+            depends_on_past=False, 
+            bash_command = templated_command,
+    )
+    task
