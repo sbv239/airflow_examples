0e70f8c792492645e01e563ebe963294c9462b25
Алексей Малахов <malaxova0403@yandex.ru>
2022-05-24 21:47:02 +0300

    add 12task

--- dags/a-malahov/12task.py
+++ dags/a-malahov/12task.py
@@ -2,10 +2,12 @@ from datetime import datetime, timedelta
 from textwrap import dedent
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import BranchPythonOperator
 from airflow.models import Variable
 
 with DAG(
-        'a-malahov_task11',
+        'a-malahov_task12',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -14,19 +16,45 @@ with DAG(
             'retries': 1,
             'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
         },
-        description='a-malachov task 11',
+        description='a-malachov task 12',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2022, 5, 10),
         catchup=False,
         tags=['malahov'],
 ) as dag:
-    def get_variables():
-        print(Variable.get("is_startml"))
 
+    def print_start_ml():
+        print('StartML is a starter course for ambitious people')
 
-    variables_get = PythonOperator(
-        task_id='variables_id',
-        python_callable=get_variables,
+    def print_not_start_ml():
+        print('Not a startML course, sorry')
+
+    def get_variables_start_ml():
+        if Variable.get("is_startml") == 'True':
+            return 'startml_desc'
+        return 'not_startml_desc'
+
+    branching = BranchPythonOperator(
+        task_id='branching',
+        python_callable=get_variables_start_ml ,
     )
 
-    variables_get
+    start_ml = PythonOperator(
+        task_id='startml_desc',
+        python_callable=print_start_ml ,
+    )
+
+    not_start_ml = PythonOperator(
+        task_id='not_startml_desc',
+        python_callable=print_not_start_ml ,
+    )
+
+    before_branching = DummyOperator(
+        task_id='before_branching',
+    )
+
+    after_branching = DummyOperator(
+        task_id='after_branching',
+    )
+
+    before_branching >> branching >> [start_ml, not_start_ml] >> after_branching
