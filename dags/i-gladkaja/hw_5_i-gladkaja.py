15d5acb7dddee098b2d788d7a6ad7a9d960f45f7
Gladkaya Irina <gladkyir@gmail.com>
2022-05-28 17:15:28 +0300

    hw_5_gladkaja correct

--- dags/i-gladkaja/hw_5_i-gladkaja.py
+++ dags/i-gladkaja/hw_5_i-gladkaja.py
@@ -16,7 +16,7 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-with DAG('gladkaja_hw_2',
+with DAG('gladkaja_hw_5',
          default_args=default_args,
          description='A simple tutorial DAG',
          schedule_interval=timedelta(days=1),
