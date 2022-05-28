e10922cfa12cf1fed5bd2227e8cddb9a3bd097ed
Svetlana Lomp <svetlana.lomp@gmail.com>
2022-05-28 21:06:37 +0300

    task1

---
+++ dags/s.lomp-6/task_2.py
@@ -0,0 +1,56 @@
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+
+
+
+def print_context(ds, **kwargs):
+    """Пример PythonOperator"""
+    # Через синтаксис **kwargs можно получить словарь
+    # с настройками Airflow. Значения оттуда могут пригодиться.
+    # Пока нам не нужно
+
+    # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
+    print(ds)
+    print(kwargs)
+    return 'Whatever you return gets printed in the logs'
+
+
+
+
+
+with DAG(
+    'task_2',
+    # Параметры по умолчанию для тасок
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    # Описание DAG (не тасок, а самого DAG)
+    description='A simple tutorial DAG',
+    # Как часто запускать DAG
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    t1 = BashOperator(
+        task_id='print_date',  # id, будет отображаться в интерфейсе
+        bash_command='pwd',  # какую bash команду выполнить в этом таске
+    )
+
+    run_this = PythonOperator(
+    task_id = 'print_the_context',  # нужен task_id, как и всем операторам
+    python_callable = print_context,  # свойственен только для PythonOperator - передаем саму функцию
+    )
+
+
+    t1 >> run_this
