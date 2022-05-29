0ae70488552bf77e9535ba600a6fe4299a85491b
Алексей Малахов <malaxova0403@yandex.ru>
2022-05-29 10:33:07 +0300

    correct dag lambda hw4

--- dags/a-malahov/4task.py
+++ dags/a-malahov/4task.py
@@ -57,7 +57,7 @@ with DAG(
         task_id='after_branching',
         python_callable=lambda: print('after_branching') ,
     )
-    #проверка lambda
+    #проверка lambda hw_4
 
 
     before_branching >> branching >> [start_ml, not_start_ml] >> after_branching
