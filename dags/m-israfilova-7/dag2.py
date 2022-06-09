b61d01ecdc826cf2fd05e1f2f747f0cfe6f71e8b
israfilovamaria <israfilova.m8@gmail.com>
2022-06-09 18:25:55 +0300

    HW 4 m-israfilova-7

--- dags/m-israfilova-7/dag2.py
+++ dags/m-israfilova-7/dag2.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 with DAG(
-    'myseconddag',
+    'mythirddag',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
