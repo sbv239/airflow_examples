fbd13256b0c0a5c1af39d7fe7b68a54d258ff9d1
Dmitry Lobachev <dmitry232323@gmail.com>
2022-06-15 18:35:53 +0300

    step_4

--- dags/d-lobachev/step_3_4.py
+++ dags/d-lobachev/step_3_4.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 
 with DAG(
-        'HW_3_d-lobachev_Dynamic_task',
+        'HW_4_d-lobachev_Dynamic_task',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -15,11 +15,11 @@ with DAG(
             'retries': 1,
             'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
         },
-        description='DAG step 3 of HW Lesson 11',
+        description='DAG step 4 of HW Lesson 11',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2022, 2, 10),
         catchup=False,
-        tags=['second'],
+        tags=['lobachev'],
 ) as dag:
     def print_task(task_number):
         print(f'task number is: {task_number}')
@@ -31,11 +31,25 @@ with DAG(
                 task_id='print_task' + str(i),
                 bash_command=f'echo {i}'
             )
+            bash_op.doc_md = dedent(
+                """\
+                ### `BashOperator`
+                Printing **number** of _10_ commands
+                with *echo* command in cycle
+                """
+            )
         else:
             python_op = PythonOperator(
                 task_id='task_number_' + str(i),
                 python_callable=print_task,
                 op_kwargs={'task_number': i}
+            )
+            python_op.doc_md = dedent(
+                """\
+                ### `PythonOperator`
+                Printing **number** of c_20_ commands
+                with *python* func in cycle
+                """
             )
 
     bash_op >> python_op
