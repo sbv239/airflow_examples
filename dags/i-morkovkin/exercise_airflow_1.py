4ecb10b380f4fafbbb179bdd78cd54b665e183d9
Igor Morkovkin <morkovkinigor@gmail.com>
2022-06-16 01:25:38 +0300

    DAG 1

---
+++ dags/i-morkovkin/exercise_airflow_1.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'exercise 1',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='Exercise No.1'
+) as dag:
+
+    def print_ds(ds):
+        print(ds)
+        return 'First python operator successfully completed'
+
+    t1 = PythonOperator(
+        task_id='pyth_1',
+        python_callable=print_ds
+    )
+
+    t2 = BashOperator(
+        task_id='bash_1',
+        bash_command='pwd'
+    )
+
+    t2 >> t1
