675afd9eeef798e45756e64273f0b254223b9d40
Igor Morkovkin <morkovkinigor@gmail.com>
2022-06-16 01:39:05 +0300

    litle chngings

--- dags/i-morkovkin/exercise_airflow_1.py
+++ dags/i-morkovkin/exercise_airflow_1.py
@@ -13,7 +13,8 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
-    description='Exercise No.1'
+    description='Exercise No.1',
+    start_date=datetime('2022-06-16')
 ) as dag:
 
     def print_ds(ds):
