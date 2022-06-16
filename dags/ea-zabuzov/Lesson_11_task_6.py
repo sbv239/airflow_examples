bf796a412481958224bbf499efd8aa3739711f29
Eugene Z <eugene.zabuzov@gmail.com>
2022-06-16 15:21:09 +0500

    Lesson_11_step_6

--- dags/ea-zabuzov/Lesson_11_task_6.py
+++ dags/ea-zabuzov/Lesson_11_task_6.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-        'Lesson_11_step_2',
+        'Lesson_11_step_6',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
