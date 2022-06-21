25dcc32f6f3dc3bcbfd33675a8c9ce1b09d3106f
Borislav Zubanov <borislavzubanov@gmail.com>
2022-06-21 18:31:55 +0500

    rework

--- dags/b-zubanov/hw3.py
+++ dags/b-zubanov/hw3.py
@@ -12,7 +12,7 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 with DAG(
-    'zubanovtest_4',
+    'zubanov_dag',
     # Параметры по умолчанию для тасок
     default_args={
         # Если прошлые запуски упали, надо ли ждать их успеха
