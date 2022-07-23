369ebbc8919e559ca6e66590bcce44bbe65db023
m-gogin <gfhjkm131196@gmail.com>
2022-07-23 19:55:17 +0300

    add dag

--- dags/m-gogin/11_3.py
+++ dags/m-gogin/11_3.py
@@ -7,7 +7,7 @@ from airflow.operators.python import PythonOperator
 
 
 with DAG(
-    '11_3',
+    'hw_3_m-gogin',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -16,7 +16,7 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
-    description='11_2',
+    description='11_3',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 7, 20),
     catchup=False,
@@ -45,8 +45,6 @@ with DAG(
             op_kwargs={'task_number': 3},
         )
     t1 >> t2
-
-
 
 
 
