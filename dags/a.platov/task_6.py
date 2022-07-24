57954e3d997c739c2ba05b44c19bbd2fc04723f9
Антон Платов <platovmm@mail.ru>
2022-07-24 10:20:56 +0300

    HW 7 a.platob

--- dags/a.platov/task_6.py
+++ dags/a.platov/task_6.py
@@ -19,27 +19,40 @@ with DAG(
         description='First task',
         start_date=datetime(2022, 7, 17),
         catchup=False,
-        tags=['first_task'],
+        tags=['a.platov'],
     ) as dag:
         
-    def print_data(ts, run_id, **kwargs):
-        task_number = kwarg["task_number"]
-        print(f'{ts}')
-        print(f'{run_id}')
+    def print_date(ts, run_id, **kwargs):
+        print(ts, run_id)
         print(f'task number: {task_number}')
+    
+    date = "{{ ds }}"
 
-    for task_number in range(10):
-        t_bash = BashOperator(
-                    task_id='run_bush_op_'+str(task_number),  # id, будет отображаться в интерфейсе
+    def t_bash(task_number: int, date):
+        return  BashOperator(
+                task_id='run_bush_op_'+str(task_number),  # id, будет отображаться в интерфейсе
                 bash_command='echo $NUMBER',
-                env={"NUMBER": str(task_number)},
-                dag=dag,
-            )
-        
-    for task_number in range(20):
-        t_python = PythonOperator(
+                env={"NUMBER": str(task_number), "DATA START": date},
+                dag=dag,)
+
+    def t_python(task_number: int, func):
+        return PythonOperator(
                 task_id='run_python_op_'+str(task_number),
-                op_kwargs={"task_number", task_number},
-                python_callable=print_data,
+                op_kwargs={"task_number": task_number,
+                           'ts': "{{ ts }}",
+                           'rub_id': "{{ run_id }}"},
+                python_callable=func,
             )
-    t_bash >> t_python
+
+    for task_number in range(30):
+        if task_manager == 0:
+            t = t_bash(task_number, date)
+        if task_manager < 10:
+            new_task = t_bash(task_number, date)
+            t >> new_task
+            t = new_task
+        else:
+            new_task = t_python(task_number, print_date)
+            t >> new_task
+            t = new_task
+
