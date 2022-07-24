e0bf7f7ce6a69192b0d86750d67cb5e294acb936
Антон Платов <platovmm@mail.ru>
2022-07-24 11:08:02 +0300

    HW 7 a.platov

--- dags/a.platov/task_6.py
+++ dags/a.platov/task_6.py
@@ -17,31 +17,31 @@ with DAG(
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
         },
         description='First task',
-        start_date=datetime(2022, 7, 17),
+        start_date=datetime(2022, 7, 24),
         catchup=False,
         tags=['a.platov'],
     ) as dag:
         
-    def print_date(task_number, ts, run_id):
-        print(ts)
-        print(run_id)
-        print('task number: ', task_number)
+        def print_date(task_number, ts, run_id):
+            print(ts)
+            print(run_id)
+            print('task number: ', task_number)
     
-    date = "{{ ds }}"
+        date = "{{ ds }}"
 
-    def task_bash(task_number: int, date):
-        return  BashOperator(
-                task_id='run_bush_op_'+str(task_number),                 
-                bash_command='echo $NUMBER',
-                env={"NUMBER": str(task_number), "DATA START": date},
-                dag=dag,)
+        def task_bash(task_number: int, date):
+            return  BashOperator(
+                    task_id='run_bush_op_'+str(task_number),                 
+                    bash_command='echo $NUMBER',
+                    env={"NUMBER": str(task_number), "DATA START": date},
+                    dag=dag,)
 
-    def task_python(task_number: int, func):
-        return PythonOperator(
-                task_id='run_python_op_'+str(task_number),
-                op_kwargs={"task_number": task_number,
-                           'ts': "{{ ts }}",
-                           'rub_id': "{{ run_id }}"},
-                python_callable=func,
-            )
-    t_bash(task_number, date)
+        def task_python(task_number: int, func):
+            return PythonOperator(
+                    task_id='run_python_op_'+str(task_number),
+                    op_kwargs={"task_number": task_number,
+                               'ts': "{{ ts }}",
+                               'rub_id': "{{ run_id }}"},
+                    python_callable=func,
+                    )
+        t_bash(task_number, date) >> t_bash(task_number, date)
