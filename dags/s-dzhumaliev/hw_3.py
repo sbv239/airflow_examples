f5f5fd991b4a9928962c6c5aa8d510ba08f86ee6
Savva Dzhumaliev <savva.dzhumaliev@sbermarket.ru>
2022-08-22 18:16:45 +0400

    hw_3_s-dzhumaliev

---
+++ dags/s-dzhumaliev/hw_3.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+def print_task_number(task_number):
+    print(f"task number is: {task_number}")
+
+with DAG(
+    'HW_3_s-dzhumaliev',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    start_date=datetime(2022, 1, 1),
+) as dag:
+    for i in range(10):
+        BashOperator(
+            task_id=f"echo_{i}",
+            bash_command=f"echo {i}",
+        )
+
+    for i in range(20):
+        PythonOperator(
+            task_id=f'print_task_number_{i}',
+            op_kwargs={'task_number': i},
+            python_callable=print_task_number
+        )
