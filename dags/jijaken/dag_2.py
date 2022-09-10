50ab8aa4c87fc4d485f5da940f94094c9936ac7c
jijaken <jijaken@yandex.ru>
2022-09-10 14:01:24 +0300

    jijaken hw_2

---
+++ dags/jijaken/dag_2.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG('tutorial_dag_2'
+        ,default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}) as dag:
+    t1 = BashOperator(task_id='pwd_bash',
+                      bash_command= 'pwd')\
+    t2 = PythonOperator(task_id='print_python',
+                        python_callable='print(ds)')
+
+
+    t1 >> t2
