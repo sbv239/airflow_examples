43298d2d66198b9a0948a8fa7c74df6120321917
Sergey <sergey@Sergeys-MacBook-Pro.local>
2022-09-12 16:27:57 +0300

    task 2-12 completed++

--- dags/s-duzhak-12/task_9.py
+++ dags/s-duzhak-12/task_9.py
@@ -6,7 +6,7 @@ from textwrap import dedent
 
 
 with DAG(
-        's-duzhak-2-task_8',
+        's-duzhak-2-task_9',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
