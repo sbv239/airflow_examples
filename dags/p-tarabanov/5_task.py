a72fb859769573d44c9affa795f122d82bb19f45
p-tarabanov <pavel55645@gmail.com>
2022-09-14 09:49:08 +0000

    Correction of 5_task.py

--- dags/p-tarabanov/5_task.py
+++ dags/p-tarabanov/5_task.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'hw_4_p-tarabanov',
+    'hw_5_p-tarabanov',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
