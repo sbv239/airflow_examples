2a2f68a3e1ce10514940f7a34978904d8c7a862e
DmitryGrigoriev <grigd2013@gmail.com>
2022-09-19 20:53:49 +0500

    hw_3 10 bash operator

---
+++ dags/d-grigorev/hw_3.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'homework_3',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        start_date=datetime(2022, 1, 1),
+        tags=['hw_3_d-grigorev']
+) as dag:
+    for i in range(10):
+        t1 = BashOperator(
+            task_id=f'task_{i}',
+            bash_command=f'echo {i}',
+            dag=dag
+        )
+
+
+    def print_ds(ds, **kwargs):
+        print(ds)
+        return 'printing ds'
+
+
+    t2 = PythonOperator(
+            task_id='printing_ds',
+            dag=dag,
+            python_callable=print_ds
+    )
+
+t1 >> t2
