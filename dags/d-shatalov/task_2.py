30149c2d3fe0c5e961d7c96d909531e5efd4aac5
Dmitrysw.txt <shatalovdmitrys@gmail.com>
2022-09-21 21:49:41 +0400

    task_2

---
+++ dags/d-shatalov/task_2.py
@@ -0,0 +1,34 @@
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'tutorial',
+        default_args={
+            'depands_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),
+        },
+        description="task_2",
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 9, 21),  # логическая дата
+        catchup=False,
+) as dag:
+
+    t1 = BashOperator(
+        task_id='print_dir',
+        bash_command='pwd')
+
+    def print_contex(ds):
+        print(ds)
+        return "whatever"
+    t2 = PythonOperator(
+    task_id='print_date',
+    python_callable=print_contex)
+
+t1 >> t2
