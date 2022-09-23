9c5db8fb50a77f5e54ce122a68123d0e0204b09a
shchipts <shchipts@iiasa.ac.at>
2022-09-23 15:49:44 +0200

    hw-8 print

--- dags/a-schiptsova-5/8.py
+++ dags/a-schiptsova-5/8.py
@@ -9,7 +9,7 @@ def push_xcom(ti):
 	ti.xcom_push(key = 'sample_xcom_key', value = 'xcom test')
 	
 def pull_xcom(ti):
-	ti.xcom_pull(key = 'sample_xcom_key', task_ids = 'push')
+	print(ti.xcom_pull(key = 'sample_xcom_key', task_ids = 'push'))
 	
 
 with DAG(
