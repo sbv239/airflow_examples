31af0cfef70b1e627bef287d92aff1754cec2325
n-murakami <murakami.nao13@gmail.com>
2022-10-03 12:35:42 +0300

    add changes

--- dags/n-murakami/hw_13_n-murakami.py
+++ dags/n-murakami/hw_13_n-murakami.py
@@ -1,15 +1,24 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.operators.python import BranchPythonOperator
+from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
 
-def get_var():
+def choose_path(your_var):
     from airflow.models import Variable
-    var = Variable.get("is_startml")
-    print(var)
+    if Variable.get("is_startml")=="True":
+        return "startml_desc"
+    else:
+        return "not_startml_desc"
+def print_if_true():
+    return "StartML is a starter course for ambitious people"
+
+def print_if_false():
+    return "Not a startML course, sorry"
 
 
 with DAG(
-        'hw_12_n-murakami',
+        'hw_13_n-murakami',
         default_args={
           'depends_on_past': False,
           'retries': 3,
@@ -18,11 +27,27 @@ with DAG(
         schedule_interval=timedelta(days=1),
         start_date=datetime(2022, 1, 1),
         catchup=False,
-        tags=['hw_12_n-murakami']
+        tags=['hw_13_n-murakami']
 ) as dag:
-  t= PythonOperator(
-    task_id="print_var",
-    python_callable=get_var
-  )
+    decision = BranchPythonOperator(
+        task_id="decide_path",
+        python_callable=choose_path
+    )
+    dummy_start= DummyOperator(
+        task_id="start_graph"
+    )
+    dummy_end= DummyOperator(
+        task_id="end_graph"
+    )
+    if_true = PythonOperator(
+        task_id="startml_desc",
+        python_callable=print_if_true
+    )
+    if_false = PythonOperator(
+        task_id="not_startml_desc",
+        python_callable=print_if_false
+    )
+    dummy_start >> decision >> [if_true, if_false] >> dummy_end
+
 
 
