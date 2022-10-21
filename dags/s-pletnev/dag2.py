ecdd29e97f369473c8e049a5c53b35cd4683e713
Sergey Pletnev <_serge@tut.by>
2022-10-22 00:36:32 +0300

    test

--- dags/s-pletnev/dag2.py
+++ dags/s-pletnev/dag2.py
@@ -1,7 +1,8 @@
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from datetime import datetime, timedelta
+
 
 with DAG(
         's-pletnev_task_2',
@@ -13,18 +14,19 @@ with DAG(
             'retries': 1,
             'retry_delay': timedelta(minutes=5),
         },
-        description='task_2 dag',
+        description='task_2_dag',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2022, 10, 21),
         catchup=False,
         tags=['task_2'],
 ) as dag:
     task_1 = BashOperator(
-        task_id="bash pwd",
+        task_id="bash_pwd",
         bash_command="pwd"
     )
 
-    def print_ds(ds):
+
+    def print_ds(ds, **kwargs):
         print(ds)
         print("ds printed")
 
