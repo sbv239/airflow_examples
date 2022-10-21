da69caf13d40e31dacb3f937a906e59097ed14cb
Roman <romanov_mr08@mail.ru>
2022-10-22 00:05:02 +0300

    new_commit

--- dags/r-romanov/main.py
+++ dags/r-romanov/main.py
@@ -1,12 +1,13 @@
 from airflow.operators.bash import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from airflow import DAG
-from datetime import datetime, timedelta
+from datetime import timedelta,datetime
 
 
 
 
 with DAG(
+    'hw_1 r-romanov',
 default_args={
     'depends_on_past': False,
     'email': ['airflow@example.com'],
@@ -14,7 +15,15 @@ default_args={
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-}) as dag:
+},
+description='A simple tutorial DAG',
+schedule_interval=timedelta(days=1),
+start_date=datetime(2022, 1, 1),
+
+catchup=False,
+# теги, способ помечать даги
+tags=['example'],
+) as dag:
 
     t1 = BashOperator(
         task_id = 'id_1',
@@ -22,13 +31,14 @@ default_args={
     )
 
     def print_context(ds, **kwargs):
-        print(kwargs)
         print(ds)
+        print(kwargs)
+
         return 'Whatever you return gets printed in the logs'
 
     t2 = PythonOperator(
         task_id = 'id_2',
-        python_callable = print_context,
+        python_callable = print_context
     )
 
     t1 >> t2
