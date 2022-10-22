3d808754727e5eaa696b36b333be62b7cbbeae1b
danru00@mail.ru <danru00@mail.ru>
2022-10-22 14:13:16 +0300

    i hope it finall chanche for task_2

--- dags/d-rusin/script1_1.py
+++ dags/d-rusin/script1_1.py
@@ -1,9 +1,11 @@
-from airflow.operators.bash import BashOperator
-from datetime import timedelta,datetime
 from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
+
 def get_ds(ds):
     print(ds)
+    print("super cool####")
 with DAG("firsttask",default_args={
     'depends_on_past': False,
     'email': ['airflow@example.com'],
@@ -13,10 +15,9 @@ with DAG("firsttask",default_args={
     'retry_delay': timedelta(minutes=5),},
     description='creating first DAG',
     schedule_interval=timedelta(days=1),
-    start_date=datetime(2022, 10, 21),
+    start_date=datetime(2022, 10, 20),
     catchup=False,
     tags=['task_two'],) as dag:
-    # t1, t2, t3 - это операторы (они формируют таски, а таски формируют даг)
     t1 = BashOperator(
         task_id='print_pwd1',
         bash_command='pwd')
@@ -24,4 +25,6 @@ with DAG("firsttask",default_args={
     t2 = PythonOperator(
         task_id='print_ds1',
         python_callable=get_ds)
+    
+    t1>>t2
 
