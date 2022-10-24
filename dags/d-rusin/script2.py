556ea59ddfbbc9fe551d93b0b56a2829329f9f0b
Daniil Rusin <danru00@mail.ru>
2022-10-24 11:26:55 +0000

    Update script2.py

--- dags/d-rusin/script2.py
+++ dags/d-rusin/script2.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
 
 with DAG(
-        'first question',
+        'first_question',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -13,13 +13,13 @@ with DAG(
             'retries': 1,
             'retry_delay': timedelta(minutes=5),
         },
-        description='Py and Bash operators',
+        description='Py-and-Bash-operators',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2022, 10, 22),
         catchup=False,
         tags=['task_2'],
 ) as dag:
-    task1 = BashOperator(task_id="print pwd", bash_command="pwd")
+    task1 = BashOperator(task_id="print_pwd", bash_command="pwd")
 
 
     def print_ds(ds):
