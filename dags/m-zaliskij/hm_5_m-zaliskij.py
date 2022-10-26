86b10a6571d72ab795e84721453f75a2c503776b
Maxim Zaliskiy <maksimys.mz@gmail.com>
2022-10-26 16:34:02 +0300

    add5

---
+++ dags/m-zaliskij/hm_5_m-zaliskij.py
@@ -0,0 +1,32 @@
+from datetime import timedelta, datetime
+from textwrap import dedent
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'hw_5_m-zaliskij',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5)
+        },
+        start_date=datetime(2022, 1, 1),
+        catchup=False
+
+) as dag:
+    t1 = BashOperator(
+            task_id=f"echo{i}",
+            bash_command=dedent(
+                '''
+            {% for i in range(5) %}
+                echo "{{ts}}"
+                echo "{{run_id}}"
+            {% endfor %}
+            '''
+            )
+        )
