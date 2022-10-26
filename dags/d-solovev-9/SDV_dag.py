d91d76b394a4fb703e9a386943c1ab79a72413e6
DSolovyev <denis534@mail.ru>
2022-10-27 02:01:35 +0300

    change for loop

--- dags/d-solovev-9/SDV_dag.py
+++ dags/d-solovev-9/SDV_dag.py
@@ -88,53 +88,97 @@ with DAG(
 #         bash_command=templated_command,
 #     )
     
-    def print_context(ds, **kwargs):
-        """Пример PythonOperator"""
-        # Через синтаксис **kwargs можно получить словарь
-        # с настройками Airflow. Значения оттуда могут пригодиться.
-        # Пока нам не нужно
-#         print(kwargs)
-        # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
-        print(ds)
-        return 'Whatever you return gets printed in the logs'
+#     templated_command = dedent(
+#         """
+#         {% for i in range(5) %}
+#             echo "{{ dag_run.logical_date | ts }}"
+#         {% endfor %}
+#         echo "{{ run_id }}"
+#         """
+#     )  # поддерживается шаблонизация через Jinja
+#     # https://airflow.apache.org/docs/apache-airflow/stable/concepts/operators.html#concepts-jinja-templating
+
+#     jin_task = BashOperator(
+#         task_id='templated',
+#         depends_on_past=False,
+#         bash_command=templated_command,
+#     )
+    
+#     def print_context(ds, **kwargs):
+#         """Пример PythonOperator"""
+#         # Через синтаксис **kwargs можно получить словарь
+#         # с настройками Airflow. Значения оттуда могут пригодиться.
+#         # Пока нам не нужно
+# #         print(kwargs)
+#         # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
+#         print(ds)
+#         return 'Whatever you return gets printed in the logs'
     
         
-    def print_task_number(**kwargs):
+
+# 
+    def print_context(task_number, ts, run_id, **kwargs):
         """ PythonOperator - печать номера таска"""
         print(f"task number is: {kwargs.get('task_number')}")
+        print(f"task time is: {kwargs.get('ts')}")
+        print(f"run_id is: {kwargs.get('run_id')}")
 #         t_num = kwargs.get('task_number')
         return print(f"task number is: {kwargs.get('task_number')}") # 'Whatever you return gets printed in the logs'
-
-    python_get_ds = PythonOperator(
-        task_id='print_the_context',  # нужен task_id, как и всем операторам
-        python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
-    )
     
+#     python_get_ds = PythonOperator(
+#         task_id='print_the_context',  # нужен task_id, как и всем операторам
+#         python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
+#     )
+                     
     # Генерируем таски в цикле - так тоже можно
-    for i in range(30):
+    for i in range(31):
         # Каждый таск будет спать некое количество секунд
-        if i <= 10 :
+        if i < 10 :
             task = BashOperator(
                     task_id='echo_bash' + str(i),
                     depends_on_past=False,
-                    bash_command=f'echo {i}',
+                    bash_command=f'echo {i}', #Задание 2
+#                     bash_command='echo $NUMBER',
+#                     env={"NUMBER": str(i)}
   
             )
         else:
             task = PythonOperator(
                 task_id='print_task_number'+ str(i),  # в id можно делать все, что разрешают строки в python
-                python_callable=print_task_number,
+                
                 # передаем в аргумент с названием random_base значение float(i) / 10
-                op_kwargs={'task_number': str(i)},
-
+                op_kwargs={'task_number': i,
+                           'ts' : '{{ ts }}',
+                           'run_id' : '{{run_id}}'
+                          },
+                provide_context=True,
+                python_callable=print_context,
             )
+        
+        dag.doc_md = __doc__             
+        task.doc_md = dedent(
+            """\
+        #### Task Documentation
+        You can document your task using the attributes `doc_md` (markdown),
+        `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+        rendered in the UI's Task Instance Details page.
+        Or you can use `code` like `def func(): return True`.
+        *some italic* text
+        **some bold** text
+        `` some monospace`` text
+        ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+
+        """
+        ) 
 
         # настраиваем зависимости между задачами
         # run_this - это некий таск, объявленный ранее (в этом примере не объявлен)
-        python_get_ds >> task
+        get_pwd >> task
 
     # А вот так в Airflow указывается последовательность задач
-    get_pwd >> python_get_ds
+#     get_pwd >> jin_task
+    get_pwd #>> python_get_ds
+#     python_get_ds >> jin_task
     # будет выглядеть вот так
     #      -> t2
     #  t1 | 
