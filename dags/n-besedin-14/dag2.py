e8f26f9f55c094caadaae36293a5bcc673db6561
n-besedin-14 <nicklogic97@gmail.com>
2022-10-27 23:00:47 +0300

    Edited lesson 11 step 2

--- dags/n-besedin-14/dag2.py
+++ dags/n-besedin-14/dag2.py
@@ -1,17 +1,22 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-        'hw_2_n-besedin-14',
+        'hw_2_n_besedin_14',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
             'email_on_failure': False,
             'email_on_retry': False,
             'retries': 1,
-            'retry_delay': timedelta(minutes=5)}
+            'retry_delay': timedelta(minutes=5)},
+        description='besedin_lesson2',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 10, 27),
+        catchup=False,
+        tags=['hw_2_n-besedin-14']
 ) as dag:
     t1 = BashOperator(
         task_id='pwd',
@@ -24,6 +29,7 @@ with DAG(
 
 
     t2 = PythonOperator(
-        task_id='print ds',
+        task_id='print_ds',
         python_callable=print_ds)
-    
+
+    t1 >> t2
