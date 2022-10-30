af983f81e8b8f745abaaeeb753fcdb2401d18e9c
iliyas.boztayev <boztailijas@seznam.cz>
2022-10-30 22:55:32 +0100

    changes

--- dags/i-boztayev/hw_2_i_boztayev.py
+++ dags/i-boztayev/hw_2_i_boztayev.py
@@ -1,15 +1,11 @@
 from datetime import datetime, timedelta
-from textwrap import dedent
 
-# Для объявления DAG нужно импортировать класс из airflow
 from airflow import DAG
 
-# Операторы - это кирпичики DAG, они являются звеньями в графе
-# Будем иногда называть операторы тасками (tasks)
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 with DAG(
-    'Homework 2',
+    'hw_2',
     # Параметры по умолчанию для тасок
     default_args={
         'depends_on_past': False,
@@ -19,38 +15,29 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
-    # Описание DAG (не тасок, а самого DAG)
     description='Lesson 11, homework 2',
-    # Как часто запускать DAG
     schedule_interval=timedelta(days=1),
-    # С какой даты начать запускать DAG
-    # Каждый DAG "видит" свою "дату запуска"
-    # это когда он предположительно должен был
-    # запуститься. Не всегда совпадает с датой на вашем компьютере
     start_date=datetime(2022, 1, 1),
-    # Запустить за старые даты относительно сегодня
-    # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
     catchup=False,
-    # теги, способ помечать даги
-    tags=['i_boztayev'],
+    tags=['i_boztayev']
 
 ) as dag:
 
     # t1, t2 - это операторы (они формируют таски, а таски формируют даг)
     t1 = BashOperator(
         task_id='path print',  # id, будет отображаться в интерфейсе
-        bash_command='pwd',  # какую bash команду выполнить в этом таске
+        bash_command='pwd' # какую bash команду выполнить в этом таске
     )
+
+    def print_context(ds):
+        print(ds)
+
 
     t2 = PythonOperator(
-        def print_context(ds, **kwargs):
-            print(ds)
-
-        run_this = PythonOperator(
-            task_id='print_the_context',  # нужен task_id, как и всем операторам
-            python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
-        )
+        task_id='print_the_context',  # нужен task_id, как и всем операторам
+        python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
     )
+
 
     # А вот так в Airflow указывается последовательность задач
     t1 >> t2
