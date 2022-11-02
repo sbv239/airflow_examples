6cac7d93e81a332b79881f6ec06ded269db3e165
Nikita Anufriev <nfrv.nikita@gmail.com>
2022-11-02 21:28:14 +0300

    newtask13-ver1

--- dags/n-anufriev/task13.py
+++ dags/n-anufriev/task13.py
@@ -1,16 +1,9 @@
-from airflow import DAG
-from airflow.models import Variable
-from airflow.operators.bash import BashOperator
-from airflow.operators.python import BranchPythonOperator
-from airflow.operators.dummy import DummyOperator
-
 from datetime import datetime, timedelta
 
-
-def determine_course():
-    if Variable.get("is_startml"):
-        return 'startml_desc'
-    return 'not_startml_desc'
+from airflow import DAG
+from airflow.models import Variable
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
 
 
 with DAG(
@@ -30,27 +23,37 @@ with DAG(
         tags=['hw_13_n-anufriev'],
 ) as dag:
 
-    before_branching = DummyOperator(
-        task_id='before_branching',
+    from airflow.operators.python import BranchPythonOperator
+
+    STARTML_ID = "startml_desc"
+    ANOTHER_COURSE_ID = "not_startml_desc"
+
+
+    def choose_course():
+        return STARTML_ID if Variable.get("is_startml") == "True" else ANOTHER_COURSE_ID
+
+    t4 = DummyOperator(task_id="before_branching")
+
+    branching = BranchPythonOperator(
+        task_id="determine_course",
+        python_callable=choose_course,
     )
 
-    get_course = BranchPythonOperator(
-        task_id='determine_course',
-        python_callable=determine_course,
+    t_startml = PythonOperator(
+        task_id=STARTML_ID,
+        python_callable=lambda: "StartML is a starter course for ambitious people"
     )
 
-    task_1 = BashOperator(
-        task_id='startml_desc',
-        bash_command="StartML is a starter course for ambitious people",
+    t_another_course = PythonOperator(
+        task_id=ANOTHER_COURSE_ID,
+        python_callable=lambda: "Not a startML course, sorry"
     )
 
-    task_2 = BashOperator(
-        task_id='not_startml_desc',
-        bash_command="Not a startML course, sorry",
+    t5 = DummyOperator(task_id="after_branching")
+
+    print_var = PythonOperator(
+        task_id="print_var",
+        python_callable=lambda: print(Variable.get("is_startml"))
     )
 
-    after_branching = DummyOperator(
-        task_id='after_branching',
-    )
-
-    before_branching >> get_course >> [task_1, task_2] >> after_branching
+    t4 >> print_var >> branching >> [t_startml, t_another_course] >> t5
