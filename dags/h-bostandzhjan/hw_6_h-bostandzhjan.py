0047c797dfd9c21e070d78a5a99d246c8827991b
Hristo <boton6355@gmail.com>
2022-11-14 19:34:49 +0300

    hw_6_h-bostandzhjan

--- dags/h-bostandzhjan/hw_6_h-bostandzhjan.py
+++ dags/h-bostandzhjan/hw_6_h-bostandzhjan.py
@@ -1,7 +1,8 @@
 from airflow import DAG
+from textwrap import dedent
+from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from datetime import timedelta, datetime
-from textwrap import dedent
 
 default_args = {
     'depends_on_past': False,
@@ -12,12 +13,13 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 with DAG('hw_6_h-bostandzhjan',
-         default_args=default_args,
-         description='A simple tutorial DAG',
-         schedule_interval=timedelta(days=1),
-         start_date=datetime(2021, 1, 1),
-         catchup=False) \
-        as dag:
+    default_args = default_args,
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['hristo'],
+    ) as dag:
 
     def print_task_info(task_number, ts, run_id):
         print(f"task number is: {task_number}")
@@ -26,10 +28,9 @@ with DAG('hw_6_h-bostandzhjan',
 
     for i in range(20):
         t1 = PythonOperator(
-            task_id=f't1_iter_task_number {i}',
+            task_id=f't1_task_6_iter{i}',
             python_callable=print_task_info,
-            op_kwargs={'task_number': i
-                       }
+            op_kwargs={'task_number': i}
         )
     t1.doc_md = dedent(
         '''
