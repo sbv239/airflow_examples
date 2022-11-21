3f5b60dc394d89a6a3b0814be3125556a6f0a865
Oleg Chikin <chikin.ok@phystech.edu>
2022-11-21 16:44:49 +0300

    HW 2 o-chikin

--- dags/o-chikin/task2.py
+++ dags/o-chikin/task2.py
@@ -1,14 +1,15 @@
-from datetime import datetime, timedelta
 from airflow import DAG
-
-# Операторы - это кирпичики DAG, они являются звеньями в графе
-# Будем иногда называть операторы тасками (tasks)
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import date, timedelta, datetime
+
+
+def print_ds_from_python(ds, **kwargs):
+    print(ds)
+    return 'Function to print ds date is working OK'
 
 with DAG(
-    'o-chikin_task_2',
-    # Параметры по умолчанию для тасок
+    'o-chikin_task2',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -16,29 +17,22 @@ with DAG(
         'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-    },
-    # Описание DAG (не тасок, а самого DAG)
-    description='A simple DAG',
+        },
+    description='task2_DAG',
     schedule_interval=timedelta(days=1),
-    start_date=datetime(2022, 1, 1),
+    start_date=datetime(2022, 5, 16),
     catchup=False,
-    tags=['task 2'],
+    tags=['Oleg_Chikin_DAG']
 ) as dag:
 
     t1 = BashOperator(
-        task_id='o-chikin_task_2/1',  # id, будет отображаться в интерфейсе
-        bash_command= pwd,  # какую bash команду выполнить в этом таске
+        task_id='print_bash_pwd',
+        bash_command='pwd',
     )
 
-    date = "{{ ds }}"
-    def print_context(ds, **kwargs):
-        print(kwargs)
-        print(ds)
-        return 'Whatever you return gets printed in the logs'
-
     t2 = PythonOperator(
-        task_id='o-chikin_task_2/2',  # нужен task_id, как и всем операторам
-        python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
-)
+        task_id='print_ds_date_from_python',
+        python_callable=print_ds_from_python,
+    )
 
     t1 >> t2
