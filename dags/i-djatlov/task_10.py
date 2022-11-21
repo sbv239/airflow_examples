61cb3192854cbf8bc301cc69cde01aac8a0293e0
Ivan Dyatlov <ivan69rus@mail.ru>
2022-11-21 21:16:24 +0300

    modif task_10.py

--- dags/i-djatlov/task_10.py
+++ dags/i-djatlov/task_10.py
@@ -7,7 +7,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'hw_9_i-djatlov',
+    'hw_10_i-djatlov',
     default_args={
     'depends_on_past': False,
     'email': ['airflow@example.com'],
@@ -16,7 +16,7 @@ with DAG(
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
     },
-    description='task_9',
+    description='task_10',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 11, 1),
     catchup=False,
