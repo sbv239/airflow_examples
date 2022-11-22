93f448cf03b9c3e3dfec3b676e16ef4f583be192
yancharskaya <y.yancharskaya@gmail.com>
2022-11-22 03:02:35 +0300

    ex11, attempt: 2

--- dags/j-jancharskaja/ex11.py
+++ dags/j-jancharskaja/ex11.py
@@ -3,45 +3,47 @@ from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 
-import pandas as pd
+def get_connection():
+    from airflow.hooks.base import BaseHook
+    import psycopg2
+    from psycopg2.extras import RealDictCursor
 
-from airflow.providers.postgres.operators.postgres import PostgresHook
+    creds = BaseHook.get_connection('startml_feed')
+    with psycopg2.connect(
+    f"postgresql://{creds.login}:{creds.password}"
+    f"@{creds.host}:{creds.port}/{creds.schema}",
+    cursor_factory=RealDictCursor
+    ) as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(
+            """
+            SELECT user_id, COUNT(action='like') AS count
+            FROM "feed_action"
+            WHERE action='like'
+            GROUP BY user_id
+            ORDER BY count DESC
+            LIMIT 1
+            """)
+            return dict(cursor.fetchone())
+            
 
-postgres = PostgresHook(postgres_conn_id="postgresql://robot-startml-ro:pheiph0hahj1Vaif@"
-    "postgres.lab.karpov.courses:6432/startml")
-
-with postgres.get_conn() as conn: 
-    with conn.cursor() as cursor:
-        likers = cursor.execute("""
-        SELECT post_id, COUNT(action='like') AS likes
-        FROM "feed_action"
-        WHERE action='like'
-        GROUP BY post_id
-        ORDER BY likes DESC
-        """)
-        liker = likers.iloc[0]
-
-    with DAG(
-        'j-jancharskaja_10',
-        default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-        },
-        description = 'My eigth DAG',
-        schedule_interval = timedelta(days=1),
-        start_date = datetime(2022, 11, 11),
-        catchup = False,
-        tags = ['eigth']
-    ) as dag:
-
-        def push_test():
-            return 'Airflow tracks everything'
+with DAG(
+    'j-jancharskaja_11',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    },
+    description = 'My ninth DAG',
+    schedule_interval = timedelta(days=1),
+    start_date = datetime(2022, 11, 11),
+    catchup = False,
+    tags = ['ninth']
+) as dag:
     
-        t1 = PythonOperator(
-            task_id = 'push_test',
-            python_callable = push_test,
-            )
+    t1 = PythonOperator(
+        task_id = 'liker',
+        python_callable = get_connection)
