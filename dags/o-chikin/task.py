e538ff06ea5a8b0c004824f0a800fbdf7c49c060
Oleg Chikin <chikin.ok@phystech.edu>
2022-11-23 18:24:10 +0300

    HW 9 o-chikin

--- dags/o-chikin/task.py
+++ dags/o-chikin/task.py
@@ -1,10 +1,22 @@
 from airflow import DAG
-from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import PythonOperator
-from datetime import date, timedelta, datetime
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+
+def get_info(ti):
+    ti.xcom_push(
+        key="sample_xcom_key",
+        value="xcom test"
+    )
+
+def print_info(ti):
+    result = ti.xcom_pull(
+        key="sample_xcom_key",
+        value="xcom test"
+    )
+    print(result)
 
 with DAG(
-    'o-chikin_task7',
+    'o-chikin_task9',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -13,27 +25,19 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
         },
-    description='task7',
+    description='task9',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 5, 16),
     catchup=False,
     tags=['Oleg_Chikin_DAG']
 ) as dag:
+    t1 = PythonOperator(
+        task_id = 'get_info',
+        python_callable=get_info
+    )
+    t2 = PythonOperator(
+        task_id = 'print_info',
+        python_callable=print_info
+    )
 
-    for i in range(10):
-        t1 = BashOperator(
-            task_id='bash_operator' + str(i),
-            bash_command=f'echo {i}'
-        )
-
-    def python_func(task_number, ts, run_id):
-        print(f"task number is: {task_number}")
-        print(ts)
-        print(run_id)
-
-    for i in range(20):
-        t2 = PythonOperator(
-            task_id='python_operator' + str(i),
-            python_callable=python_func,
-            op_kwargs={'task_number': i},
-        )
+    t1 >> t2
