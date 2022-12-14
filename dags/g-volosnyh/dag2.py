55625cecf0bd6099afa0f1331558e608ee41d72f
George Volosnyh <g.volosnyh@orsyst.ru>
2022-12-14 16:05:00 +0300

    add dag2

--- dags/g-volosnyh/dag2.py
+++ dags/g-volosnyh/dag2.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'dag1_g-volosnyh',
+    'dag2_g-volosnyh',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -15,7 +15,7 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
-    description='dag1',
+    description='dag2',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 1, 1),
     catchup=False,
