a46f1dc904f8840cc033c7faeecc403eac78abaa
Rabbit323 <maxim.vasilevskii@gmail.com>
2022-12-15 11:10:05 +0400

    task2 commit3

--- dags/m-vasilevskij/task2.py
+++ dags/m-vasilevskij/task2.py
@@ -20,7 +20,8 @@ with DAG(
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-    }
+    },
+    start_date = datetime(2022, 12, 14),
 ) as dag:
 
     t1 = BashOperator(
