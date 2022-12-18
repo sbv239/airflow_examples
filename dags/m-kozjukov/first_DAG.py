e5bd5b8d1541a1d82e88d6049782d050abcf783d
Максим Козюков <ms-intro95@mail.ru>
2022-12-18 20:00:58 +0000

    Update first_DAG.py

--- dags/m-kozjukov/first_DAG.py
+++ dags/m-kozjukov/first_DAG.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from datetime import timedelta, datetime
 
 with DAG(
-    'first_dag'
+    'first_dag',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -14,7 +14,7 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
-    description='first dag'
+    description='first dag',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 12, 18),
     catchup=False,
