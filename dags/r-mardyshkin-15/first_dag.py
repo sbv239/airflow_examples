8bb0a05a4ef1f6cb4e3d2075e290b369e26fc2c2
Rostislav Mardyshkin <mardyshkinrr@yandex.ru>
2022-12-26 18:03:25 +0300

    passed test HW 1 in LMS

--- dags/r-mardyshkin-15/first_dag.py
+++ dags/r-mardyshkin-15/first_dag.py
@@ -14,7 +14,8 @@ with DAG(
 		'email_on_retry': False,
 		'retries': 1,
 		'retry_delay': timedelta(minutes=5)
-	}
+	},  
+    start_date=datetime(2022, 12, 26)
 ) as dag:
 	t1 = BashOperator(
 		task_id='my_path',
