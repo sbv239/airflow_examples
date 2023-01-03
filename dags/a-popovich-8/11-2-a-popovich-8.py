6a7b04163571459e03640619fcba94509e5ad7b9
Anastasiya <anastasiiachaa@yandex.ru>
2023-01-03 12:14:48 +0300

    popo

--- dags/a-popovich-8/11-2-a-popovich-8.py
+++ dags/a-popovich-8/11-2-a-popovich-8.py
@@ -8,7 +8,7 @@ from airflow.operators.python import PythonOperator
 
 
 with DAG(
-    '2-a-popovich-8',
+    'hw-2-a-popovich-8',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -21,7 +21,7 @@ with DAG(
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 1, 1),
     catchup=False,
-    tags=['example'],
+    tags=['hw_a-popovich-8'],
 ) as dag:
 
     t1 = DummyOperator(task_id='start_dag')
