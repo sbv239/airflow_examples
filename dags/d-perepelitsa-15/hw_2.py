a994737cac591ae745b9e0c367f9f7f21e9f9093
perepelitsa.dd@yandex.ru <perepelitsa.dd@yandex.ru>
2023-01-12 11:28:16 +0300

    hw_2

---
+++ dags/d-perepelitsa-15/hw_2.py
@@ -0,0 +1,35 @@
+from datetime import timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}
+
+def print_world(ds: str):
+    print(ds)
+
+templated_command = dedent(
+    """pwd"""
+)
+
+with DAG(
+        default_args=default_args,
+        description='Задание hw_2'
+) as dag:
+    task1 = PythonOperator(
+        task_id='task_1',
+        python_callable=print_world
+    )
+    task2 = BashOperator(
+        task_id='task_2',
+        bash_command=templated_command
+    )
+    task2 >> task1
