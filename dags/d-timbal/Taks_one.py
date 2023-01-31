595eb1af54ada07fa3d1e2b410d72ca299270445
Danil Timbal <danilartful4@gmail.com>
2023-01-31 21:25:53 +0300

    make print_ds and print_dir

---
+++ dags/d-timbal/Taks_one.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operarots import BashOperator
+from airflow.operators import PythonOperator
+
+from datetime import datetime, timedelta
+
+
+
+with DAG(
+    'TaskNumberOne',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='FirstDag',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 1, 30),
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    def print_ds(ds):
+        print(ds)
+        return 'QQ'
+
+    t2 = PythonOperator(
+        task_id='print datestart',
+        python_callable=print_ds
+    )
+
+    t1 = BashOperator(
+        task_id='print dir',
+        bash_command='pwd'
+    )
+
+    t1 >> t2
