148996937fe11dde8dd99819402cfabd38b6b8a4
Ilya Kulikov <lawfirmfacc@yandex.ru>
2023-02-11 14:21:01 +0300

    HW 4 i-kulikov-17

---
+++ dags/i-kulikov-17/ex_4.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'hw_3_i-kulikov-17',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='Second ex with 30 iterations',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 2, 10),
+    catchup=False,
+    tags=['hw_3_lawfirmfacc@mail.ru'],
+
+) as dag:
+
+    for i in range(10):
+        t1 = BashOperator(
+            task_id='bash_' + str(i),
+            bash_command=f'echo {i}',
+        )
+
+    t1.doc_md = dedent(
+        """
+    #### Task Documentation
+    You **can** document your *task* using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    # rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+    """
+    )
+
+    def print_task(task_number):
+        print(f"task number is: {task_number}")
+
+    for i in range(20):
+        t2 = PythonOperator(
+            task_id=f'print_task_{i}',
+            python_callable=print_task,
+            op_kwargs={'task_number': i}
+        )
+
+    t2.doc_md = dedent(
+        """
+    #### Task Documentation
+    You **can** document your *task* using the attributes `doc_md` (markdown),
+    `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+    # rendered in the UI's Task Instance Details page.
+    ![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+    """
+    )
+
+    t1 >> t2
