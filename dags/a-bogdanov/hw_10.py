ce7b2e7df4de8f9b840b914ac320d8fba4fa5a3c
antonio-st@yandex.ru <antonio-st@yandex.ru>
2023-02-11 22:28:03 +0300

    add hw_10_a-bogdanov

--- dags/a-bogdanov/hw_10.py
+++ dags/a-bogdanov/hw_10.py
@@ -1,28 +1,30 @@
 """
-hw_9.py DAG
+hw_10.py DAG
 """
 from datetime import timedelta, datetime
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
-# x_com отправляем данные явно
-def get_testing_increase(ti):
+
+# x_com отправляем данные неявно
+def x_com_push(ti):
     ti.xcom_push(
-        key='sample_xcom_key',
-        value='xcom test'
+        key='to_return_xcom_key',
     )
+    return "Airflow tracks everything"
+
+
 # xcom_pull принимаем данные
-def get_testing_data(ti):
+def x_com_pull(ti):
     data_read_push = ti.xcom_pull(
-        key='sample_xcom_key',
+        key='return_value',
         task_ids='push_data_out',
     )
     print('Test xcom: ', data_read_push)
 
 
-
 with DAG(
-        'hw_9_a-bogdanov',  # уникальное имя DAG
+        'hw_10_a-bogdanov',  # уникальное имя DAG
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -32,22 +34,23 @@ with DAG(
             'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
         },
 
-        description='Lesson 11, Task 9',
+        description='Lesson 11, Task 10',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2022, 1, 1),
         catchup=False,
 
-        tags=['Lesson_11_Task_9'],
+        tags=['Lesson_11_Task_10'],
 ) as dag:
-
+    # отправляем значения
     t1 = PythonOperator(
         task_id='push_data_out',
-        python_callable=get_testing_increase
+        python_callable=x_com_push
     )
 
+    # принимаем значения
     t2 = PythonOperator(
         task_id='pull_data_in',
-        python_callable=get_testing_data
+        python_callable=x_com_pull
     )
 
     t1 >> t2
