bc3406fa49f42f573523fa6766a2a5e25dfaf0f6
DinKariya <dinkariya@gmail.com>
2023-02-11 16:38:32 +0300

    add task5.py

---
+++ dags/k-d/task5.py
@@ -0,0 +1,45 @@
+"""
+Создайте новый DAG, состоящий из одного BashOperator. 
+Этот оператор должен  использовать шаблонизированную команду следующего вида: 
+"Для каждого i в диапазоне от 0 до 5 не включительно распечатать значение ts и затем распечатать значение run_id". 
+Здесь ts и run_id - это шаблонные переменные (вспомните, как в лекции подставляли шаблонные переменные).
+"""
+
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator 
+from datetime import timedelta, datetime
+
+
+with DAG (
+    'k-d',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description = 'description text',
+    schedule_interval = timedelta(days=1),
+    start_date = datetime(2023,1,1),
+    catchup=False
+) as dag:
+    
+    templated_command = dedent(
+        """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ run_id }}"
+    {% endfor %}
+    """
+    )  # поддерживается шаблонизация через Jinja
+    # https://airflow.apache.org/docs/apache-airflow/stable/concepts/operators.html#concepts-jinja-templating
+
+    t3 = BashOperator(
+        task_id='templated',
+        depends_on_past=False,
+        bash_command=templated_command,
+    )
