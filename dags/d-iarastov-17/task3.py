932c363ecaa9eb6363bb1d3933a3571c42071fe3
Dima Iarastov <ydima@google.com>
2023-02-12 14:01:44 +0400

    changed for loop

--- dags/d-iarastov-17/task3.py
+++ dags/d-iarastov-17/task3.py
@@ -21,17 +21,19 @@ with DAG(
     description = "For loop in tasks iteration"
 ) as dag:
     for i in range(30):
-        if i <=10:
+        if i < 10:
             t1 = BashOperator(
-                task_id='print_in_bash_' + str(i),
+                task_id=f'print_in_bash_{i}',
                 bash_command=f"echo line number {i}"
                 )
         else:
-            def print_task_number(task_number, **kwargs):
-                print(f'task number is: {task_number}')
-                print(kwargs)
+            def print_task_number(ds, **kwargs):
+                task_number = kwargs['task_number']
+                print(f"task number is: {task_number}")
+
+
             t2 = PythonOperator(
-                task_id='task_number_' + str(i),
+                task_id=f'task_number_{i}',
                 python_callable=print_task_number,
                 op_kwargs={'task_number': i}
                 )
