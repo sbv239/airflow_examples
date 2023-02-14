8c47d2751c8dbe07978a24128a00044a92f968f0
Igortigr <liigor.acer@gmail.com>
2023-02-14 22:22:48 +0300

    hm5

---
+++ dags/i-li/hm5.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import timedelta, datetime
+from textwrap import dedent
+
+with DAG(
+        'hm_2',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='первая работа с DAG',
+        start_date=datetime(2023, 2, 13)
+) as dag:
+    templated_command = dedent(
+        '''
+        {% for i in range(5) %}
+            echo "{{ tпшs }}"
+            echo "{{ run_id }}"
+        {% endfor %}
+        '''
+    )
+    t1 = BashOperator(
+        task_id='hm_5',
+        bash_command=templated_command
+    )
