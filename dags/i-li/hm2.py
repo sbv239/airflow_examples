0391e585e7e21afe4580be51bc8bce76353ba2c6
Igortigr <liigor.acer@gmail.com>
2023-02-14 12:19:57 +0300

    print_ds

---
+++ dags/i-li/hm2.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import timedelta, datetime
+
+with DAG(
+        'hm_1',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='первая работа с DAG',
+        start_date=datetime(2023, 2, 13)
+) as dag:
+    t1 = BashOperator(
+        task_id='pwd',
+        bash_command='pwd'
+    )
+
+
+    def print_ds(ds):
+        print(ds)
+
+
+    t2 = PythonOperator(
+        task_id='print_ds',
+        python_callable=print_ds
+    )
