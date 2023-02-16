d9e730a5172ece9ae83b4b4c6c353b41b6e74665
Ivan Beresnev <Beresnev27@yandex.ru>
2023-02-16 23:41:29 +0400

    HW 2

--- dags/i-beresnev/dag2_IB.py
+++ dags/i-beresnev/dag2_IB.py
@@ -6,14 +6,14 @@ from airflow.operators.python import PythonOperator
 
 
 with DAG(
-    'hw_simple_dag',
+    'HW_3_Beresnev',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
-        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        'retry_delay': timedelta(minutes=5),
     },
     description='A simple tutorial DAG',
     schedule_interval=timedelta(days=1),
@@ -22,19 +22,21 @@ with DAG(
     tags=['example'],
 ) as dag:
 
-    t1 = BashOperator(
-        task_id='print_dir',
-        bash_command='pwd',
-    )
+    for i in range(10):
+        t1 = BashOperator(
+            task_id=f'print_smth_{i}',
+            bash_command=f"echo {i}"
+        )
 
-    def print_date(ds, **kwargs):
-        print(ds)
-        return 'check logs!'
+    def print_context(ds, **kwargs):
+        print(f"task number is: {kwargs.get('task_number')}")
+        return 'Check logs x2'
 
 
-    t2 = PythonOperator(
-        task_id='print_date',
-        python_callable=print_date,
-    )
-
+    for i in range(20):
+        t2 = PythonOperator(
+            task_id=f'print_tn_{i}',
+            python_callable=print_context,
+            op_kwargs={"task_number": i}
+        )
     t1 >> t2
