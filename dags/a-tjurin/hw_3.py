109161ab2fbc3a97f07bea66138f022d62fd0bb0
Artem Tyurin <tyur.art@mail.ru>
2023-02-16 05:13:20 +0300

    task_3

---
+++ dags/a-tjurin/hw_3.py
@@ -0,0 +1,46 @@
+"""
+hw_3.py DAG
+"""
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'hw_3_a-tjurin',  
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  
+        },
+
+        description='Task 3',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 2, 16),
+        catchup=False,
+
+        tags=['Task_3'],
+) as dag:
+    for i in range(10):
+        t1 = BashOperator(
+            task_id='print_in_bash_' + str(i),
+            bash_command=f"echo line number {i}", 
+        )
+
+    def task_number(ds, task_number, **kwargs):
+        print(kwargs)
+        print(ds)
+        return f"task number is: {task_number}"
+
+    for i in range(20):
+        t2 = PythonOperator(
+            task_id='print_task_num_' + str(i),
+            python_callable=task_number, 
+            op_kwargs={'task_number': i} 
+        )
+
+
+    t1 >> t2
