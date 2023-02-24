eababf63014aefd3e0d4ad49e7c04f63f6ddf90b
Ksenia <kassia.nery@mail.ru>
2023-02-24 08:14:31 +0300

    modified first_file

--- dags/k-bychkova-16/first_script.py
+++ dags/k-bychkova-16/first_script.py
@@ -1,32 +1,47 @@
-import airflow
+from datetime import datetime, timedelta
+
+from textwrap import dedent
+
+# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
 
-# Операторы - это кирпичики DAG, они являются звеньями в графе
-# Будем иногда называть операторы тасками (tasks)
+# Operators; we need this to operate!
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-"""
-Напишите DAG, который будет содержать BashOperator и PythonOperator.
-В функции PythonOperator примите аргумент ds и распечатайте его. Можете распечатать дополнительно любое другое сообщение.
-В BashOperator выполните команду pwd, которая выведет директорию, где выполняется ваш код Airflow. Результат
-может оказаться неожиданным, не пугайтесь - Airflow может запускать ваши задачи на разных машинах
- или контейнерах с разными настройками и путями по умолчанию.
-Сделайте так, чтобы сначала выполнялся BashOperator, потом PythonOperator.
-"""
-# вот так можно попросить Airflow подставить логическую дату
-# в формате YYYY-MM-DD
-#date = "{{ ds }}"
-t = BashOperator(
-    task_id="pwd_env",
-    bash_command="/pwd "
-)
+with DAG(
+    'tutorial',
+    default_args={
+        'start_date': datetime(2017, 2, 1),
+           # Если прошлые запуски упали, надо ли ждать их успеха
+        'depends_on_past': False,
+        # Кому писать при провале
+        'email': ['airflow@example.com'],
+        # А писать ли вообще при провале?
+        'email_on_failure': False,
+        # Писать ли при автоматическом перезапуске по провалу
+        'email_on_retry': False,
+        # Сколько раз пытаться запустить, далее помечать как failed
+        'retries': 1,
+        # Сколько ждать между перезапусками
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
 
-def print_context(ds):
-    print(ds)
-    return 'Hello'
+) as dag:
+    def print_context(ds):
+        #print(kwargs)
+        print(ds)
+        return 'Whatever you return gets printed in the logs'
+    # t1, t2, t3 - это операторы (они формируют таски, а таски формируют даг)
 
-run_this = PythonOperator(
-    task_id='print_the_context',  # нужен task_id, как и всем операторам
-    python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
-)
+    t1 = BashOperator(
+        task_id='MyDAG_1',
+        bash_command='pwd',
+    )
+
+    t2 = PythonOperator(
+        task_id='MyDAG_2',
+        python_callable=print_context
+    )
+
+t1 >> t2
