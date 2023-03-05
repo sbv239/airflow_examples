7da9d9a959325fbc6527d97d5a689c31c2e27a30
dearbuckwheat <jlol72305@gmail.com>
2023-03-05 19:11:07 +0600

    fix task_2.py

--- dags/a-breus/task_2.py
+++ dags/a-breus/task_2.py
@@ -3,20 +3,20 @@ from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
 
 with DAG(
-    'HomeWork_r_safarov',
+    'HW_2_Safarov',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
-        'retry_delay': timedelta(minutes=5)
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
-    description='Task_2',
-    shedule_interval=timedelta(days=1),
-    start_date=datetime(2023, 4, 1),
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
     catchup=False,
-    tags=['task_2']
+    tags=['task_2'],
 ) as dag:
 
     t1 = BashOperator(
