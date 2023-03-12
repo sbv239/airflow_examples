23dd6e7a530552bf683ced56c3eb2f03f15a624e
Reez <reez.ms@mail.ru>
2023-03-12 22:58:30 +0300

    task_5

---
+++ dags/s-faretdinova-12/task_5.py
@@ -0,0 +1,30 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'task_5',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        },
+    description='An attempt to create DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['attempt'],
+) as dag:
+    
+    for i in range(10):
+        t1 = BashOperator(
+            task_id = 'bash_part' + str(i), # task_id = f"print_date_{i}" - можно было и так
+            bash_command="echo $NUMBER",
+            env={"NUMBER":i}, # env={"NUMBER" : str(i)} - так тоже можно
+        )
