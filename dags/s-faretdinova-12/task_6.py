6d19868f0e35ac84ca60b3fc3709170ec4a2c063
Reez <reez.ms@mail.ru>
2023-03-13 23:14:38 +0300

    task_6

---
+++ dags/s-faretdinova-12/task_6.py
@@ -0,0 +1,44 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'task_6',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        },
+    description='An attempt to create DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['attempt'],
+) as dag:
+    
+    for i in range(10):
+        bash_task = BashOperator(
+            task_id = 'bash_part' + str(i),
+            bash_command=f"echo {i}",
+        )
+        
+    def print_smth(ts, run_id, **kwargs):
+        print(**kwargs) # напечатает все поименованные аргументы, т.е. op_kwargs={'task_number': i} из питоноператора
+        print(f"task number is: {kwargs.get('task_number')}")
+        print(ts)
+        print(run_id)   
+        
+    for i in range(11, 30):
+        python_task = PythonOperator(
+            task_id = 'python_part' + str(i),
+            python_callable=print_smth,
+            op_kwargs={'task_number': i},
+        )
+            
+    bash_task >> python_task
