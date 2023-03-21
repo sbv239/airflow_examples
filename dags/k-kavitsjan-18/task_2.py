3d16a5caf18343fd7521b8ec16cec8e561ff6353
MrKorsar <kirkor96@gmail.com>
2023-03-21 12:16:34 +0300

    task_2

---
+++ dags/k-kavitsjan-18/task_2.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+with DAG(
+    'k-kavitsjan-18_task_2',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+        },
+    description='task_2',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 3, 20),
+    catchup=False,
+    tags=['k-kavitsjan-18'],
+    ) as dag:
+    t1 = BashOperator(
+        task_id='print_pwd',
+        bash_command='pwd',
+    )
+
+    t2 = PythonOperator(
+        task_id='print_ds',
+        python_callable=print(ds),
+    )
+    dag.doc_md="""
+    This is documentation"""
+
+    t1 >> t2
