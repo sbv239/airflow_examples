9a917bc0b3f80b486fd4181c475cdba2ee0d1eb8
Nika Nemsveridze <nikonems@yandex.ru>
2023-03-21 19:46:23 +0300

    DAG_2_NN

--- dags/n-nemsveridze/DAG_2_NN.py
+++ dags/n-nemsveridze/DAG_2_NN.py
@@ -1,35 +1,36 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash import  BashOperator
+from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-With DAG ('task2NN',
-          default_args={
-              'depends_on_past': False,
-              'email': ['airflow@example.com'],
-              'email_on_failure': False,
-              'email_on_retry': False,
-              'retries': 1,
-              'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-          },
-          descrition ='My first DAG',
-          schedule_interval = timedelta(days=1),
-          start_date = datetime(2023,3,21),
-          catchup = False,
-          tags=['example']
-) as dag
+with DAG('task2NN',
+         default_args={
+             'depends_on_past': False,
+             'email': ['airflow@example.com'],
+             'email_on_failure': False,
+             'email_on_retry': False,
+             'retries': 1,
+             'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+         },
+         descrition='My first DAG',
+         schedule_interval=timedelta(days=1),
+         start_date=datetime(2023, 3, 21),
+         catchup=False,
+         tags=['example']
+         ) as dag:
+    task1 = BashOperator(
+        task_id='BashPWD',
+        bash_command='pwd'
+    )
 
-t1=BashOperator(
-    task_id = 'BashPWD'
-    bash_command = 'pwd'
-)
 
-def print_something(ds):
-    return print(ds)
+    def print_something(ds):
+        return print(ds)
 
-t2= PythonOperator(
-    task_id='Pyprint'
-    python_callable = print_something
-)
 
-t1>>t2
+    task2 = PythonOperator(
+        task_id='Pyprint',
+        python_callable=print_something
+    )
+
+    task1 >> task2
