7a2aa45f612723b838ef8506defdcef2a3ae8a5c
Nika Nemsveridze <nikonems@yandex.ru>
2023-03-21 19:32:22 +0300

    DAG_2_NN

---
+++ dags/n-nemsveridze/DAG_2_NN.py
@@ -0,0 +1,35 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import  BashOperator
+from airflow.operators.python import PythonOperator
+
+With DAG ('task2NN',
+          default_args={
+              'depends_on_past': False,
+              'email': ['airflow@example.com'],
+              'email_on_failure': False,
+              'email_on_retry': False,
+              'retries': 1,
+              'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+          },
+          descrition ='My first DAG',
+          schedule_interval = timedelta(days=1),
+          start_date = datetime(2023,3,21),
+          catchup = False,
+          tags=['example']
+) as dag
+
+t1=BashOperator(
+    task_id = 'BashPWD'
+    bash_command = 'pwd'
+)
+
+def print_something(ds):
+    return print(ds)
+
+t2= PythonOperator(
+    task_id='Pyprint'
+    python_callable = print_something
+)
+
+t1>>t2
