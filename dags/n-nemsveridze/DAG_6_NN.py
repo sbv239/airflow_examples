056ed0387cb9571ce788a02d381220608aafb1fe
Nika Nemsveridze <nikonems@yandex.ru>
2023-03-22 20:08:29 +0300

    DAG_6_NN

--- dags/n-nemsveridze/DAG_6_NN.py
+++ dags/n-nemsveridze/DAG_6_NN.py
@@ -1,31 +1,27 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from datetime import datetime, timedelta
-from textwrap import dedent
-
 
 with DAG (
-    'task5NN',
+    'task6NN',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
             'email_on_failure': False,
             'email_on_retry': False,
             'retries': 1,
-            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+            'retry_delay': timedelta(minutes=5),
         },
     description = 'ten times bash twenty times py',
     schedule_interval = timedelta(days=1),
-    start_date=datetime(2023,3,21,22,30,20),
+    start_date=datetime(2023,3,21),
     catchup=False,
-    tags=['NNtask3'],
+    tags=['NNtask6'],
 ) as dag:
-    templated = dedent("""
-    {% for i in range(5) %}
-    echo {{ts}}
-    echo {{run_id}}
-    {% endfor %}
-    """)
 
-    task1=BashOperator(task_id='Tamplated',
-                       bash_command=templated)
+    for i in range(10):
+        NUMBER=i
+        task1=BashOperator(task_id='Number_' + str(NUMBER),
+                           dag=dag,
+                           bash_command="echo$NUMBER",
+                           env={"NUMBER": NUMBER})
