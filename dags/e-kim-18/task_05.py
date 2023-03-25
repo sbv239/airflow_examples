2260b49607331e53560a11bb5beed9f0773a1bb3
Evgeny Kim <kim.ev.prm@gmail.com>
2023-03-25 17:30:18 +0300

    task 05

---
+++ dags/e-kim-18/task_05.py
@@ -0,0 +1,44 @@
+"""
+Task 05 documentation
+"""
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+
+with DAG(
+    'task_02',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='A DAG for task 02',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 3, 20),
+    catchup=False,
+    tags=['task 2'],
+) as dag:
+
+    templated_command = dedent(
+        """
+        {% for i in range(5) %}
+            echo "{{ ts }}"
+            echo "{{ run_id }}"
+        {% endfor %}
+    """
+    )  # поддерживается шаблонизация через Jinja
+
+    t1 = BashOperator(
+        task_id='templated_command',  # id, будет отображаться в интерфейсе
+        depends_on_past=False,
+        bash_command=templated_command,  # какую bash команду выполнить в этом таске
+    )
+
+    t1
