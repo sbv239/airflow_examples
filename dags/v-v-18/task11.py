bebfe3fe8647d6a23e21dc40f7377c71ee72a0a4
Valery <lerochka.fr@gmail.com>
2023-03-26 00:27:00 +0700

    back to PythonOperator

--- dags/v-v-18/task11.py
+++ dags/v-v-18/task11.py
@@ -17,34 +17,34 @@ with DAG(
     },
     start_date = datetime(2023, 3, 23)
 ) as dag:
-    # postgres = PostgresHook(postgres_conn_id="startml_feed")
-    # with postgres.get_conn() as conn:
-    #     with conn.cursor() as cursor:
-    #         @task(task_id="get_active_user")
-    #         def get_active_user():
-    #             cursor.execute("""
-    #             SELECT user_id, COUNT(action) AS "count"
-    #             FROM "feed_action"
-    #             WHERE action = 'like'
-    #             GROUP BY user_id
-    #             ORDER BY COUNT(action) desc
-    #             LIMIT 1
-    #             """)
-    #             result = cursor.fetchone()
-    #             return result
+    postgres = PostgresHook(postgres_conn_id="startml_feed")
+    with postgres.get_conn() as conn:
+        with conn.cursor() as cursor:
+            @task(task_id="get_active_user")
+            def get_active_user():
+                cursor.execute("""
+                SELECT user_id, COUNT(action) AS "count"
+                FROM "feed_action"
+                WHERE action = 'like'
+                GROUP BY user_id
+                ORDER BY COUNT(action) desc
+                LIMIT 1
+                """)
+                result = cursor.fetchone()
+                return result
 
 
 
-    get_active_user = PostgresOperator(
-        task_id="get_active_user",
-        postgres_conn_id="startml_feed",
-        sql=
-            """
-            SELECT user_id, COUNT(action) AS "count"
-            FROM "feed_action"
-            WHERE action = 'like'
-            GROUP BY user_id
-            ORDER BY COUNT(action) desc
-            LIMIT 1
-            """,
-        )
+    # get_active_user = PostgresOperator(
+    #     task_id="get_active_user",
+    #     postgres_conn_id="startml_feed",
+    #     sql=
+    #         """
+    #         SELECT user_id, COUNT(action) AS "count"
+    #         FROM "feed_action"
+    #         WHERE action = 'like'
+    #         GROUP BY user_id
+    #         ORDER BY COUNT(action) desc
+    #         LIMIT 1
+    #         """,
+    #     )
