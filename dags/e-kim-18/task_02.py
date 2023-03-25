af3dab0a819ff847a4f5aee0b70eabf93dbaf88b
Evgeny Kim <kim.ev.prm@gmail.com>
2023-03-25 16:17:16 +0300

    task 02

---
+++ dags/e-kim-18/task_02.py
@@ -0,0 +1,43 @@
+"""
+Task 02 documentation
+"""
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+
+with DAG(
+    'task_02',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='A DAG for task 02',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 3, 20),
+    catchup=False,
+    tags=['task 2'],
+) as dag:
+
+    t1 = BashOperator(
+        task_id='run_pwd_command',  # id, будет отображаться в интерфейсе
+        bash_command='pwd',  # какую bash команду выполнить в этом таске
+    )
+
+    def print_ds(ds):
+        print(ds)
+        print('любое другое сообщение')
+
+    t2 = PythonOperator (
+        task_id='print_ds',  # нужен task_id, как и всем операторам
+        python_callable=print_ds,  # свойственен только для PythonOperator - передаем саму функцию
+    )
+
+    t1 >> t2
