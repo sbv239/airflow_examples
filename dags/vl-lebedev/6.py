d0b9c23613be34693ce13147a91d1dafae327f0f
Vladimir Lebedev <vladscientific@gmail.com>
2023-04-02 20:30:45 +0300

    changed dag desc

--- dags/vl-lebedev/6.py
+++ dags/vl-lebedev/6.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-	'lebedev-dag2',
+	'lebedev-dag6',
 	default_args={
 		'depends_on_past': False,
 		'email': ['airflow@example.com'],
@@ -13,11 +13,11 @@ with DAG(
 		'retries': 1,
 		'retry_delay': timedelta(minutes=5),
 	},
-	description='L-DAG2',
+	description='L-DAG6',
 	schedule_interval=timedelta(days=1),
 	start_date=datetime(2023, 1, 1),
 	catchup=False,
-	tags=['hw2_lebedev'],
+	tags=['hw6_lebedev'],
 ) as dag:
 	t1 = BashOperator(
 		task_id='pwd_task',
