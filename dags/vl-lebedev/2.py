f4fe35e8dbc290d34ced60aaa783e51870289751
Vladimir Lebedev <vladscientific@gmail.com>
2023-04-02 07:26:02 +0300

    changed 2.py

--- dags/vl-lebedev/2.py
+++ dags/vl-lebedev/2.py
@@ -1,72 +1,35 @@
+from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python_operator import PythonOperator
-
-from datetime import datetime, timedelta
-
-def date_print(ds):
-    print(ds)
+from airflow.operators.python import PythonOperator
 
 with DAG(
-    'hw_1_cherepashchuk',
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-},
-description='hw1',
-schedule_interval=timedelta(days=1),
-start_date=datetime(2023, 1, 19),
-catchup=False,
+	'lebedev-dag2',
+	default_args={
+		'depends_on_past': False,
+		'email': ['airflow@example.com'],
+		'email_on_failure': False,
+		'email_on_retry': False,
+		'retries': 1,
+		'retry_delay': timedelta(minutes=5),
+	},
+	description='L-DAG2',
+	schedule_interval=timedelta(days=1),
+	start_date=datetime(2023, 1, 1),
+	catchup=False,
+	tags=['hw2_lebedev'],
 ) as dag:
-    task_1 = BashOperator(
-        task_id = 'pwd',
-        bash_command='pwd'
-    )
+	t1 = BashOperator(
+		task_id='pwd_task',
+		bash_command='pwd',
+	)
+	
+	def prn(ds, **kwargs):
+		print(ds)
+	
+	t2 = PythonOperator(
+		task_id='prn_ds',
+		python_callable=prn,
+	)
 
-    task_2 = PythonOperator(
-        task_id = 'print_date',
-        python_callable=date_print
-    )
-
-    task_1 >> task_2
-
-
-
-with DAG(
-     'lebedev_zad1',
-     # Параметры по умолчанию для тасок
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-    },
-    description="Zad 1 DAG",
-    schedule=timedelta(days=1),
-    start_date=datetime(2023, 1, 1),
-    catchup=False,
-    tags=["example"],
-    ) as dag:
-
-    t1 = BashOperator(
-        task_id='pwd_task',  # id, будет отображаться в интерфейсе
-        bash_command='pwd',  # какую bash команду выполнить в этом таске
-    )
-
-    def print_context(ds, **kwargs):
-        print(ds)
-        return 'Whatever you return gets printed in the logs'
-
-    t2 = PythonOperator(
-        task_id='print_the_context',  # нужен task_id, как и всем операторам
-        python_callable=print_context  # свойственен только для PythonOperator - передаем саму функцию
-        )
-    t1 >> t2
-    
-    
+	t1>>t2
