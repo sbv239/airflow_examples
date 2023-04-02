23a3345d7c256914010073b8e11d14a172813481
Vladimir Lebedev <vladscientific@gmail.com>
2023-04-02 08:02:39 +0300

    fixed dac desc

--- dags/vl-lebedev/3.py
+++ dags/vl-lebedev/3.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-	'lebedev-dag2',
+	'lebedev-dag3',
 	default_args={
 		'depends_on_past': False,
 		'email': ['airflow@example.com'],
