71d8fce95f0df56e4cd060f8d605845e01fd444d
Павел Стрюк <pavel.stryuk@mail.ru>
2023-04-03 21:34:39 +0300

    task_3

---
+++ dags/p-strjuk-18/task_3.py
@@ -0,0 +1,40 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator, op_kwargs
+from datetime import timedelta, datetime
+from textwrap import dedent
+
+def print_task(var):
+    print(f'task number is: {var}')
+    return 'Home' + str(var)
+
+with DAG(
+    'task_3',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='dag for task 3',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023,4,2),
+    catchup=False,
+    tags=['task_3', 'stryuk']
+) as dag:
+    for i in range(1, 11):
+        bash_task = BashOperator(
+            task_id = f'bash_task {i}',
+            bash_command = f'echo {i}',
+
+        )
+    for i in range(11, 31):
+        pyth_task = PythonOperator(
+            task_id = f'pyth_task_{i}',
+            python_callable=print_task,
+
+            op_kwargs={'var': i}
+        )
+    bash_task >> pyth_task
