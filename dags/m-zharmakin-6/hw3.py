331e70eb3150578002354ac890c5290b11a4971a
mike <zharmakin@gmail.com>
2023-04-03 15:00:14 +0600

    HW 3

--- dags/m-zharmakin-6/hw3.py
+++ dags/m-zharmakin-6/hw3.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 
-with DAG('hw_3_m-zharmakin-6',
+with DAG('hw_2_m-zharmakin-6',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -18,23 +18,21 @@ with DAG('hw_3_m-zharmakin-6',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2022, 1, 1),
         catchup=False,
-        tags=['hw_3_m-zharmakin-6']
+        tags=['hw_2_m-zharmakin-6']
         ) as dag:
-    
-    for i in range(10):
-        bash_task = BashOperator(
-            task_id=f'bash_task {i}',
-            bash_command=f'echo {i}'
+
+    t1 = BashOperator(
+        task_id='location',
+        bash_command='pwd'
         )
         
-    def py_task(**kwargs):
-        task_number = kwargs.get('task_number')
-        return 'task number is: {task_number}'
-        
-    for i in range(20):
-        python_task = PythonOperator(
-            task_id=f'python_task {i}'
-            op_kwargs={"task_number": i}
+    def current_date(ds, **kwargs):
+        print(ds)
+        return 'first DAG with python operator'
+            
+    t2 = PythonOperator(
+        task_id='current_date',
+        python_callable=current_date,
         )
-        
-    bash_task >> python_task
+            
+    t1 >> t2
