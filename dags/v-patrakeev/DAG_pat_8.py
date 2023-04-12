29a04f040de6b6ced23fad5e5c6c86b56ea87087
valentinpatrakeev <patrakeevvalentin@gmail.com>
2023-04-12 12:44:46 +0500

    task HW 10

---
+++ dags/v-patrakeev/DAG_pat_8.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+
+def xcom_task_1(ti):
+    return "Airflow tracks everything"
+
+
+def xcom_task_2(ti):
+    print(ti.xcom_pull(key="return_value", task_ids='xcom_pusher'))
+
+
+with DAG(
+        'hw_10_v-patrakeev',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),
+        },
+        start_date=datetime.now()
+) as dag:
+    t1 = PythonOperator(
+        task_id='xcom_pusher',
+        python_callable=xcom_task_1
+    )
+
+    t2 = PythonOperator(
+        task_id='xcom_puller',
+        python_callable=xcom_task_2
+    )
+
+    t1 >> t2
+
