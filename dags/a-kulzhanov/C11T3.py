cfb9b26597ac75bb8bd17d00d312fa1768aad8eb
Asylkhan Kulzhanov <acylhan@mail.ru>
2023-04-15 15:51:02 +0300

    new file C11T3

---
+++ dags/a-kulzhanov/C11T3.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+# def print_da(ds, **kwargs):
+#     print("This is ds date")
+#     print(ds)
+#     return 'Ok'
+#
+
+def print_task(task_number):
+    print(f'task number is: {task_number}')
+
+with DAG(
+    'aakulzhanov_task_2',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+   },
+    description='A simple Task 2',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    for i in range(11):
+        t1 = BashOperator(
+            task_id='print_path',
+            depends_on_past=False,
+            bash_command=f'echo{i}'
+        )
+    for j in range(21):
+        t2 = PythonOperator(
+            task_id='print_ds',
+            op_kwargs={'task_number': j},
+            python_callable=print_task
+        )
+
+    t1 >> t2
