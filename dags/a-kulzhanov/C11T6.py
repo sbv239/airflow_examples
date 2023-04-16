38016e037b4d89e0e2ae3c730bca8454929a060c
Asylkhan Kulzhanov <acylhan@mail.ru>
2023-04-16 12:31:45 +0300

    HW 6 a-kulzhanov

--- dags/a-kulzhanov/C11T6.py
+++ dags/a-kulzhanov/C11T6.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+
 from textwrap import dedent
 
 
