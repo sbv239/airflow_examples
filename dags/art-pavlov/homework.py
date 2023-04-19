f1cc1b097696215fb9621bc43d050aa8340ee65b
Pavlov <artyom_pavlov85@mail.ru>
2023-04-20 01:01:20 +0300

    HW 13 art-pavlov

--- dags/art-pavlov/homework.py
+++ dags/art-pavlov/homework.py
@@ -3,18 +3,28 @@ from textwrap import dedent
 
 from airflow import  DAG
 from airflow.operators.bash import BashOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 
 
-def get_var():
+
+def choose_path():
     from airflow.models import Variable
 
-    var = Variable.get("is_startml")
-    print(var)
-    return var
+    if Variable.get("is_startml") == "True":
+        return 'startml_desc'
+    else:
+        return 'not_startml_desc'
+
+def startml():
+    print("StartML is a starter course for ambitious people")
+    return "Something"
+
+def not_startml():
+    print("Not a startML course, sorry")
+    return "Something"
 
 with DAG(
-    "Variables",
+    "BranchingOperatorTest",
 
 default_args={
     'depends_on_past': False,
@@ -28,10 +38,22 @@ default_args={
     schedule_interval= timedelta(days=1),
     start_date= datetime(2023, 4, 17),
     catchup= False,
-    tags=['idk12']
+    tags=['idk13']
 ) as dag:
 
-    t1 = PythonOperator(
-        task_id="return_var",
-        python_callable=get_var
+    t1 = BranchPythonOperator(
+        task_id="determine_course",
+        python_callable=choose_path,
+        trigger_rule="one_success"
     )
+    TrueStartML = PythonOperator(
+        task_id='startml_desc',
+        python_callable=startml
+    )
+
+    FalseStartML = PythonOperator(
+        task_id='not_startml_desc',
+        python_callable=not_startml
+    )
+
+    t1 >> [TrueStartML, FalseStartML]
