097ad3ec5a69f2e02ddff337e408342cef19c492
Roman Korchagin <korchagin.roma@gmail.com>
2023-04-23 22:00:05 +0300

    hw_5_r-korchagin

---
+++ dags/r-korchagin/hw_5_r-korchagin.py
@@ -0,0 +1,49 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+# Для объявления DAG нужно импортировать класс из airflow
+from airflow import DAG
+
+# Операторы - это кирпичики DAG, они являются звеньями в графе
+# Будем иногда называть операторы тасками (tasks)
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+with DAG(
+    'HW5_rkorch',
+    # Параметры по умолчанию для тасок
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+    # Описание DAG (не тасок, а самого DAG)
+    description='HW3',
+    # Как часто запускать DAG
+    schedule_interval=timedelta(days=1),
+    # С какой даты начать запускать DAG
+    # Каждый DAG "видит" свою "дату запуска"
+    # это когда он предположительно должен был
+    # запуститься. Не всегда совпадает с датой на вашем компьютере
+    start_date=datetime(2022, 1, 1),
+    # Запустить за старые даты относительно сегодня
+    # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+    catchup=False,
+    # теги, способ помечать даги
+    tags=['example'],
+) as dag:
+
+    templated_command = dedent('''
+    {% for i in range(5) %}
+        echo '{{ ts }}'
+        echo '{{ run_id }}'
+    {% endfor %}
+    '''
+    )
+
+    t1 = BashOperator(
+        task_id='templ',
+        bash_command=templated_command
+    )
