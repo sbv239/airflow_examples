d74698be2e8297c4cda687f890828032635ac8e8
Mihail Ponomarev <mihail.ponomarev6@yandex.ru>
2023-04-23 12:26:43 +0300

    add task_3_mi-ponomarev_2

--- dags/mi-ponomarev/task_11_3.py
+++ dags/mi-ponomarev/task_11_3.py
@@ -6,12 +6,12 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-'hw_3_mi-ponomarev',
+        'hw_3_mi-ponomarev',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
         'email_on_failure': False,
-        'email_on_retry': False,
+        'email_on_re            try': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
@@ -24,16 +24,16 @@ with DAG(
 
         for i in range(10):
                 task_1 = BashOperator(
-                        task_id=f"write {i} string",
+                        task_id=f'echo' + str(i),
                         bash_command=f"echo {i}"
                 )
 
-        def write_task_number(task_number):
+        def print_task_number(task_number):
                 print(f"task number is: {task_number}")
 
         for i in range(20):
                 task_2 = PythonOperator(
-                        task_id="",
-                        python_callable=write_task_number,
+                        task_id=f"print" + str(i),
+                        python_callable=print_task_number,
                         op_kwargs={'task_number': i}
                 )
