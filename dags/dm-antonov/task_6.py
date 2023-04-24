ae90619b706382ab53367db4a3f965c9a01c45bf
Dmitry Antonov <Antonov-Dmitry1994@yandex.ru>
2023-04-24 16:47:08 +0300

    task_6 rename DAG

--- dags/dm-antonov/task_6.py
+++ dags/dm-antonov/task_6.py
@@ -4,7 +4,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-        'task_3_dm-antonov',
+        'task_6_dm-antonov',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -13,11 +13,11 @@ with DAG(
             'retries': 1,
             'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
         },
-        description='task_3',
+        description='task_6',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2023, 4, 23),
         catchup=False,
-        tags=['task_3']
+        tags=['task_6']
 ) as dag:
     for i in range(10):
         t1 = BashOperator(
