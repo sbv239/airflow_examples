ef5bff17e7126cf91d2e4699ea20821674363247
Dmitry Antonov <Antonov-Dmitry1994@yandex.ru>
2023-04-24 16:10:57 +0300

    task_6

---
+++ dags/dm-antonov/task_6.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'task_3_dm-antonov',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='task_3',
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2023, 4, 23),
+        catchup=False,
+        tags=['task_3']
+) as dag:
+    for i in range(10):
+        t1 = BashOperator(
+            task_id='echo' + str(i),
+            bash_command='echo $NUMBER',
+            env={'NUMBER': i}
+        )
