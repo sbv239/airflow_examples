626f62fc44b587b00b5fab92df059be7d7b3ba14
Dmitry Antonov <Antonov-Dmitry1994@yandex.ru>
2023-04-25 14:13:11 +0300

    task_9 DAG rename

--- dags/dm-antonov/task_9.py
+++ dags/dm-antonov/task_9.py
@@ -3,7 +3,7 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 with DAG(
-        'task_2_dm-antonov',
+        'task_9_dm-antonov',
         default_args={
             'depends_on_past': False,
             'email': ['airflow@example.com'],
@@ -12,11 +12,11 @@ with DAG(
             'retries': 1,
             'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
         },
-        description='task_2',
+        description='task_9',
         schedule_interval=timedelta(days=1),
         start_date=datetime(2023, 4, 23),
         catchup=False,
-        tags=['task_2']
+        tags=['task_9']
 ) as dag:
     def xcom_push(ti):
         ti.xcom_push(
