5f819dcfae9f30bab33788bc17a4670366f1290e
Alex Savelev <sav240468@gmail.com>
2023-04-26 14:18:08 +0300

    lesson 11 test 7.3

---
+++ dags/al-savelev/hw_7_al-savelev.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+with DAG(
+    'hw_7_al-savelev',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+    },
+    description='test_11_7',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 4, 25),
+    catchup=False,
+    tags=['hw_al-savelev']
+) as dag:
+
+    for i in range(10):
+        t1 = BashOperator(
+            task_id=f'hw_7_al-savelev_bash_{i}',
+            bash_command=f'echo {i}')
+
+    def print_task(task_number, ts, run_id):
+        print(f'task number is: {task_number}')
+        print(ts)
+        print(run_id)
+
+    for i in range(20):
+        t2 = PythonOperator(
+            task_id=f'hw_3_al-savelev_python_{i}',
+            python_callable=print_task,
+            op_kwargs={'task_number': i})
+
+    t1 >> t2
