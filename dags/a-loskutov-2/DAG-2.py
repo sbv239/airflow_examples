8114eb5c2aa26b209fd449f42f36296833474830
a-loskutov-2 <andreyvloskutov@gmail.com>
2023-04-26 13:42:54 +0300

    solutoin is ok

--- dags/a-loskutov-2/DAG-2.py
+++ dags/a-loskutov-2/DAG-2.py
@@ -10,7 +10,7 @@ from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    ' hw_a_loskutov_2',
+    'hw_a_loskutov',
     # Параметры по умолчанию для тасок
 
     default_args={
