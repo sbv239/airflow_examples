bba42cb2e8ec1495ff85bba88b6d8ae7badc3a24
Илья Гарачев <garachev.i@native-force.com>
2023-04-29 21:51:39 +0300

    hw_9_garachev

---
+++ dags/i-garachev-28/hw_garachev_9.py
@@ -0,0 +1,42 @@
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow import DAG
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+
+with DAG(
+    'tutorial',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='hw_garachev_9_dag',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['hw_9'],
+) as dag:
+
+
+    def put_data():
+        return "Airflow tracks everything"
+
+    def get_data(ti):
+        print(ti.xcom_pull(key="return_value", task_ids="hw_9_garachev_put_data_task"))
+
+    t1 = PythonOperator(
+        task_id='hw_9_garachev_put_data_task',
+        python_callable=put_data
+    )
+
+    t2 = PythonOperator(
+        task_id='hw_9_garachev_get_data_task',
+        python_callable=get_data
+    )
+
+    t1 >> t2
