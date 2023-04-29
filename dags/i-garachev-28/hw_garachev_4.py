da0dea8a74db2811263e6fe2a5a2419fe65f5744
Илья Гарачев <garachev.i@native-force.com>
2023-04-29 14:46:09 +0300

    hw_4_garachev

--- dags/i-garachev-28/hw_garachev_4.py
+++ dags/i-garachev-28/hw_garachev_4.py
@@ -19,7 +19,7 @@ with DAG(
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 1, 1),
     catchup=False,
-    tags=['hw_2'],
+    tags=['hw_4'],
 ) as dag:
 
     def print_context(task_number):
@@ -28,8 +28,14 @@ with DAG(
     for i in range(30):
         if i < 10:
             t1 = BashOperator(
-                task_id=f"hw_3_garachev_{i}_bash",
+                task_id=f"hw_4_garachev_{i}_bash",
                 bash_command=f"echo {i}"
+            )
+            t1.doc_md = dedent(
+                f"""\
+                #### абзац {i} 
+                строка `моноширный` **жирный** высот в _курсив_
+                """
             )
             t1
         else:
@@ -37,5 +43,11 @@ with DAG(
                 task_id=f'hw_3_garachev_{i}_python',
                 python_callable=print_context ,
                 op_kwargs = {"task_number": i}
+            )
+            t2.doc_md = dedent(
+                f"""\
+                #### абзац {i} 
+                строка `моноширный` **жирный** высот в _курсив_
+                """
             )
             t2
