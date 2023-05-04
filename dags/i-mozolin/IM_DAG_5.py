4eef1394c6621eb7961e6029730d6c9670e6e9fb
i-mozolin <mozolinia@gmail.com>
2023-05-04 18:43:48 +0300

    HW 5 i-mozolin

---
+++ dags/i-mozolin/IM_DAG_5.py
@@ -0,0 +1,60 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG # Для объявления DAG нужно импортировать класс из airflow
+
+# Операторы - это кирпичики DAG, они являются звеньями в графе
+# Будем иногда называть операторы тасками (tasks)
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'IM_DAG_5',
+    # Параметры по умолчанию для тасок
+    default_args={
+        # Если прошлые запуски упали, надо ли ждать их успеха
+        'depends_on_past': False,
+        # Кому писать при провале
+        'email': ['airflow@example.com'],
+        # А писать ли вообще при провале?
+        'email_on_failure': False,
+        # Писать ли при автоматическом перезапуске по провалу
+        'email_on_retry': False,
+        # Сколько раз пытаться запустить, далее помечать как failed
+        'retries': 1,
+        # Сколько ждать между перезапусками
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+        # Описание DAG (не тасок, а самого DAG)
+        description='My (IM) first DAG',
+        # Как часто запускать DAG
+        schedule_interval=timedelta(days=1),
+        # С какой даты начать запускать DAG
+        # Каждый DAG "видит" свою "дату запуска"
+        # это когда он предположительно должен был
+        # запуститься. Не всегда совпадает с датой на вашем компьютере
+        start_date=datetime(2023, 1, 1),
+        # Запустить за старые даты относительно сегодня
+        # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+        catchup=False,
+        # теги, способ помечать даги
+        tags=['IM_DAG_5'],
+) as dag:
+
+    templated_command = dedent(
+        """
+    {% for i in range(5) %}
+        echo "{{ ts }}"
+        echo "{{ run_id }}"
+    {% endfor %}
+    """
+    )
+
+    t1 = BashOperator(
+        task_id='IM_t1_id5',
+        bash_command=templated_command,
+    )
+
+
+
+    t1
