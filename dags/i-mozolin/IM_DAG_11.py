ba9dd7d82de39eaa46b3e954c407ad7200025808
i-mozolin <mozolinia@gmail.com>
2023-05-06 12:20:11 +0300

    HW 11 i-mozolin

---
+++ dags/i-mozolin/IM_DAG_11.py
@@ -0,0 +1,69 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG # Для объявления DAG нужно импортировать класс из airflow
+
+# Операторы - это кирпичики DAG, они являются звеньями в графе
+# Будем иногда называть операторы тасками (tasks)
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+postgres = PostgresHook(postgres_conn_id="startml_feed")
+with postgres.get_conn() as conn:   # вернет тот же connection, что вернул бы psycopg2.connect(...)
+  with conn.cursor() as cursor:
+      def get_user():
+          cursor.execute("""
+                SELECT user_id, COUNT(action)
+                FROM "feed_action"
+                WHERE action = 'like'
+                GROUP BY user_id
+                ORDER BY COUNT(action) desc
+                LIMIT 1
+            """)
+          result = cursor.fetchone()
+          user_id = result[0]
+          like_count = result[1]
+          return {'user_id': user_id, 'count': like_count}
+
+with DAG(
+    'IM_DAG_11',
+    # Параметры по умолчанию для тасок
+    default_args={
+        # Если прошлые запуски упали, надо ли ждать их успеха
+        'depends_on_past': False,
+        # Кому писать при провале
+        'email': ['airflow@example.com'],
+        # А писать ли вообще при провале?
+        'email_on_failure': False,
+        # Писать ли при автоматическом перезапуске по провалу
+        'email_on_retry': False,
+        # Сколько раз пытаться запустить, далее помечать как failed
+        'retries': 1,
+        # Сколько ждать между перезапусками
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+        # Описание DAG (не тасок, а самого DAG)
+        description='My (IM) first DAG',
+        # Как часто запускать DAG
+        schedule_interval=timedelta(days=1),
+        # С какой даты начать запускать DAG
+        # Каждый DAG "видит" свою "дату запуска"
+        # это когда он предположительно должен был
+        # запуститься. Не всегда совпадает с датой на вашем компьютере
+        start_date=datetime(2023, 1, 1),
+        # Запустить за старые даты относительно сегодня
+        # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+        catchup=False,
+        # теги, способ помечать даги
+        tags=['IM_DAG_11'],
+) as dag:
+
+
+    t1 = PythonOperator(
+        task_id = 'IM_t1_id_12',
+        python_callable = get_user,
+    )
+
+    t1
