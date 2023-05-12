3446f97a9c1bc5a9c34454770032c0136e5afbfc
Ratibor <ratibor.podlesny@gmail.com>
2023-05-13 02:21:52 +0400

    task13

--- dags/p-ratibor-20/task12.py
+++ dags/p-ratibor-20/task12.py
@@ -5,8 +5,9 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+
 with DAG(
-    'ratibor_task6',
+    'ratibor_task12',
     start_date=datetime(2023, 5, 11),
     default_args={
         'depends_on_past': False,
@@ -17,9 +18,12 @@ with DAG(
         'retry_delay': timedelta(minutes=5),
     },
 ) as dag:
-    for i in range(5):
-        bash_task = BashOperator(
-            task_id=f'task{i}',
-            env={"NUMBER": i},
-            bash_command=f"echo $NUMBER"
-        )
+
+    def print_variable():
+        from airflow.models import Variable
+        print(Variable.get("is_startml"))
+
+    xcom_setter = PythonOperator(
+        task_id='print_is_startml',
+        python_callable=print_variable
+    )
