f976c30c42fffa760d3ec595d1d859d8bb510b85
madflytep <alexnder.shirshov@gmail.com>
2023-05-21 13:49:04 +0300

    hw_5 dag

---
+++ dags/al-shirshov/hw_5.py
@@ -0,0 +1,34 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+with DAG(
+    "hw_al-shirshov_5",
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+},
+    start_date=datetime(2023,5,20),
+    catchup=True    
+) as dag: 
+    templated_command = dedent(
+        """
+        {% for i in range(5) %}
+            echo "{{ ts }}"
+            echo "{{ run_id }}"}
+        {% endfor %}
+        """
+    )
+    task = BashOperator(
+        task_id = "bash_template",
+        depends_on_past=False,
+        bash_command = templated_command
+    )
