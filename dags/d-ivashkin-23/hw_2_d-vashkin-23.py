29c87367b618afcd848232824f55b4c21f9afc34
d-ivashkin-23 <dmivashkin@gmail.com>
2023-05-22 16:48:32 +0600

    HW_2_DAG

---
+++ dags/d-ivashkin-23/hw_2_d-vashkin-23.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from datetime import timedelta
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+with DAG (
+    'hw_d-ivashkin-23_2',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='Homework 2-nd step DAG'
+) as dag:
+    t1 = BashOperator(
+        task_id='print_working_directory',
+        bash_command='pwd'
+    )
+
+
+    def print_context(ds, **kwargs):
+        print(kwargs)
+        print(ds)
+        return 'What did Master Yoda say when he saw himself in 4K? HD Am I!'
+
+
+    t2 = PythonOperator(
+        task_id='print_context',
+        python_callable=print_context
+    )
+
+t1 >> t2
