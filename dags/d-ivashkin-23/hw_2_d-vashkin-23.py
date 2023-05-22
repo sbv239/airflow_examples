177f82831540c011cfa49a37f19f9f1099397b5d
d-ivashkin-23 <dmivashkin@gmail.com>
2023-05-22 17:01:21 +0600

    HW_2_DAG

--- dags/d-ivashkin-23/hw_2_d-vashkin-23.py
+++ dags/d-ivashkin-23/hw_2_d-vashkin-23.py
@@ -1,5 +1,5 @@
 from airflow import DAG
-from datetime import timedelta
+from datetime import timedelta, datetime
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 with DAG (
