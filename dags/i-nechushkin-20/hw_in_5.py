e13b74b0bee648415ae04ddefec271dc7aa2cdcc
Igor N <bibigon5@rambler.ru>
2023-05-24 19:20:38 +0300

    DAG 5 - create DAG

--- dags/i-nechushkin-20/hw_in_5.py
+++ dags/i-nechushkin-20/hw_in_5.py
@@ -24,38 +24,27 @@ with DAG(
         'owner': 'i-nechushkin-20'
     },
     start_date=datetime.now(),
-    tags=['Task_5'],
+    tags=['i-nechushkin-20_Task_5'],
 ) as dag:
 
-    for i in range(10):
-        t1 = BashOperator(
-            task_id=f'print_{i+1}',
-            bash_command=f'echo {i+1}',
-        )
+    templated_command = dedent(
+        """
+    {% for i in range(5) %}
+        echo "{{ ts }}"
+        echo "{{ run_id }}"
+    {% endfor %}
+    """
+    ) # шаблонизация через Jinja
+    t = BashOperator(
+        task_id=f'templated',
+        bash_command=templated_command,
+    )
 
-    def print_task_num(task_number):
-        print(f"task number is: {task_number}")
-        return None
-
-    for i in range(20):
-        t2 = PythonOperator(
-            task_id=f'print_task_num{i}',
-            python_callable=print_task_num,
-            op_kwargs={'task_number': i},
-        )
-
-    t1.doc_md = dedent(
+    t.doc_md = dedent(
     """
     ## Task **«t1»** documentation
     This task is repeated *10* times using a `for` loop.
     """
     )
 
-    t2.doc_md = dedent(
-    """
-    ## Task **«t2»** documentation
-    This task is repeated *20* times using a `for` loop and *passes* parameters to the function.
-    """
-    )
-
-    t1 >> t2
+    t
