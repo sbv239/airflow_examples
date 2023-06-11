84e4236774fff16af36d8b436305cb51e6335a74
Дмитрий Кох <dnkdnk7@yandex.ru>
2023-06-11 16:07:47 +0300

    2

--- dags/d-koh/hw_2_d-koh.py
+++ dags/d-koh/hw_2_d-koh.py
@@ -1,26 +1,40 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
 
-date = "{{ ds }}"
-t = BashOperator(
-    task_id="test_pwd",
-    bash_command="pwd ",  # обратите внимание на пробел в конце!
-    # пробел в конце нужен в случае BashOperator из-за проблем с шаблонизацией
-    # вики на проблему https://cwiki.apache.org/confluence/pages/viewpage.action?pageId=62694614
-    # и обсуждение https://github.com/apache/airflow/issues/1017
-)
+with DAG(
+    'HW_2_d-koh',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='First DAG from step 2 of HW Lesson 11',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 2, 10),
+    catchup=False,
+    tags=['first'],
+) as dag:
 
+    t1 = BashOperator(
+        task_id='print_dir',
+        bash_command='pwd',
+    )
 
-def print_context(ds, **kwargs):
-    """Пример PythonOperator"""
-    # Через синтаксис **kwargs можно получить словарь
-    # с настройками Airflow. Значения оттуда могут пригодиться.
-    # Пока нам не нужно
-    print(kwargs)
-    # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
-    print(ds)
-    return 'Whatever you return gets printed in the logs'
+    def print_ds(ds):
+        """Печать ds"""
+        print('Logical date is: ')
+        print(ds)
 
-run_this = PythonOperator(
-    task_id='print_the_context',  # нужен task_id, как и всем операторам
-    python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
-)
+    t2 = PythonOperator(
+        task_id='print_ds',
+        python_callable=print_ds,
+    )
+
+    t1 >> t2
