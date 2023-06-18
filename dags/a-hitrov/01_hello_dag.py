840cfbeb0ad7edbe52a1bb7e1c0c9ba72c0242b2
Андрей Хитров <a.n.khitrov@gmail.com>
2023-06-18 17:52:18 +0000

    HW 2 a-hitrov

--- dags/a-hitrov/01_hello_dag.py
+++ dags/a-hitrov/01_hello_dag.py
@@ -11,8 +11,14 @@ from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
+
+def print_ds(ds, **kwargs):
+    print(ds)
+    print('Variables passed in to the function:', kwargs)
+
+
 dag_params = {
-    'dag_id': 'XA01-hello-dag',
+    'dag_id': 'xxa01-hello-dag',
     'description': __doc__ + ' [description]',  # Высплывающая подсказка?
     'doc_md': __doc__,  # Подробное описание графа?
     'start_date': datetime.datetime(2023, 6, 18),
@@ -26,7 +32,7 @@ dag_params = {
         'retry_delay': datetime.timedelta(minutes=5),
     },
     'catchup': False,
-    'tags': ['xa'],
+    'tags': ['xxa'],
 }
 
 with DAG(**dag_params) as dag:
@@ -38,7 +44,6 @@ with DAG(**dag_params) as dag:
     )
     ds = PythonOperator(
         task_id='ds',
-        python_callable=print,
-        op_args=['{{ ds }}'],
+        python_callable=print_ds,
     )
     pwd >> ds
