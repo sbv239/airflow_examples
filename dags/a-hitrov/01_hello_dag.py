cffacdbe761bae17fe86ded821d7ba691084cac1
a <a@example.com>
2023-06-18 18:31:36 +0300

    Hello, DAG

---
+++ dags/a-hitrov/01_hello_dag.py
@@ -0,0 +1,38 @@
+'''
+Первый DAG на AirFlow.
+
+Выполняет команду `pwd` в рамках BashOperator, а затем распечатывает аргумент `ds` через PythonOperator.
+'''
+import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+dag_params = {
+    'dag_id': '01. Hello, DAG!',
+    'doc_md': __doc__,
+    'start_date': datetime.datetime(2023, 6, 18),
+    'schedule': datetime.timedelta(days=1),
+    'default_args': {
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+}
+
+with DAG(**dag_params):
+    pwd = BashOperator({
+        'task_id': 'pwd',
+        'bash_command': 'pwd',
+        'env': {},
+    })
+    ds = PythonOperator({
+        'task_id': 'ds',
+        python_callable: print,
+        op_args: ['{{ ds }}'],
+    })
+    pwd >> ds
