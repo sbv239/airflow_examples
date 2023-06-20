199dd0abceb350c335bdbaee899333ef68bbc11e
DMI <dmipmit@gmail.com>
2023-06-20 12:40:31 +0300

    HW3_1

--- dags/maks-novikov/hw_2.py
+++ dags/maks-novikov/hw_2.py
@@ -10,7 +10,7 @@ def print_date(ds):
     return f'Current date: {ds}'
     
 with DAG(
-    'hw_2-maks-novikov',
+    'hw_-maks-novikov_3',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -24,35 +24,27 @@ with DAG(
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 6, 16),
     catchup=False,
-    tags=['hw_2-maks-novikov'],
+    tags=['hw_-maks-novikov_3'],
 ) as dag:
     
-    templated_command = dedent(
-        """
-        {% for i in range(10) %}
-        echo "{{ ds }}"
-        echo "{{ macros.ds_add(ds, 7)}}"
-        {% endfor %}
-        """
-        )
-
-    t1 = BashOperator(
-        task_id='templated',
-        depends_on_past=False,
-        bash_command=templated_command,
-        )
+    for i in range(20):
+        t1 = BashOperator(
+            task_id='templated',
+            depends_on_past=False,
+            bash_command=templated_command,
+            )
     
     def my_sleeping_function(random_base):
         time.sleep(random_base)
 
     for i in range(20):
-    # Каждый таск будет спать некое количество секунд
+        # Каждый таск будет спать некое количество секунд
         t2 = PythonOperator(
-        task_id='sleep_for_' + str(i), # в id можно делать все, что разрешают строки в python
-        python_callable=my_sleeping_function,
-        # передаем в аргумент с названием random_base значение float(i) / 10
-        op_kwargs={'random_base': float(i) / 10},
-        )
+            task_id='sleep_for_' + str(i), # в id можно делать все, что разрешают строки в python
+            python_callable=my_sleeping_function,
+            # передаем в аргумент с названием random_base значение float(i) / 10
+            op_kwargs={'random_base': float(i) / 10},
+            )
 # настраиваем зависимости между задачами
 # run_this - это некий таск, объявленный ранее (в этом примере не объявлен)
 
