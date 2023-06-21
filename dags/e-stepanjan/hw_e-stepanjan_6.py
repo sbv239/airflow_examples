b34230a8c59eb0711f1ce6cfd53932e6551347ff
Eduard_S <eduardstepanyan88@yandex.ru>
2023-06-21 19:11:08 +0400

    step 6

---
+++ dags/e-stepanjan/hw_e-stepanjan_6.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'hw_e-stepanjan_6',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='My second training DAG',
+        start_date=datetime(2023, 6, 20),
+        schedule_interval=timedelta(days=1),
+        catchup=False,
+        tags=['e-stepanjan', 'step_6']
+) as dag:
+    def print_task_number(task_number, **kwargs):
+        print(f'task number is: {task_number}')
+
+
+    for i in range(10):
+        NUMBER = i
+        t_bash = BashOperator(
+            task_id=f'BashOperator_{i}',
+            bash_command=f"echo $NUMBER",
+            env={'NUMBER': str(i)}
+        )
