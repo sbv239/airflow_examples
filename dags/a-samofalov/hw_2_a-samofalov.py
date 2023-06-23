296ec2c0490bea9cc286e06f297d41e5955534c7
Aleksandr Samofalov <samofalovwork@yandex.ru>
2023-06-23 20:02:57 +0300

    HW_2_a-samofalov

---
+++ dags/a-samofalov/hw_2_a-samofalov.py
@@ -0,0 +1,65 @@
+"""
+Test documentation
+"""
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+# Для объявления DAG нужно импортировать класс из airflow
+from airflow import DAG
+
+# Операторы - это кирпичики DAG, они являются звеньями в графе
+# Будем иногда называть операторы тасками (tasks)
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'tutorial',
+    # Параметры по умолчанию для тасок
+    default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+    # Описание DAG (не тасок, а самого DAG)
+    description='A simple tutorial DAG',
+    # Как часто запускать DAG
+    schedule_interval=timedelta(days=1),
+    # С какой даты начать запускать DAG
+    # Каждый DAG "видит" свою "дату запуска"
+    # это когда он предположительно должен был
+    # запуститься. Не всегда совпадает с датой на вашем компьютере
+    start_date=datetime(2022, 1, 1),
+    # Запустить за старые даты относительно сегодня
+    # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+    catchup=False,
+    # теги, способ помечать даги
+    tags=['example'],
+) as dag:
+
+
+    t1 = BashOperator(
+        task_id='print_date',  # id, будет отображаться в интерфейсе
+        bash_command='pwd',  # какую bash команду выполнить в этом таске
+    )
+
+
+    def print_context(ds, **kwargs):
+        """Пример PythonOperator"""
+        # Через синтаксис **kwargs можно получить словарь
+        # с настройками Airflow. Значения оттуда могут пригодиться.
+        # Пока нам не нужно
+        print(kwargs)
+        # В ds Airflow за нас подставит текущую логическую дату - строку в формате YYYY-MM-DD
+        print(ds)
+        return 'Whatever you return gets printed in the logs'
+
+
+    t2 = PythonOperator(
+        task_id='print_the_context',  # нужен task_id, как и всем операторам
+        python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
+    )
+    # А вот так в Airflow указывается последовательность задач
+    t1 >> t2
