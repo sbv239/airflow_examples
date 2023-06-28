996f64c3c7da472fe1c906ed6d6f7324b7873cf3
Max Sazonov <sazonov.maksim.v@gmail.com>
2023-06-28 23:56:12 +0300

    hw_10_msazonov

---
+++ dags/m-sazonov/hw_10_m-sazonov.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from textwrap import dedent
+
+with DAG(
+    'hw_10_m-sazonov',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    },
+    description='hw_10_m-sazonov',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 6, 27),
+    catchup=False,
+) as dag:
+
+    def push_xcom():
+        return "Airflow tracks everything"
+    def pull_xcom(ti):
+            task = ti.xcom_pull(
+                key='return_value',
+                task_ids='push_xcom'
+            )
+            print(task)
+
+    t1 = PythonOperator(
+            task_id='push_xcom',
+            python_callable=push_xcom)
+    t2 = PythonOperator(
+            task_id='pull_xcom',
+            python_callable=pull_xcom,
+        )
+    t1 >> t2
