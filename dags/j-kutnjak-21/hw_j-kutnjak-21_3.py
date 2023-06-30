16d19defbe99d1bf687e0607e64e57a4cebbcc7a
Yulia Kutnyak <julia.kutnyak@yandex.ru>
2023-06-30 17:14:54 +0300

    task 3

---
+++ dags/j-kutnjak-21/hw_j-kutnjak-21_3.py
@@ -0,0 +1,41 @@
+"""
+Test documentation
+"""
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+with DAG(
+    'hw_j-kutnjak-21_3',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 6, 30),
+    catchup=False,
+    tags=['example'],
+) as dag:
+
+    for i in range(10):
+        t1 = BashOperator(
+            task_id='j-kutnjak-21_task3-1_' + str(i),
+            bash_command=f"echo {i}",
+        )
+
+    for i in range(20):
+        def print_number(task_number = {i}):
+            print(f"task number is: {task_number}")
+
+        t2 = PythonOperator(
+            task_id='print_number_' + str(i),
+            python_callable=print_number,
+            op_kwargs={'id': i}
+        )
