174352e5f6b12c460d3ad4dc92c10971d6ae7e2d
nikita@greatwormhole <nikita.chistyakov2002@gmail.com>
2023-06-30 15:03:43 +0300

    Second task

---
+++ dags/n-chistjakov/task_3.py
@@ -0,0 +1,42 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+ID = "hw_3_n-chistjakov_"
+
+def print_info(task_number):
+    print(f'task number is: {task_number}')
+
+with DAG(
+    'hw_3_n-chistjakov',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    },
+    description="Second task",
+    start_date=datetime(2023, 6, 30),
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+    tags=["task_03"],
+) as dag:
+    for i in range(10):
+        bash = BashOperator(
+            task_id=ID + f"1_{i}",
+            bash_command=f"echo {i}"    
+        )
+
+    for i in range(10, 30):
+        pyth = PythonOperator(
+            task_id=ID + f"2_{i}",
+            python_callable=print_info,
+            op_kwargs={'task_number': i}
+        )
+
+    bash >> pyth
