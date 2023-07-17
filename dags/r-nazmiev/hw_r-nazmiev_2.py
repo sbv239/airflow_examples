9b6d4641ce7c9891dad4b7c60257a2e9c96f8929
Radion Nazmiev <radionnazmiev@gmail.com>
2023-07-17 14:30:49 -0700

    add DAG #2

---
+++ dags/r-nazmiev/hw_r-nazmiev_2.py
@@ -0,0 +1,40 @@
+"""
+My first DAG
+"""
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.BashOperator import BashOperator
+from airflow.operators.PythonOperator import PythonOperator
+
+with DAG(
+    'hw_r-nazmiev_2',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5), 
+    },
+    description='A simple practice DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023,7,17),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    
+    def print_context(ds):
+        print(ds)
+
+    
+    print_ds = PythonOperator(
+        task_id='print_ds', 
+        python_callable=print_context,
+    )
+
+    pwd = BashOperator(
+        task_id='print_directory_path',
+        bash_command='pwd',
+    )
+
+    pwd >> print_ds
