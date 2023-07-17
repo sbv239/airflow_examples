5223b6c28d7dd158e9ab1be8509168b3e5bea2ef
Andrey Fawkeen <65026452+avfawkes@users.noreply.github.com>
2023-07-17 20:52:17 +0400

    experiment1

--- dags/and-f/hw_2_and-f.py
+++ dags/and-f/hw_2_and-f.py
@@ -9,12 +9,13 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 
-def first_python_operator(ds, *args, **kwargs):
+def first_python_operator(*args, **kwargs):
     # from loguru import logger
     # logger.info(args)
     # logger.info(kwargs)
     # print(kwargs['ds'])
-    print(ds)
+    print(args)
+    print(kwargs)
 
 
 default_args={
@@ -27,7 +28,7 @@ default_args={
     'retry_delay': timedelta(minutes=5),
 }
 
-with DAG(dag_id='hw_2_avf',
+with DAG(dag_id='hw_2_and-f',
          default_args=default_args,
          description='--DAG description here--',
          schedule_interval=timedelta(days=1),
@@ -41,4 +42,4 @@ with DAG(dag_id='hw_2_avf',
     t2 = PythonOperator(task_id='first_python_operator', python_callable=first_python_operator)
     t2.doc_md = dedent("""#### Task Documentation in markdown for 'first_python_operator' task""")
 
-    t1 >> t2
+    [t1, t2]
