9d3da2dfdbadf877a7e2b61d706b5323eb007c3a
Pavel Pertsov <pavel.pertsov@gmail.com>
2023-07-22 15:07:18 +0100

    hw_2

---
+++ dags/p-pertsov-36/hw_p-pertsov-36_2.py
@@ -0,0 +1,39 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'hw_2',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='hw_2 DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 7, 21),
+    catchup=False,
+    tags=['homework_2'],
+) as dag:
+
+    t1 = BashOperator(
+        task_id='print_directory',
+        bash_command='pwd',
+    )
+
+def print_ds(ds):
+    print(ds)
+    return 'Whatever you return gets printed in the logs'
+
+run_this = PythonOperator(
+    task_id='print_ds',
+    python_callable=print_ds,
+)
+
+t1 >> run_this
