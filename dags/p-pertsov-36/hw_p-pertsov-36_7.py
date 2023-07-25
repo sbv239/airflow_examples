42d416f056df578686caa792600fd2c407075de8
Pavel Pertsov <pavel.pertsov@gmail.com>
2023-07-25 14:39:35 +0100

    hw 7 update

--- dags/p-pertsov-36/hw_p-pertsov-36_7.py
+++ dags/p-pertsov-36/hw_p-pertsov-36_7.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'hw_p-pertsov-36_6',
+    'hw_p-pertsov-36_7',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -19,13 +19,13 @@ with DAG(
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 7, 21),
     catchup=False,
-    tags=['pavelp_hw_6'],
+    tags=['pavelp_hw_7'],
 ) as dag:
 
     for i in range(1, 31):  # 10 задач сделайте типа BashOperatort
         if i <= 10:
             t1 = BashOperator(
-                task_id=f'hw_3_p-pertsov-36_{i}',  # id, будет отображаться в интерфейсе
+                task_id=f'hw_7_p-pertsov-36_{i}',  # id, будет отображаться в интерфейсе
                 bash_command=f'echo {i} ',  # можете указать f"echo {i}"
                 dag=dag
             )
@@ -37,7 +37,7 @@ with DAG(
 
 
             t2 = PythonOperator(
-                task_id=f'hw_6_p-pertsov-36_{i}',
+                task_id=f'hw_7_p-pertsov-36_{i}',
                 python_callable=print_task_number,
                 op_kwargs={'task_number': i},
             )
