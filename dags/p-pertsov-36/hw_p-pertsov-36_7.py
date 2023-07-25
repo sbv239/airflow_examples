0448435dfff4b252786f657580c8a2e56f88afc8
Pavel Pertsov <pavel.pertsov@gmail.com>
2023-07-25 14:57:03 +0100

    hw_7 another update

--- dags/p-pertsov-36/hw_p-pertsov-36_7.py
+++ dags/p-pertsov-36/hw_p-pertsov-36_7.py
@@ -22,11 +22,11 @@ with DAG(
     tags=['pavelp_hw_7'],
 ) as dag:
 
-    for i in range(1, 31):  # 10 задач сделайте типа BashOperatort
+    for i in range(1, 31):
         if i <= 10:
             t1 = BashOperator(
-                task_id=f'hw_7_p-pertsov-36_{i}',  # id, будет отображаться в интерфейсе
-                bash_command=f'echo {i} ',  # можете указать f"echo {i}"
+                task_id=f'hw_7_p-pertsov-36_{i}', 
+                bash_command=f'echo {i} ',
                 dag=dag
             )
         else:
@@ -37,7 +37,7 @@ with DAG(
 
 
             t2 = PythonOperator(
-                task_id=f'hw_7_p-pertsov-36_{i}',
+                task_id=f'hw_7_p-pertsov-36_{task_number[i]}',
                 python_callable=print_task_number,
                 op_kwargs={'task_number': i},
             )
