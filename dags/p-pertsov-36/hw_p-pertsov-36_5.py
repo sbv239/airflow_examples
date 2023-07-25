4796779aa3583886fb94c7e1093987256c1327d3
Pavel Pertsov <pavel.pertsov@gmail.com>
2023-07-25 12:47:50 +0100

    hw_5_ppertsov

---
+++ dags/p-pertsov-36/hw_p-pertsov-36_5.py
@@ -0,0 +1,43 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'hw_p-pertsov-36_5',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='template operators example',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 7, 21),
+    catchup=False,
+    tags=['pavelp_hw_5'],
+) as dag:
+
+        templated_command = dedent(
+                """
+            {% for i in range(5) %}
+                echo "{{ ts }}"
+                echo "{{ run_id}}"
+            {% endfor %}
+            """
+        )
+
+        t1 = BashOperator(
+                task_id='hw_5_templated',
+                depends_on_past=False,
+                bash_command=templated_command,
+        )
+
+        t1
+
+
