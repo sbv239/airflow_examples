2df86a2459b5f535582637542ec335b4642b1fe5
nbagro20 <nbagro@yandex.ru>
2023-08-21 23:40:46 +0300

    lesson_11_step_2

---
+++ dags/n-bagro-20/hw_2_n-bagro-20.py
@@ -0,0 +1,32 @@
+from airflow.operators.bash import BashOperator
+from airflow import DAG
+from datetime import datetime, timedelta
+
+with DAG (
+    "hw_2_n-bagro-20",
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description = 'lesson_11_step_2',
+    shedule_interval = timedelta(days=1),
+    start_date = datetime(2023, 08, 21),
+    catchup = False,
+) as dag:
+    t1 = BashOperator(
+        task_id = 'print_pwd',
+        bash_command = 'pwd',
+    )
+    def print_context(ds):
+        print(ds)
+
+    t2 = PythonOperator(
+        task_id = 'print_ds'
+        python_callable = print_context,
+    )
+
+    t1 >> t2
