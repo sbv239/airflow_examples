f948fe26cc195690c7fb16948efe45c11003f15e
Evdon <evdokimovpetrd@gmail.com>
2023-08-21 22:35:23 +0400

    hw_p-evdokimov_6

--- dags/p-evdokimov/hw_p-evdokimov_6.py
+++ dags/p-evdokimov/hw_p-evdokimov_6.py
@@ -5,7 +5,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'hw_p-evdokimov_3',
+    'hw_p-evdokimov_6',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -14,19 +14,18 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
-    description='hw_p-evdokimov_3',
+    description='hw_p-evdokimov_6',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 1, 1),
     catchup=False,
-    tags=['hw_p-evdokimov_3'],
+    tags=['p-evdokimov_6'],
 ) as dag:
-    def print_number(task_number):
-        print(f'task number is: {task_number}')
 
     for i in range(10):
         t1 = BashOperator(
             task_id='counter_num_' + str(i),
-            bash_command=f'echo {i}',
+            bash_command=f'echo $NUMBER',
+            env={'NUMBER':str(i)},
         )
         t1.doc_md = dedent(
         """
@@ -34,16 +33,4 @@ with DAG(
         `code` **bald** _italic_ 
         """
         ) 
-    for i in range(20):
-        t2 = PythonOperator(
-            task_id='task_num_' + str(i),
-            python_callable=print_number,
-            op_kwargs={'task_number': i}
-        )
-        t1.doc_md = dedent(
-        """
-        #### paragraph
-        `code` **bald** _italic_ 
-        """
-        )
-t1 >> t2
+t1
