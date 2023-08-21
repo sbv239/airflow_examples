236224d0491b45fe20d652b5dca9f4c6503cc15d
Barchynai Kimsanova <kimsanova@iamo.de>
2023-08-21 14:50:54 +0200

    Add my first Python script

---
+++ dags/b-kimsanova-22/hw_1.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+def print_date(ds, **kwargs):
+    print("Current Date:", ds)
+
+default_args = {
+    'owner': 'kimsanova',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 8, 21),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+dag = DAG(
+    'hw_your_username_1',  # Replace 'your_username' with your actual username
+    default_args=default_args,
+    schedule_interval=timedelta(days=1),
+    catchup=False,
+)
+
+bash_task = BashOperator(
+    task_id='print_working_directory',
+    bash_command='pwd',
+    dag=dag,
+)
+
+python_task = PythonOperator(
+    task_id='print_date_task',
+    python_callable=print_date,
+    provide_context=True,
+    dag=dag,
+)
+
+bash_task >> python_task  # Set the dependency order
+
