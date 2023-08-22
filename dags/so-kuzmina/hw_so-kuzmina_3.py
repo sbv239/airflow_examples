b0304547aed7ffc6df7f946f8238bb6f9341d2bc
Sonya Kuzmina <s@ssivan.ru>
2023-08-22 11:40:29 +0300

    gotovo ne my

--- dags/so-kuzmina/hw_so-kuzmina_3.py
+++ dags/so-kuzmina/hw_so-kuzmina_3.py
@@ -1,4 +1,4 @@
-from datetime import datetime, timedelta
+'''from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
@@ -6,7 +6,7 @@ from airflow.operators.python_operator import PythonOperator
 default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
-    'start_date': datetime(2023, 8, 15),
+    'start_date': datetime(2023, 8, 21),
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
@@ -46,3 +46,38 @@ with dag:
             python_tasks.append(task)
             if i > 10:
                 python_tasks[i - 1] >> task  # Устанавливаем зависимость от предыдущей python-задачи
+'''
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
+
+def print_i(task_number, **kwargs):
+    print(f"task number is: {task_number}")
+
+with DAG(
+    'hw_so-kuzmina_3',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}, description = 'dag with for',
+    schedule_interval = timedelta(days = 1),
+    start_date = datetime(2023, 8, 20)
+) as dag:
+    for i in range(30):
+        if i < 10:
+            t1 = BashOperator(
+                task_id = f'bash_task_{i}',
+                bash_command = f'echo {i}'
+            )
+        else:
+            t2 = PythonOperator(
+                task_id = f'python_task_{i}',
+                python_callable = print_i,
+                op_kwargs = {'task_id': f'{i}'}
+            )
