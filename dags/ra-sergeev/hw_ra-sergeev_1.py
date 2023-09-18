f43e28caa6f42eadd75b680ca1efb56e307676ba
RatSeOff <ratseoff@gmail.com>
2023-09-18 06:57:15 +0500

    add hw_ra-sergeev_1.py

---
+++ dags/ra-sergeev/hw_ra-sergeev_1.py
@@ -0,0 +1,36 @@
+"""
+Create BashOperator - pwd & PythonOperator - print ds
+"""
+from datetime import timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'hw_ra-sergeev_1',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        description='my_first_dag'
+) as dag:
+    t1 = BashOperator(
+        task_id='get_directory',
+        bash_command='pwd'
+    )
+
+
+    def print_ds(ds):
+        print(ds)
+        return f'Logic time is {ds}'
+
+
+    t2 = PythonOperator(
+        task_id='',
+        python_callable=print_ds
+    )
+    t1 >> t2
