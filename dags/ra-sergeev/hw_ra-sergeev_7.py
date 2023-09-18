963a7b9296add66e92a3eacea0b23e7a2d26e987
RatSeOff <ratseoff@gmail.com>
2023-09-18 09:07:20 +0500

    fix? hw_ra-sergeev_7

--- dags/ra-sergeev/hw_ra-sergeev_7.py
+++ dags/ra-sergeev/hw_ra-sergeev_7.py
@@ -2,7 +2,6 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
-from textwrap import dedent
 
 with DAG(
         'hw_ra-sergeev_7',
@@ -16,22 +15,26 @@ with DAG(
         },
         description='task_7',
         schedule_interval=timedelta(days=1),
-        start_date=datetime(2023, 2, 11),
+        start_date=datetime(2023, 9, 17),
         catchup=False,
         tags=['hw_ra-sergeev_7']
 ) as dag:
     for i in range(10):
         t1 = BashOperator(
-            task_id='bash_'+f'{i}',
+            task_id='bash_' + f'{i}',
             bash_command=f'echo {i}'
         )
-    def print_ts_run_id(ts, run_id, task_number, **kwargs):
+
+
+    def print_ts_run_id(ts, run_id, **kwargs):
+        task_number = kwargs['task_number']
         print(ts, run_id, end='\n')
-        return ts, run_id
+        return ts, run_id, task_number
+
 
     for i in range(20):
         t2 = PythonOperator(
-            task_id='python_'+f'{i}',
+            task_id='python_' + f'{i}',
             python_callable=print_ts_run_id,
             op_kwargs={'task_number': i}
         )
