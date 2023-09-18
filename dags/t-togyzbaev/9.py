b5c2483c1c21920ede0f30de7ac966dcb60ef33d
Timur Togyzbaev <tim.togyzbaev@gmail.com>
2023-09-19 02:13:32 +0300

    HW 9 t-togyzbaev

---
+++ dags/t-togyzbaev/9.py
@@ -0,0 +1,41 @@
+"""
+DAG for task 9
+"""
+from datetime import timedelta, datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+        'hw_t-togyzbaev_9',
+        default_args={
+            'depends_on_past': False,
+            'email': ['airflow@example.com'],
+            'email_on_failure': False,
+            'email_on_retry': False,
+            'retries': 1,
+            'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+        },
+        schedule_interval=timedelta(days=1),
+        start_date=datetime(2022, 1, 1),
+        catchup=False,
+        tags=['t-togyzbaev']
+) as dag:
+    def put_in(ti):
+        ti.xcom_push(key="sample_xcom_key", value="some key")
+
+    def print_value(ti):
+        val = ti.xcom_pull(key="sample_xcom_key", task_ids="put value")
+        print("valud in xcom: ", val)
+
+    t1 = PythonOperator(
+        task_id="put value",
+        python_callable=put_in
+    )
+    t2 = PythonOperator(
+        task_id="print value",
+        python_callable=print_value
+    )
+
+    t1 >> t2
