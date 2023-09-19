77da989281c16fec10265409b3aa9612577337c5
Nataliia Kniazeva <knyazeva.n89@gmail.com>
2023-09-19 16:10:53 +0400

    hw_2

---
+++ dags/n-knjazeva-24/hw_n-knjazeva-24_2.py
@@ -0,0 +1,37 @@
+"""
+hw_n-knjazeva-24_2
+"""
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datetime import timedelta
+
+
+def print_ds(ds, **kwargs):
+    print(ds)
+    return 'Ds printed'
+
+with DAG(
+    'hw_n-knyazeva-24_2',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    }
+) as dag:
+
+    t1 = BashOperator(
+        task_id='print_pwd',
+        bash_command='pwd'
+    )
+
+    t2 = PythonOperator(
+        task_id='print_ds',
+        python_callable=print_ds
+    )
+
+    t1 >> t2
+
