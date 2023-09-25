cfcc0c4af28309972f74db51c40cdd76752ba638
koryakovda <koryakovda@gmail.com>
2023-09-25 21:46:03 +0900

    hw10

--- dags/d-korjakov/hw_10_d-korjakov.py
+++ dags/d-korjakov/hw_10_d-korjakov.py
@@ -5,16 +5,13 @@ from datetime import datetime, timedelta
 from textwrap import dedent
 
 
-def python_operator_put(ti):
-    ti.xcom_push(
-        key='sample_xcom_key',
-        value='xcom test',
-    )
+def python_operator_one():
+    return "Airflow tracks everything"
 
 
-def python_operator_pull(ti):
+def python_operator_two(ti):
     xcom_pull = ti.xcom_pull(
-        key='sample_xcom_key',
+        key='return_value',
         task_ids='push_data',
     )
     return print(xcom_pull)
@@ -29,19 +26,19 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-with DAG('hw_9_d-korjakov',
+with DAG('hw_10_d-korjakov',
          default_args=default_args,
-         description='DAG with "for" BashOperator and PythonOperator',
+         description='DAG with PythonOperator and PythonOperator',
          start_date=datetime(2023, 9, 24),
          schedule_interval=timedelta(days=1)
          ) as dag:
     t1 = PythonOperator(
         task_id='push_data',
-        python_callable=python_operator_put,
+        python_callable=python_operator_one,
     )
     t2 = PythonOperator(
         task_id='pull_data',
-        python_callable=python_operator_pull,
+        python_callable=python_operator_two,
     )
 
     t1 >> t2
