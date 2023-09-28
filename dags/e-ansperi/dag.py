15187a58638f5ff254186aca5f2c5fe1cd41e620
Eduard Ansperi <circa.shoes@gmail.com>
2023-09-28 17:03:19 +0300

    add dag for task 10

--- dags/e-ansperi/dag.py
+++ dags/e-ansperi/dag.py
@@ -13,43 +13,40 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-# Function to push value to XCom
-def push_to_xcom(ti, **kwargs):
-    key = 'sample_xcom_key'
-    value = 'xcom test'
-    ti.xcom_push(key=key, value=value)
+# Function to return a string
+def push_string_to_xcom(**kwargs):
+    return "Airflow tracks everything"
 
-# Function to pull value from XCom and print it
-def pull_from_xcom(ti, **kwargs):
-    key = 'sample_xcom_key'
-    value = ti.xcom_pull(key=key, task_ids='push_value_task')
-    print(f"Value from XCom with key '{key}': {value}")
+# Function to pull the string from XCom and print it
+def pull_string_from_xcom(ti, **kwargs):
+    value = ti.xcom_pull(key='return_value', task_ids='push_string_task')
+    print(f"Value from XCom: {value}")
 
 # Define the DAG, its schedule, and set it to run
 dag = DAG(
-    'hw_e-ansperi_9',
+    'hw_e-ansperi_10',
     default_args=default_args,
-    description='A DAG with two PythonOperators working with XCom',
+    description='A DAG with two PythonOperators demonstrating implicit XCom',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 9, 28),
     catchup=False
 )
 
-# Task using PythonOperator to push value to XCom
-push_value_task = PythonOperator(
-    task_id='push_value_task',
-    python_callable=push_to_xcom,
+# Task using PythonOperator to push a string to XCom
+push_string_task = PythonOperator(
+    task_id='push_string_task',
+    python_callable=push_string_to_xcom,
     provide_context=True,
     dag=dag
 )
 
-# Task using PythonOperator to pull value from XCom and print it
-pull_value_task = PythonOperator(
-    task_id='pull_value_task',
-    python_callable=pull_from_xcom,
+# Task using PythonOperator to pull the string from XCom and print it
+pull_string_task = PythonOperator(
+    task_id='pull_string_task',
+    python_callable=pull_string_from_xcom,
     provide_context=True,
     dag=dag
 )
 
 # Set task order
-push_value_task >> pull_value_task
+push_string_task >> pull_string_task
