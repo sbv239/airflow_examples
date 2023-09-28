49c5a55b8fd0cdb3ac43d311ab7055b0180980c3
Eduard Ansperi <circa.shoes@gmail.com>
2023-09-28 16:28:37 +0300

    add dag for task 6

--- dags/e-ansperi/dag.py
+++ dags/e-ansperi/dag.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.bash import BashOperator
 
 # Define the default arguments
@@ -13,27 +14,35 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-# Define the templated command
-templated_command = '''
-for i in {0..4}; do
-    echo "Value of ts: {{ ts }}"
-    echo "Value of run_id: {{ run_id }}"
-done
-'''
+# Define the Python function for the PythonOperator
+def print_task_number(task_number, **kwargs):
+    print(f"task number is: {task_number}")
 
 # Define the DAG, its schedule, and set it to run
 dag = DAG(
-    'hw_e-ansperi_5',
+    'hw_e-ansperi_6',
     default_args=default_args,
-    description='A DAG with a single BashOperator using templated command',
+    description='A DAG with tasks declared using a for loop and environment variable',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 9, 28),
     catchup=False
 )
 
-# Task using BashOperator with templated command
-bash_task = BashOperator(
-    task_id='print_ts_and_run_id',
-    bash_command=templated_command,
-    dag=dag
-)
+# First 10 tasks using BashOperator with environment variable
+for i in range(10):
+    bash_task = BashOperator(
+        task_id=f'bash_task_{i}',
+        bash_command="echo $NUMBER",
+        env={'NUMBER': str(i)},
+        dag=dag
+    )
+
+# Next 20 tasks using PythonOperator
+for i in range(10, 30):
+    python_task = PythonOperator(
+        task_id=f'python_task_{i}',
+        python_callable=print_task_number,
+        op_kwargs={'task_number': i},
+        provide_context=True,
+        dag=dag
+    )
