7c8cff40066808e1b6c862dbac08d3d2248d2575
norderus <ilya.dan.work@gmail.com>
2023-09-28 18:32:10 +0300

    some fixes

--- dags/i-daniljuk/hw4.py
+++ dags/i-daniljuk/hw4.py
@@ -20,11 +20,11 @@ with DAG(
     'retries': 1,
     'retry_delay': timedelta(minutes=5)
     },
-    description='A cycle tasks DAG',
+    description='templated DAG',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2022, 1, 1),
     catchup=False,
-    tags=['first_dag'],
+    tags=['templated'],
 ) as dag:
     
     i = 0
@@ -39,22 +39,3 @@ with DAG(
             contains function `echo {i}`
         """
         i += 1
-
-    def print_funcnum(task_number):
-        """Заснуть на random_base секунд"""
-        print(f"task number is: {task_number}")
-
-    while i < 30:
-        # Каждый таск будет спать некое количество секунд
-        task_python = PythonOperator(
-            task_id='task_number_' + str(i),
-            python_callable=print_funcnum,
-            op_kwargs={'task_number': i},
-        )
-        task_python.doc_md = """\
-            **task_python** makes print func *twenty* times
-            **task_python** contains function `print_funcnum` and print func number
-        """
-        i += 1
-
-    task_bash >> task_python
