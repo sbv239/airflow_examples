d8faf911d160b0fa845a24498deb5b57c04413c1
Eduard Ansperi <circa.shoes@gmail.com>
2023-09-28 16:08:49 +0300

    add dag.py for task 3

--- dags/e-ansperi/dag.py
+++ dags/e-ansperi/dag.py
@@ -15,35 +15,33 @@ default_args = {
 }
 
 # Define the Python function for the PythonOperator
-def print_context(ds, **kwargs):
-    print(kwargs)
-    print(ds)
-    return 'Whatever you return gets printed in the logs'
+def print_task_number(task_number, **kwargs):
+    print(f"task number is: {task_number}")
 
 # Define the DAG, its schedule, and set it to run
 dag = DAG(
-    'HW_2_e-ansperi',
+    'hw_e-ansperi_3',
     default_args=default_args,
-    description='A refined DAG with BashOperator and PythonOperator',
+    description='A DAG with tasks declared using a for loop',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 9, 28),
     catchup=False
 )
 
-# Define the task using a BashOperator
-bash_task = BashOperator(
-    task_id='print_working_directory',
-    bash_command='pwd',
-    dag=dag
-)
+# First 10 tasks using BashOperator
+for i in range(10):
+    bash_task = BashOperator(
+        task_id=f'bash_task_{i}',
+        bash_command=f'echo {i}',
+        dag=dag
+    )
 
-# Define the task using a PythonOperator
-python_task = PythonOperator(
-    task_id='print_ds_and_context',
-    python_callable=print_context,
-    provide_context=True,  # This is required to access the ds variable and other context
-    dag=dag
-)
-
-# Set the task order
-bash_task >> python_task
+# Next 20 tasks using PythonOperator
+for i in range(10, 30):
+    python_task = PythonOperator(
+        task_id=f'python_task_{i}',
+        python_callable=print_task_number,
+        op_kwargs={'task_number': i},
+        provide_context=True,
+        dag=dag
+    )
