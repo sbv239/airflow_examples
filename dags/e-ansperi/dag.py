d6bbf459d206c3def75368c11b1e4007e0c0dedc
Eduard Ansperi <circa.shoes@gmail.com>
2023-09-28 17:33:17 +0300

    add dag for task 11

--- dags/e-ansperi/dag.py
+++ dags/e-ansperi/dag.py
@@ -1,6 +1,7 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHook
 
 # Define the default arguments
 default_args = {
@@ -13,40 +14,45 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-# Function to return a string
-def push_string_to_xcom(**kwargs):
-    return "Airflow tracks everything"
 
-# Function to pull the string from XCom and print it
-def pull_string_from_xcom(ti, **kwargs):
-    value = ti.xcom_pull(key='return_value', task_ids='push_string_task')
-    print(f"Value from XCom: {value}")
+# Function to retrieve the user who has the most likes
+def get_top_user(**kwargs):
+    query = '''
+    SELECT user_id, COUNT(*) as count 
+    FROM "feed_action" 
+    WHERE action = 'like' 
+    GROUP BY user_id 
+    ORDER BY count DESC 
+    LIMIT 1
+    '''
+
+    # Using PostgresHook to connect to the database and execute the query
+    postgres = PostgresHook(postgres_conn_id='startml_feed')
+    with postgres.get_conn() as conn:
+        with conn.cursor() as cursor:
+            cursor.execute(query)
+            result = cursor.fetchone()
+            top_user = {'user_id': result[0], 'count': result[1]}
+            print(top_user)  # Additional print statement for debugging
+
+    # The returned value will be saved to XCom
+    return top_user
+
 
 # Define the DAG, its schedule, and set it to run
 dag = DAG(
-    'hw_e-ansperi_10',
+    'hw_e-ansperi_11',
     default_args=default_args,
-    description='A DAG with two PythonOperators demonstrating implicit XCom',
+    description='A DAG with a PythonOperator to find the user with the most likes',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 9, 28),
     catchup=False
 )
 
-# Task using PythonOperator to push a string to XCom
-push_string_task = PythonOperator(
-    task_id='push_string_task',
-    python_callable=push_string_to_xcom,
+# Task using PythonOperator to retrieve and return the top user
+top_user_task = PythonOperator(
+    task_id='get_top_user_task',
+    python_callable=get_top_user,
     provide_context=True,
     dag=dag
 )
-
-# Task using PythonOperator to pull the string from XCom and print it
-pull_string_task = PythonOperator(
-    task_id='pull_string_task',
-    python_callable=pull_string_from_xcom,
-    provide_context=True,
-    dag=dag
-)
-
-# Set task order
-push_string_task >> pull_string_task
