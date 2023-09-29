ee89d50a56e830b516e30e87976dbe1488f574f3
Maksim Vdovin <vdmax@rambler.ru>
2023-09-29 19:01:15 +0500

    hw_2

---
+++ dags/mak-vdovin/hw_2_mak-vdovin.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'first_dag',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='home work "first dag"',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 9, 28),
+    catchup=False,
+    tags=['hw_2'],
+) as dag:
+
+    t1 = BashOperator(
+        task_id='print_pwd',
+        bash_command='pwd',
+    )
+
+    def print_ds(ds):
+        print(ds)
+
+    t2 = PythonOperator(
+        task_id='print_ds',
+        python_callable=print_ds,
+    )
+
+    t1 >> t2
+
+    if __name__ == "__main__":
+        dag.test()
