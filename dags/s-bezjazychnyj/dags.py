e90532e20393d8f290038923691dd4513ca952c5
s-bezjazychnyj <makov.semen@mail.ru>
2023-10-06 16:34:55 +0300

    fix 3

--- dags/s-bezjazychnyj/dags.py
+++ dags/s-bezjazychnyj/dags.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 with DAG(
-    dag_name='hw_s-bezjazychnyj',
+    'hw_s-bezjazychnyj',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -13,6 +13,7 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
     },
+    description='First DAG',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 10, 6),
     catchup=False,
