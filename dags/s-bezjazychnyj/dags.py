10750f926bde86c98a37040423e81e3b80ea8ec3
s-bezjazychnyj <makov.semen@mail.ru>
2023-10-06 14:56:06 +0300

    airflow2

---
+++ dags/s-bezjazychnyj/dags.py
@@ -0,0 +1,27 @@
+from airflow improt DAG
+from textwrap import dedent
+
+with DAG(
+    'hw_s-bezjazychnyj_1',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}
+
+) as dag:
+    t1=BashOperator(
+        task_id='print_pwd',
+        bash_command='pwd ',
+        dag=dag,
+    )
+    def print_ds(ds):
+        print(ds)
+    t2=PythonOperator(
+        task_id='prind_ds',
+        python_callable=print_ds,
+    )
+    t1>>t2
