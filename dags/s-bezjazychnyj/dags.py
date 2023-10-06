e21be1ba04e852e2a282f8882f74a68985e5c4bf
s-bezjazychnyj <makov.semen@mail.ru>
2023-10-06 15:31:55 +0300

    fix

--- dags/s-bezjazychnyj/dags.py
+++ dags/s-bezjazychnyj/dags.py
@@ -1,10 +1,10 @@
 from airflow import DAG
 from textwrap import dedent
-from datetime import timedelta
+from datetime import datetime, timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 with DAG(
-    'hw_s-bezjazychnyj_1',
+    dag_name='hw_s-bezjazychnyj_1',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -12,12 +12,14 @@ with DAG(
         'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-}
-
+    },
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 10, 6),
+    catchup=False,
 ) as dag:
     t1=BashOperator(
         task_id='print_pwd',
-        bash_command='pwd ',
+        bash_command='pwd',
     )
     def print_ds(ds,**kwargs):
         print(ds)
