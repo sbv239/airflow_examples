10bc2addf4fa2e22f17f825814b686f5c3c16153
Aleksandr Bosikov <alexanderbosikov@gmail.com>
2023-10-16 11:53:58 +0300

    task 3

---
+++ dags/a-bosikov/hw_3.py
@@ -0,0 +1,41 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+def python_task(task_number: int):
+    print(f'task number is: {task_number}')
+
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}
+
+dag = DAG(
+    'hw_3_a-bosikov',
+    default_args = default_args,
+    start_date = datetime.now(),
+    tags=['a-bosikov']
+)
+
+for i in range(30):
+    if i < 10:
+        bash_task = BashOperator(
+            task_id=f'bash_task_{i}',
+            bash_command=f'echo "Bash task {i}"',
+            dag=dag,
+        )
+        bash_task
+    else:
+        python_task_op = PythonOperator(
+            task_id=f'python_task_{i}',
+            python_callable=python_task,
+            op_kwargs={'task_number': i},
+            provide_context=True,
+            dag=dag,
+        )
+        python_task
