b3557bceb7346e2c12e6802ee4179f44d3773e9d
Aleksandr Bosikov <alexanderbosikov@gmail.com>
2023-10-16 12:34:09 +0300

    HW 9 a-bosikov

---
+++ dags/a-bosikov/hw_9.py
@@ -0,0 +1,44 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+def xcom_task_1(ti):
+    ti.xcom_push(
+            key="sample_xcom_key",
+            value="xcom test"
+    )
+
+def xcom_task_2(ti):
+    print(ti.xcom_pull(key="sample_xcom_key", task_ids='xcom_pusher'))
+
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'hw_9_a-bosikov',
+    default_args = default_args,
+    start_date = datetime.now(),
+    tags=['a-bosikov']
+)
+
+t1 = PythonOperator(
+    task_id='xcom_pusher',
+    python_callable=xcom_task_1,
+    dag = dag
+)
+
+t2 = PythonOperator(
+    task_id='xcom_puller',
+    python_callable=xcom_task_2,
+    dag = dag
+)
+
+t1 >> t2
