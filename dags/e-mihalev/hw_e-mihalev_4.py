49138f6368e160161a06730fb66d4ad1af7c247f
Egor Mikhalev <egorkam2005@gmail.com>
2023-10-16 13:32:36 +0300

    hw_4

---
+++ dags/e-mihalev/hw_e-mihalev_4.py
@@ -0,0 +1,42 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from textwrap import dedent
+
+with DAG(
+'hw_e-mihalev_3',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='A simple DAG',
+    start_date=datetime(2023, 10, 15),
+    catchup=False,
+    tags=['example']
+) as dag:
+    for i in range(10):
+        task1 = BashOperator(task_id='current_directory'+str(i), bash_command=f"echo {i}")
+        task1.doc_md = dedent(
+            """
+            #task1 documentation
+            
+            `code`, **bold**, *cursive*
+          """
+        )
+    def func(task_number):
+        print(f'task number is: {task_number}')
+    for j in range(20):
+        task2 = PythonOperator(task_id='func'+str(j), python_callable=func, op_kwargs={'task_number':j})
+        task2.doc_md = dedent(
+            """
+            #task2 documentation
+
+            `code`, **bold**, *cursive*
+          """
+        )
+    task1 >> task2
