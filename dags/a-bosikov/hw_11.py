99f51fea6f357fe64d89744be8cf4aa24381d2a4
Aleksandr Bosikov <alexanderbosikov@gmail.com>
2023-10-16 13:05:48 +0300

    HW 11 a-bosikov

---
+++ dags/a-bosikov/hw_11.py
@@ -0,0 +1,48 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresHook
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+
+def pg_select():
+    postgres = PostgresHook(postgres_conn_id="startml_feed")
+    with postgres.get_conn() as conn:   # вернет тот же connection, что вернул бы psycopg2.connect(...)
+        with conn.cursor() as cursor:
+            cursor.execute(
+                    """
+                    SELECT user_id, COUNT(action) AS count
+                    FROM feed_action
+                    WHERE action = 'like'
+                    GROUP BY user_id
+                    ORDER BY count DESC
+                    LIMIT 1
+                    """
+                    )
+            return cursor.fetchone()
+
+
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'hw_11_a-bosikov',
+    default_args = default_args,
+    start_date = datetime.now(),
+    tags=['a-bosikov']
+)
+
+t1 = PythonOperator(
+    task_id='pg_connection_test',
+    python_callable=pg_select,
+    dag = dag
+)
+
+t1
