da95278770affdcbb20d64f9309e938f99a88b21
MatchenkovPavel <paha19961@rambler.ru>
2023-10-17 22:51:30 +0300

    2 fix

--- dags/p-matchenkov/DAG_2.py
+++ dags/p-matchenkov/DAG_2.py
@@ -1,3 +1,5 @@
+import datetime
+
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
@@ -6,7 +8,7 @@ from datetime import timedelta
 
 
 
-def print_context(ds, **kwargs):
+def get_ds(ds, **kwargs):
     print(kwargs)
     print(ds)
     return ds
@@ -21,7 +23,12 @@ with DAG(
         'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-    }
+    },
+    description='task_2_dag',
+    #schedule_interval=timedelta(days=1)
+    start_date=datetime.datetime(2023, 10, 16),
+    catchup=False,
+    tags=['matchenkov_2']
 ) as dag:
 
     t1 = BashOperator(
