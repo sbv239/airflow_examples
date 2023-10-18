7d11d1e20338cbf92fd76e77db3b402c4a2b91b6
Irina Lomskaya <is.lomskaya@gmail.com>
2023-10-19 01:31:18 +0300

    fix hw 7

--- dags/i-lomskaja/hm7.py
+++ dags/i-lomskaja/hm7.py
@@ -1,43 +1,42 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
 
-def push_xcom(ti):
-    ti.xcom_push(
-        key='sample_xcom_key',
-        value='xcom test'
-    )
-
-
-def pull_xcom(ti):
-    result = ti.xcom_pull(
-        key='sample_xcom_key',
-        task_ids='hm_7'
-    )
-    print(result)
-
+def print_context(ts, run_id, task_number, **kwargs):
+    print(ts)
+    print(run_id)
+    print(task_number)
 
 with DAG(
-        'hm_7_i-lomskaja',
-        default_args={
-            'dependes_on_past': False,
-            'email': ['airflow@example.com'],
-            'email_on_failure': False,
-            'email_in_retry': False,
-            'retries': 1,
-            'retry_delay': timedelta(minutes=5)
-        },
-        start_date=datetime(2023, 10, 17)
+    'hw_7_i-lomskaya',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  
+},
+
+    description='hw7',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 10, 17),
+    catchup=False
+
 ) as dag:
-    t1 = PythonOperator(
-        task_id='hm_7_1',
-        python_callable=push_xcom
-    )
-
-    t2 = PythonOperator(
-        task_id='hm_7_2',
-        python_callable=pull_xcom
-    )
-
-t1>>t2
+    for i in range(10):
+        t1 = BashOperator(
+        task_id = f'hw7_BashOperator_{i}',      
+        bash_command=f"echo $NUMBER", 
+        env=({'NUMBER': i}),         
+        )
+    for i in range(20):
+        t2 = PythonOperator(
+        task_id = f'hw7_PythonOperator_{i}',
+        python_callable = print_context,
+        op_kwargs={'task_number': i}
+        )
+    t1 >> t2
