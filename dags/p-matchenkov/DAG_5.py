720f85e55869657f163d008007ab41cad0a0b0db
MatchenkovPavel <paha19961@rambler.ru>
2023-10-18 20:35:11 +0300

    template

---
+++ dags/p-matchenkov/DAG_5.py
@@ -0,0 +1,43 @@
+import datetime
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from textwrap import dedent
+
+from datetime import timedelta
+
+
+def print_task_number(task_number):
+    print(f'task number is: {task_number}')
+
+
+with DAG(
+    'hw_p-matchenkov_4',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='task 3 dag',
+    start_date=datetime.datetime(2023, 10, 16),
+    catchup=False,
+    tags=['matchenkov']
+) as dag:
+
+    bash_commands = """
+    {% for i in range(5) %}
+        echo "{{ ts }}"
+        echo '{{ i }}"        
+    {% endfor %}
+    """
+
+    bash_task = BashOperator(
+        task_id='print vars',
+        bash_command=bash_commands
+    )
+
+    bash_task
