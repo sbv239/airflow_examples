8006632674bb9fb26a30d5741ba7c08fe1a400a3
Javid Rzayev <rz.cavid@gmail.com>
2023-10-20 11:48:28 +0200

    HW 2

---
+++ dags/j-rzayev/script_2.py
@@ -0,0 +1,38 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+
+def print_ds(ds, **kwargs):
+    print(ds)
+
+
+with DAG(
+    'Print_ds_pwd',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='Print ds and directory',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 10, 20),
+    catchup=False,
+    tags=['task_2', 'lesson_11']
+) as dag:
+    t1 = BashOperator(
+        task_id='hw_j-rzayev_1',
+        bash_command='pwd',
+    )
+
+    t2 = PythonOperator(
+        task_id='hw_j-rzayev_2',
+        python_callable=print_ds,
+    )
+
+    t1 >> t2
