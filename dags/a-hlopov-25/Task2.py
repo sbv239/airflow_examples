dbc9d6adedfe7f67799074cb8eba505904524287
Alex Khlopov <akhlopov@at-consulting.ru>
2023-10-25 12:57:53 +0700

    first_DAG

---
+++ dags/a-hlopov-25/Task2.py
@@ -0,0 +1,51 @@
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+def print_logic_date(ds):
+        print(ds)
+        return 'Whatever you return gets printed in the logs'
+with DAG(
+    'hw_a-hlopov-25_2',
+    default_args={
+        # Если прошлые запуски упали, надо ли ждать их успеха
+        'depends_on_past': False,
+        # Кому писать при провале
+        'email': ['airflow@example.com'],
+        # А писать ли вообще при провале?
+        'email_on_failure': False,
+        # Писать ли при автоматическом перезапуске по провалу
+        'email_on_retry': False,
+        # Сколько раз пытаться запустить, далее помечать как failed
+        'retries': 1,
+        # Сколько ждать между перезапусками
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    # Описание DAG (не тасок, а самого DAG)
+    description='A simple tutorial DAG',
+    # Как часто запускать DAG
+    schedule_interval=timedelta(days=1),
+    # С какой даты начать запускать DAG
+    # Каждый DAG "видит" свою "дату запуска"
+    # это когда он предположительно должен был
+    # запуститься. Не всегда совпадает с датой на вашем компьютере
+    start_date=datetime(2022, 1, 1),
+    # Запустить за старые даты относительно сегодня
+    # https://airflow.apache.org/docs/apache-airflow/stable/dag-run.html
+    catchup=False,
+    # теги, способ помечать даги
+    tags=['example'],
+) as dag:
+    t1 = BashOperator(
+        task_id="BashPwd",
+        bash_command="pwd ",  
+    )
+  
+    t2 = PythonOperator(
+        task_id='PrintDs',
+        python_callable=print_logic_date
+    )
+    
+    t1>>t2
+
+
