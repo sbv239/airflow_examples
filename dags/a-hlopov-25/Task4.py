b2d5edfa142422da880c5de5ec918e89efd8d6a4
Alex Khlopov <akhlopov@at-consulting.ru>
2023-10-25 17:38:08 +0700

    Task4

---
+++ dags/a-hlopov-25/Task4.py
@@ -0,0 +1,48 @@
+from datetime import timedelta, datetime
+from textwrap import dedent
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+
+with DAG(
+    'hw_4_a-hlopov-25',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+},
+description='hw_4_a-hlopov-25',
+schedule_interval=timedelta(days=1),
+start_date=datetime(2023, 10, 25),
+catchup=False,
+) as dag:
+    def task_num(task_number):
+        print(f"task number is: {task_number}")
+
+    for i in range(30):
+        if i < 10:
+            task = BashOperator(
+                task_id="task_number_"+str(i),
+                bash_command=f"echo {i}",
+            )
+        else:
+            task = PythonOperator(
+                task_id="task_number_"+str(i),
+                python_callable=task_num,
+                op_kwargs={'task_number' : int(i)},
+            )
+    task.doc_md = dedent(
+        """
+            # DAG description
+            You can **document** your _task_ using the attributes `doc_md` (markdown),
+            `doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+            rendered in the UI's Task Instance Details page
+            `def task_num(task_number):
+                print(f"task number is: {task_number}")`
+        """
+    )
