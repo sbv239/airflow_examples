237b13a751a4a6b2da41541c5d734f298bc3cfdc
Andrei Buldakov <andrey.buldakov@domrf.ru>
2023-10-26 23:34:39 +0300

    hws a-buldakov

---
+++ dags/a-buldakov/hw_7.py
@@ -0,0 +1,36 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+def python_task(task_number, ts, run_id):
+    print(f'task number is: {task_number}')
+    print(ts)
+    print(run_id)
+
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+dag = DAG(
+    'hw_7_a-buldakov',
+    default_args = default_args,
+    start_date = datetime.now(),
+    tags=['a-buldakov']
+)
+
+for i in range(20):
+    python_task_op = PythonOperator(
+        task_id=f'python_task_{i}',
+        python_callable=python_task,
+        op_kwargs={'task_number': i},
+        provide_context=True,
+        dag=dag,
+    )
+    python_task
