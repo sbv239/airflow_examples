a2f7a7946678c9e409176b01b8f1e8223ebc5348
Degorych <sergeeve618@gmail.com>
2023-11-02 23:04:37 +0300

    third file

---
+++ dags/e-sergeev-23/hw_4_e-sergeev-23.py
@@ -0,0 +1,58 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+def pythtask(task_number):
+    print(f'task number is: {task_number}')
+with DAG(
+    "hw_4_e-sergeev-23",
+    default_args={
+
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+
+    },
+    schedule_interval=timedelta(days=1),
+    start_date=datetime.now()-timedelta(days=1),
+    catchup=False
+
+
+
+) as dag:
+    for i in range(10):
+
+        t1=BashOperator(
+            task_id=f'Bash{i}',
+            bash_command=f"echo {i}"
+            )
+    for i in range(20):
+        t2=PythonOperator(
+            task_id='pythtask'+ str(i),
+            python_callable=pythtask,
+            op_kwargs={"task_number":int(i)}
+        )
+    t1.doc_md=dedent(
+        '''
+        ### Echo generation
+        `code` this function create echo in for
+        *not smoking*
+        **not vodka**
+        
+        
+        '''
+    )
+    t2.doc_md=dedent(
+        '''
+        ### Z
+        `Z` *Z* **Z**
+        
+        '''
+
+
+    )
