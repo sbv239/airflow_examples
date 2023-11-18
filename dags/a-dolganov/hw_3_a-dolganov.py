7480082eef0f60d8b1fd1de76478eb2d8764fdbf
Alexei Dolganov <alexei.dolganov@gmail.com>
2023-11-18 18:56:12 +0300

    add 10 bo and 20 po

---
+++ dags/a-dolganov/hw_3_a-dolganov.py
@@ -0,0 +1,41 @@
+from datetime import datetime, timedelta
+from textwrap import dedent
+
+from airflow import DAG
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'hw_a-dolganov_3',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),
+    },
+    description='a-dolganov second DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 1, 1),
+    catchup=False,
+    tags=['a-dolganov'],
+) as dag:
+
+    def print_num(task_number):
+        return f"task number is: {task_number}"
+
+    for i in range(30):
+        if i < 10:
+            task = BashOperator(
+                    task_id = 'print_num_' + str(i),
+                    bash_command = f"echo {i}",
+            )
+        else:
+            task = PythonOperator(
+                    task_id ='print_num_' + str(i),
+                    python_callable = print_num,
+                    op_kwargs = {'task_number': i},
+            )
+    task
