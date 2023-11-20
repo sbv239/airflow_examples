288b4b8ebb395f779c73e73e02436d71d45309e8
Sanasar1 <almasyansb@gmail.com>
2023-11-20 18:49:43 +0300

    hw3

--- dags/s-almasjan/dag.py
+++ dags/s-almasjan/dag.py
@@ -33,3 +33,36 @@ with DAG(
     )
 
     t1 >> t2
+
+with DAG(
+    'hw_3_s-almasjan',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+
+    start_date=datetime(2022, 1, 1)
+
+) as dag:
+    
+    def print_task_num(i):
+        print(f'task number is: {i}')
+
+    for i in range(30):
+        if i < 10:
+            print(i)
+            task = BashOperator(
+                task_id = 'it_num',
+                bash_command = 'f"echo {i}"'
+            )
+        else:
+            print(i)
+            task = PythonOperator(
+                task_id = 'task_num',
+                python_callable = print_task_num,
+                op_kwargs = {'i': i}
+            )
