63f23203e557a14fad0539693b27b7776ac49f57
Sanasar1 <almasyansb@gmail.com>
2023-11-20 20:58:02 +0300

    hw 5

--- dags/s-almasjan/dag.py
+++ dags/s-almasjan/dag.py
@@ -96,3 +96,27 @@ with DAG(
     
     task1>>task2
 
+with DAG(
+    'hw_5_s-almasjan',
+    default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+
+    start_date=datetime(2022, 1, 1)
+
+) as dag:
+    templated_command = dedent("""
+        {% for i in range(5) %}
+            echo "{{ ts }}"
+            echo "{{ run_id }}"
+        {% endfor %}
+    """)
+    task = BashOperator(
+        task_id = 'templated_command',
+        bash_command = templated_command
+    )
