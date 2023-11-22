fdb59bd39fdfb8c0581d52cbe2ffc517ca8d612a
Maxim Uglov <maximeuglov@yandex.ru>
2023-11-22 18:11:52 +0300

    add hw_u-maksim_3.py

---
+++ dags/u-maksim/hw_u-maksim_3.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta
+
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+with DAG(
+    'hw_u-maksim_3',
+    default_args={
+        # Если прошлые запуски упали, надо ли ждать их успеха
+        'depends_on_past': False,
+        # Кому писать при провале
+        'email': ['airflow@example.com'],
+        # А писать ли вообще при провале?
+        'email_on_failure': False,
+        # Писать ли при автоматическом перезапуске по провалу
+        'email_on_retry': False,
+        # Сколько раз пытаться запустить, далее помечать как failed
+        'retries': 1,
+        # Сколько ждать между перезапусками
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    # Описание DAG (не тасок, а самого DAG)
+    description='DAG 3',
+    # Как часто запускать DAG
+    schedule_interval=timedelta(days=1),
+    # С какой даты начать запускать DAG
+    # Каждый DAG "видит" свою "дату запуска"
+    # это когда он предположительно должен был
+    # запуститься. Не всегда совпадает с датой на вашем компьютере
+    start_date=datetime(2022, 1, 1),
+    # Запустить за старые даты относительно сегодня
+    catchup=False,
+    # теги, способ помечать даги
+    tags=['example'],
+) as dag:
+
+    # t1, t2, t3 - это операторы (они формируют таски, а таски формируют даг)
+    for i in range(10):
+        t1 = BashOperator(
+        task_id='echo_' + str(i),  # id, будет отображаться в интерфейсе
+        bash_command=f"echo {i}",  # какую bash команду выполнить в этом таске
+    )
+        
+    def task_number(ds, task_number, **kwargs):
+        print(kwargs)
+        print(ds)
+        return f"task number is: {task_number}"
+    
+    for i in range(20):
+        t2 = PythonOperator(
+            task_id='print_task_num_' + str(i),
+            python_callable=task_number, # передаем функцию task_number
+            op_kwargs={'task_number': i} # передаем значение i в функцию task_number свв виде словаря
+        )
+    # А вот так в Airflow указывается последовательность задач
+    t1 >> t2
+    # будет выглядеть вот так
+    #      -> t2
+    #  t1 | 
+    #      -> t3
