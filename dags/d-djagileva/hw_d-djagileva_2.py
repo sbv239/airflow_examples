37e34dc32f77496db3a44d29942fa8bed0f72e6b
Daria Diagileva <darya.dyagileva1@gmail.com>
2023-11-25 14:15:30 +0400

    G task2

---
+++ dags/d-djagileva/hw_d-djagileva_2.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+with DAG(
+    'tutorial',
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}) as dag:
+
+    # t1, t2, t3 - это операторы (они формируют таски, а таски формируют даг)
+    t1 = BashOperator(
+        task_id='directoria',  # id, будет отображаться в интерфейсе
+        bash_command='pwd',  # какую bash команду выполнить в этом таске
+    )
+
+    def print_context(ds, **kwargs):
+        print(kwargs)
+        print(ds)
+        return 'Hi'
+
+    t2 = PythonOperator(
+        task_id='data',  # нужен task_id, как и всем операторам
+        python_callable=print_context,  # свойственен только для PythonOperator - передаем саму функцию
+    )
+
+
+
+
+
