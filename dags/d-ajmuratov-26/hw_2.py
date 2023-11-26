03c17806f61408f09c7c05c04bde499fde19d68f
Dias Aymuratov <davincipro02@Diass-MacBook-Air.local>
2023-11-26 22:20:22 +0600

    HW 2

---
+++ dags/d-ajmuratov-26/hw_2.py
@@ -0,0 +1,41 @@
+"""
+Hello, Airflow Documentation!!!
+"""
+from textwrap import dedent
+
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+
+default_args={
+    'depends_on_past': False,
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+}
+with DAG(
+    'hw_d-ajmuratov-26_2',
+    default_args=default_args,
+    description='Home Work 2',
+    catchup=False,
+    tags=['Home Work 2']
+) as dag:
+    t1 = BashOperator(
+        task_id='print_curr_dir',
+        bash_command='pwd'
+    )
+
+    def print_df(ds, **kwargs):
+        print('Hello, Airflow!!!')
+        print(**kwargs)
+        print(ds)
+    t2 = PythonOperator(
+        task_id='print_ds',
+        python_callable=print_df
+    )
+
+    t1 >> t2
