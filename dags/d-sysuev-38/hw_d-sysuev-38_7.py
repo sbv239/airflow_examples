2cce731a650b7d1df61d6c655df8e15531439c71
Danila <beast2rkk272@gmail.com>
2023-11-27 20:15:47 +0300

    HW 7 d-sysuev-38

--- dags/d-sysuev-38/hw_d-sysuev-38_7.py
+++ dags/d-sysuev-38/hw_d-sysuev-38_7.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
 with DAG(
-    'hw_d-sysuev-38_3',
+    'hw_d-sysuev-38_7',
     default_args={
         'depends_on_past': False,
         'email': ['airflow@example.com'],
@@ -15,11 +15,11 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
-    description='hw_3',
+    description='hw_7',
     schedule_interval=timedelta(days=1),
     start_date=datetime(2023, 11, 25),
     catchup=False,
-    tags=["hw_3"]
+    tags=["hw_7"]
 ) as dag:
 
     def print_task_number(task_number, ts, run_id):
