c98318acb87d8301d952cdbaa5dfe57d28571c05
Dias Aymuratov <davincipro02@Diass-MacBook-Air.local>
2023-11-28 10:36:31 +0600

    HW 3 Updated

--- dags/d-ajmuratov-26/hw_3.py
+++ dags/d-ajmuratov-26/hw_3.py
@@ -1,42 +1,42 @@
-"""
-Hello, Airflow Documentation!!!
-"""
-from textwrap import dedent
-
-from datetime import timedelta
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from airflow import DAG
+
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 
-default_args={
-    'depends_on_past': False,
-    'email': ['airflow@example.com'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
-}
 with DAG(
     'hw_d-ajmuratov-26_3',
-    default_args=default_args,
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5)
+        },
+    schedule_interval=timedelta(days=1),
     start_date=datetime.now(),
     catchup=False,
-    tags=['HM3']
+    tags=['HW 3']
 ) as dag:
-    def print_curr_task_number(task_number):
+
+    for i in range(10):
+        t1 = BashOperator(
+            task_id='print_the_context' + str(i),
+            bash_command=f"echo {i}",
+        )
+
+        t1
+
+    def print_numbers(task_number):
         print(f"task number is: {task_number}")
-    for i in range(30):
-        if i < 10:
-            t = BashOperator(
-                task_id=f'echo_curr_task_number_{i}',
-                bash_command=f'echo {i}'
-            )
-        else:
-            t = PythonOperator(
-                task_id=f'print_curr_task_number_{i}',
-                python_callable=print_curr_task_number,
-                op_args=i
-            )
-        t
+
+    for i in range(20):
+        t2 = PythonOperator(
+            task_id='print_number' + str(i),
+            python_callable=print_numbers,
+            op_kwargs={'task_number': i},
+        )
+
+        t2
