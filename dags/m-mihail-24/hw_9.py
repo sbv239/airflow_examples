ddf70bed9048b5ed5a5b6ac9475ebebc6713ac49
Arhons <nasibovmisha2001@mail.ru>
2023-12-06 20:30:42 +0300

    create xcoms

---
+++ dags/m-mihail-24/hw_9.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from textwrap import dedent
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python import PythonOperator
+
+def push_ti(ti):
+    ti.xcom_push(
+        key="sample_xcom_key",
+        value='xcom test'
+    )
+
+def pull_ti(ti):
+    res = ti.xcom_pull(
+        key="sample_xcom_key",
+        task_ids="push_xcom_ti"
+    )
+    print(res)
+
+with DAG(
+    'm-mihail-24_9',
+    default_args={
+        'depends_on_past': False,
+        'email': ['airflow@example.com'],
+        'email_on_failure': False,
+        'email_on_retry': False,
+        'retries': 1,
+        'retry_delay': timedelta(minutes=5),  # timedelta из пакета datetime
+    },
+    description='A simple tutorial DAG',
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2022, 1, 1),
+    catchup=False,
+    tags=['example_9'],
+) as dag:
+    python_op1 = PythonOperator(
+        task_id='push_xcom_ti',
+        python_callable=push_ti
+    )
+
+    python_op2 = PythonOperator(
+        task_id='pull_xcom_ti',
+        python_callable=pull_ti
+    )
+    python_op1 >> python_op2
